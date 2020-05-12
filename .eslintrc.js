6c53347798444dbd04270d784037859263f0e1b4
xughv <vectorxu@qq.com>
2020-05-12 15:47:43 +0800

    chore: eslint & husky config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,56 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        '@tencent/eslint-config-tencent',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'prettier',
+    'prettier/standard',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@tencent/eslint-config-tencent',
+  ],
+  plugins: ['@typescript-eslint', 'prettier', 'react-hooks'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+        imports: 'always-multiline',
+        objects: 'always-multiline',
+      },
     ],
-    plugins: ['@typescript-eslint'],
-    env: {
-        browser: true,
-        node: true,
-    },
-    settings: {
-        react: {
-            'pragma': 'React',
-            'version': 'detect'
-        }
-    },
-    parserOptions: {
-        ecmaVersion: 2019,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    rules: {
-        '@typescript-eslint/indent': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'react/display-name': 'off',
-        'max-len': ['error', 140]
-    }
-}
+    'func-style': 'off',
+    'function-paren-newline': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': ['error', { trailingComma: 'es5' }],
+    'react/display-name': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'max-len': ['error', 140],
+  },
+};
