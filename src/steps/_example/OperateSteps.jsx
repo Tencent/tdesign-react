83467e79144aa20ccbc94d3b7cf622a6deb3f3e9
rayleichen <lei91122@gmail.com>
2020-06-17 11:00:14 +0800

    feat: 添加Steps组件实现,文档,例子 (merge request !9)
    Squash merge branch 'feature/steps' into 'develop'
    
    * fix: fix readme conflicts
    
    * Merge branch 'rayleichen/tdesign-web-react-feature/steps' into develop
    
    * feat: assign Step in Steps
    
    * feat: add displayName for Step component
    
    * feat: add displayName for Steps component
    
    * feat: add Steps component

---
+++ src/steps/_example/OperateSteps.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Steps, Step } from '@tdesign/react/steps';
+
+export default function OperateSteps() {
+  const [current, setCurrent] = useState(1);
+
+  const onChange = (current, previous) => {
+    console.log(current, previous);
+  };
+
+  const goNextStep = () => {
+    setCurrent(current + 1);
+  };
+
+  const goPreviousStep = () => {
+    setCurrent(current - 1);
+  };
+
+  return (
+    <>
+      <button
+        className="t-button t-button--primary t-size-s"
+        onClick={goPreviousStep}
+      >
+        上一步
+      </button>
+      <button className="t-button t-button--line t-size-s" onClick={goNextStep}>
+        下一步
+      </button>
+      <br />
+      <br />
+      <Steps current={current} onChange={onChange}>
+        <Step title="步骤1" content="这里是提示文字"></Step>
+        <Step title="步骤2" content="这里是提示文字"></Step>
+        <Step title="步骤3" content="这里是提示文字"></Step>
+        <Step title="步骤4" content="这里是提示文字"></Step>
+      </Steps>
+
+      <Steps direction="vertical" current={current}>
+        <Step title="步骤1" content="这里是提示文字"></Step>
+        <Step title="步骤3" content="这里是提示文字"></Step>
+        <Step title="步骤4" content="这里是提示文字"></Step>
+        <Step title="步骤5" content="这里是提示文字"></Step>
+      </Steps>
+    </>
+  );
+}
