99e867530b36f7935a2b001236193051f3cf663b
gedy <755697028@qq.com>
2020-07-03 00:24:39 +0800

    feat: 添加InputGroup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Addon } from './addon';
 export { default as Button } from './button';
+export { default as InputGroup } from './input_group';
 export { default as ConfigProvider } from './config-provider';
 export { default as Icon } from './icon';
 export { default as Popup } from './popup';
