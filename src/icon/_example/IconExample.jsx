7e9a6319cb9c0fba87aa4a6e7bd4962eb9039ce4
Jason <ceocjy@vip.qq.com>
2020-07-06 11:34:36 +0800

    feat: add icon component (merge request !14)
    Squash merge branch 'feature/icon' into 'develop'
    
    * feat: add icon component

--- src/icon/_example/IconExample.jsx
+++ src/icon/_example/IconExample.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { Icon } from '@tdesign/react';
-
-const iconNameList = [
-  'bulletpoint',
-  'browse-20',
-  'close-fill',
-  'close',
-  'discount',
-  'help-fill',
-  'more',
-  'not-visible-20',
-  'loading',
-  'search',
-  'prompt-fill',
-  'success-fill',
-  'warning-fill',
-  'arrow-down',
-  'arrow-left',
-  'arrow-right',
-  'success-2',
-];
+import {
+  BrowseIcon,
+  CloseFillIcon,
+  CloseIcon,
+  DiscountIcon,
+  HelpFillIcon,
+  MoreIcon,
+  NotVisibleIcon,
+  LoadingIcon,
+  SearchIcon,
+  PromptFillIcon,
+  SuccessFillIcon,
+  WarningFillIcon,
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  DoubleArrowRightIcon,
+  EllipsisIcon,
+} from '@tdesign/react';
 
 export default function IconExample() {
   return (
@@ -29,15 +27,74 @@ export default function IconExample() {
         flexWrap: 'wrap',
       }}
     >
-      {iconNameList.map((icon) => (
-        <div
-          key={icon}
-          style={{ width: 150, height: 150, textAlign: 'center' }}
-        >
-          <Icon name={icon} />
-          <p style={{ marginTop: 12 }}>{icon}</p>
-        </div>
-      ))}
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <BrowseIcon />
+        <div style={{ marginTop: 12 }}>BrowseIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <CloseFillIcon />
+        <div style={{ marginTop: 12 }}>CloseFillIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <CloseIcon />
+        <div style={{ marginTop: 12 }}>CloseIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <DiscountIcon />
+        <div style={{ marginTop: 12 }}>DiscountIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <HelpFillIcon />
+        <div style={{ marginTop: 12 }}>HelpFillIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <MoreIcon />
+        <div style={{ marginTop: 12 }}>MoreIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <NotVisibleIcon />
+        <div style={{ marginTop: 12 }}>NotVisibleIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <LoadingIcon />
+        <div style={{ marginTop: 12 }}>LoadingIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <SearchIcon />
+        <div style={{ marginTop: 12 }}>SearchIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <PromptFillIcon />
+        <div style={{ marginTop: 12 }}>PromptFillIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <SuccessFillIcon />
+        <div style={{ marginTop: 12 }}>SuccessFillIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <WarningFillIcon />
+        <div style={{ marginTop: 12 }}>WarningFillIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <ArrowDownIcon />
+        <div style={{ marginTop: 12 }}>ArrowDownIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <ArrowLeftIcon />
+        <div style={{ marginTop: 12 }}>ArrowLeftIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <ArrowRightIcon />
+        <div style={{ marginTop: 12 }}>ArrowRightIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <DoubleArrowRightIcon />
+        <div style={{ marginTop: 12 }}>DoubleArrowRightIcon</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <EllipsisIcon />
+        <div style={{ marginTop: 12 }}>EllipsisIcon</div>
+      </div>
     </div>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { default as Button } from './button';
 export { default as Input } from './input';
 export { default as InputGroup } from './input_group';
 export { default as ConfigProvider } from './config-provider';
-export { default as Icon } from './icon';
+export * from './icon';
 export { default as Popup } from './popup';
 export { default as Table } from './table';
