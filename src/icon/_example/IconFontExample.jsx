60f1f571ab5b401ff13eb9fa69e90028111c67dd
Jason <ceocjy@vip.qq.com>
2020-07-06 11:34:36 +0800

    feat: add icon component

---
+++ src/icon/_example/IconFontExample.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { IconFont } from '@tdesign/react';
+
+export default function IconFontExample() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+      }}
+    >
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="browse" />
+        <div style={{ marginTop: 12 }}>browse</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="close-fill" />
+        <div style={{ marginTop: 12 }}>close-fill</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="close" />
+        <div style={{ marginTop: 12 }}>close</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="discount" />
+        <div style={{ marginTop: 12 }}>discount</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="help-fill" />
+        <div style={{ marginTop: 12 }}>help-fill</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="more" />
+        <div style={{ marginTop: 12 }}>more</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="not-visible" />
+        <div style={{ marginTop: 12 }}>not-visible</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="loading" />
+        <div style={{ marginTop: 12 }}>loading</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="search" />
+        <div style={{ marginTop: 12 }}>search</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="prompt-fill" />
+        <div style={{ marginTop: 12 }}>prompt-fill</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="success-fill" />
+        <div style={{ marginTop: 12 }}>success-fill</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="warning-fill" />
+        <div style={{ marginTop: 12 }}>warning-fill</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="arrow-down" />
+        <div style={{ marginTop: 12 }}>arrow-down</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="arrow-left" />
+        <div style={{ marginTop: 12 }}>arrow-left</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="arrow-right" />
+        <div style={{ marginTop: 12 }}>arrow-right</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="double-arrow-right" />
+        <div style={{ marginTop: 12 }}>double-arrow-right</div>
+      </div>
+      <div style={{ width: 80, height: 80, textAlign: 'center' }}>
+        <IconFont name="ellipsis" />
+        <div style={{ marginTop: 12 }}>ellipsis</div>
+      </div>
+    </div>
+  );
+}
