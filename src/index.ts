6c85f9d57eac9591377a8ade662a3a00628a55b2
xughv <vectorxu@qq.com>
2020-07-16 15:13:21 +0800

    refactor: fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { default as Addon } from './addon';
 export { default as Button } from './button';
 export { default as Input } from './input';
-export { default as InputGroup } from './input_group';
+export { default as InputGroup } from './input-group';
 export { default as ConfigProvider } from './config-provider';
 export * from './icon';
 export { default as Popup } from './popup';
