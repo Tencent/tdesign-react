ccb2178a4168983e1fe99762ec0c81fa80ee16ae
enjkvbej <1258658726@qq.com>
2020-07-22 11:38:50 +0800

    fix: merge

--- src/alert/Alert.tsx
+++ src/alert/Alert.tsx
@@ -2,12 +2,7 @@ import React, { forwardRef } from 'react';
 import classNames from 'classnames';
 import useConfig from '../_util/useConfig';
 import { replaceElement } from '../_util/reactNode';
-import {
-  CloseIcon,
-  PromptFillIcon,
-  SuccessFillIcon,
-  WarningFillIcon,
-} from '../icon';
+import { CloseIcon, PromptFillIcon, SuccessFillIcon, WarningFillIcon } from '../icon';
 
 export interface AlertProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
   /**
@@ -55,135 +50,119 @@ export interface AlertProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
   beforeClose?: (resolve) => void;
 }
 
-const Alert = forwardRef(
-  (props: AlertProps, ref: React.Ref<HTMLDivElement>) => {
-    const {
-      message,
-      title,
-      operation,
-      theme = 'info',
-      icon,
-      close,
-      maxLine,
-      beforeClose,
-      onClose,
-      ...alertProps
-    } = props;
+const Alert = forwardRef((props: AlertProps, ref: React.Ref<HTMLDivElement>) => {
+  const {
+    message,
+    title,
+    operation,
+    theme = 'info',
+    icon,
+    close,
+    maxLine,
+    beforeClose,
+    onClose,
+    ...alertProps
+  } = props;
 
-    const [closed, setClosed] = React.useState(false);
-    const [collapsed, setCollapsed] = React.useState(false);
-    const { classPrefix } = useConfig();
-    const iconMap = {
-      success: SuccessFillIcon,
-      info: PromptFillIcon,
-      error: WarningFillIcon,
-      warning: WarningFillIcon,
-    };
+  const [closed, setClosed] = React.useState(false);
+  const [collapsed, setCollapsed] = React.useState(false);
+  const { classPrefix } = useConfig();
+  const iconMap = {
+    success: SuccessFillIcon,
+    info: PromptFillIcon,
+    error: WarningFillIcon,
+    warning: WarningFillIcon,
+  };
 
-    const handleClose = () => {
-      if (beforeClose) {
-        const fn = new Promise((resolve) => {
-          beforeClose(resolve);
-        });
-        fn.then((value) => {
-          if (value) {
-            setClosed(true);
-            onClose?.();
-          }
-        });
-      } else {
-        setClosed(true);
-        onClose?.();
-      }
-    };
-
-    const handleCollapse = () => {
-      setCollapsed(!collapsed);
-    };
-
-    const renderIconNode = () => {
-      if (React.isValidElement(icon)) {
-        return replaceElement(
-          icon,
-          <span className={`${classPrefix}-alert__icon`}>{icon}</span>,
-          () => ({
-            className: classNames(`${classPrefix}-alert__icon`, {
-              [(icon as any).props.className]: (icon as any).props.className,
-            }),
-          })
-        );
-      }
-      return React.createElement(iconMap[theme], {
-        className: `${classPrefix}-alert__icon`,
+  const handleClose = () => {
+    if (beforeClose) {
+      const fn = new Promise((resolve) => {
+        beforeClose(resolve);
       });
-    };
+      fn.then((value) => {
+        if (value) {
+          setClosed(true);
+          onClose?.();
+        }
+      });
+    } else {
+      setClosed(true);
+      onClose?.();
+    }
+  };
 
-    const renderMessage = () => {
-      if (
-        +maxLine > 0 &&
-        Object.prototype.toString.call(message) === '[object Array]'
-      ) {
-        return (
-          <div className={`${classPrefix}-alert__description`}>
-            {message.map((item, index) => {
-              if (!collapsed) {
-                if (index < maxLine) {
-                  return <div key={index}>{item}</div>;
-                }
-              } else {
+  const handleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const renderIconNode = () => {
+    if (React.isValidElement(icon)) {
+      return replaceElement(
+        icon,
+        <span className={`${classPrefix}-alert__icon`}>{icon}</span>,
+        () => ({
+          className: classNames(`${classPrefix}-alert__icon`, {
+            [(icon as any).props.className]: (icon as any).props.className,
+          }),
+        }),
+      );
+    }
+    return React.createElement(iconMap[theme], {
+      className: `${classPrefix}-alert__icon`,
+    });
+  };
+
+  const renderMessage = () => {
+    if (+maxLine > 0 && Object.prototype.toString.call(message) === '[object Array]') {
+      return (
+        <div className={`${classPrefix}-alert__description`}>
+          {message.map((item, index) => {
+            if (!collapsed) {
+              if (index < maxLine) {
                 return <div key={index}>{item}</div>;
               }
-              return true;
-            })}
-            {+maxLine > 0 ? (
-              <div
-                className={`${classPrefix}-alert__collapse`}
-                onClick={handleCollapse}
-              >
-                {!collapsed ? '展开更多' : '收起'}
-              </div>
-            ) : null}
-          </div>
-        );
-      }
-      return (
-        <div className={`${classPrefix}-alert__description`}>{message}</div>
-      );
-    };
-
-    const renderClose = () => (
-      <div className={`${classPrefix}-alert__close`} onClick={handleClose}>
-        {typeof close === 'boolean' ? <CloseIcon /> : close}
-      </div>
-    );
-
-    return closed ? null : (
-      <div
-        ref={ref}
-        className={classNames(`${classPrefix}-alert`, {
-          [`${classPrefix}-alert--${theme}`]: true,
-        })}
-        {...alertProps}
-      >
-        {icon ? renderIconNode() : null}
-        <div className={`${classPrefix}-alert__content`}>
-          {title ? (
-            <div className={`${classPrefix}-alert__title`}>{title}</div>
+            } else {
+              return <div key={index}>{item}</div>;
+            }
+            return true;
+          })}
+          {+maxLine > 0 ? (
+            <div className={`${classPrefix}-alert__collapse`} onClick={handleCollapse}>
+              {!collapsed ? '展开更多' : '收起'}
+            </div>
           ) : null}
-          <div className={`${classPrefix}-alert__message`}>
-            {renderMessage()}
-            {operation ? (
-              <div className={`${classPrefix}-alert__operation`}>
-                {operation}
-              </div>
-            ) : null}
-          </div>
         </div>
-        {close ? renderClose() : null}
+      );
+    }
+    return <div className={`${classPrefix}-alert__description`}>{message}</div>;
+  };
+
+  const renderClose = () => (
+    <div className={`${classPrefix}-alert__close`} onClick={handleClose}>
+      {typeof close === 'boolean' ? <CloseIcon /> : close}
+    </div>
+  );
+
+  return closed ? null : (
+    <div
+      ref={ref}
+      className={classNames(`${classPrefix}-alert`, {
+        [`${classPrefix}-alert--${theme}`]: true,
+      })}
+      {...alertProps}
+    >
+      {icon ? renderIconNode() : null}
+      <div className={`${classPrefix}-alert__content`}>
+        {title ? <div className={`${classPrefix}-alert__title`}>{title}</div> : null}
+        <div className={`${classPrefix}-alert__message`}>
+          {renderMessage()}
+          {operation ? <div className={`${classPrefix}-alert__operation`}>{operation}</div> : null}
+        </div>
       </div>
-    );
-  }
-);
+      {close ? renderClose() : null}
+    </div>
+  );
+});
 
 Alert.displayName = 'Alert';
 
