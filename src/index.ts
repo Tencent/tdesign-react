4bbf4818b43bfc205b98f867df491cf80c7057e2
magentaqin <magenta2127@gmail.com>
2020-08-03 10:15:48 +0800

    feat: add SingleSelect example

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export { default as Steps } from './steps';
 export { default as Message } from './message';
 export { default as Table } from './table';
 export { default as Tag } from './tag';
+export { default as Select } from './select';
 export { List, ListItem, ListItemMeta } from './list';
 export { Tabs } from './tabs';
 export { default as Notification } from './notification';
--- src/select/index.ts
+++ src/select/index.ts
@@ -1,5 +1,6 @@
 import Select from './base/Select';
+import { SelectProps } from './SelectProps';
 
-export { SelectProps } from './SelectProps';
+export { SelectProps };
 
 export default Select;
