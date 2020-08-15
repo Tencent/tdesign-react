c16fac1d271fb72c1ae2c35494e72e57b23e9f4e
magentaqin <magenta2127@gmail.com>
2020-08-15 21:30:14 +0800

    feat: add MultipleSelect example

--- src/select/_example/SelectSizes.jsx
+++ src/select/_example/SelectSizes.jsx
@@ -5,15 +5,23 @@ import { Select } from '@tdesign/react';
 const { Option } = Select;
 
 const SelectSizes = () => {
-  const [value, setValue] = useState('apple');
-  const onChange = (value) => {
-    setValue(value);
+  const [value1, setValue1] = useState('apple');
+  const onChangeValue1 = (value) => {
+    setValue1(value);
+  };
+  const [value2, setValue2] = useState('apple');
+  const onChangeValue2 = (value) => {
+    setValue2(value);
+  };
+  const [value3, setValue3] = useState('apple');
+  const onChangeValue3 = (value) => {
+    setValue3(value);
   };
   return (
     <div>
       <div style={{ display: 'flex' }}>
         <h4 style={{ width: '80px' }}>Small</h4>
-        <Select value={value} change={onChange} size="small" style={{ width: '30%' }}>
+        <Select value={value1} change={onChangeValue1} size="small" style={{ width: '30%' }}>
           <Option key="apple" label="Apple" value="apple" />
           <Option key="orange" label="Orange" value="orange" />
           <Option key="banana" label="Banana" value="banana" />
@@ -21,7 +29,7 @@ const SelectSizes = () => {
       </div>
       <div style={{ display: 'flex' }}>
         <h4 style={{ width: '80px' }}>Default</h4>
-        <Select value={value} change={onChange} style={{ width: '30%' }}>
+        <Select value={value2} change={onChangeValue2} style={{ width: '30%' }}>
           <Option key="apple" label="Apple" value="apple" />
           <Option key="orange" label="Orange" value="orange" />
           <Option key="banana" label="Banana" value="banana" />
@@ -29,7 +37,7 @@ const SelectSizes = () => {
       </div>
       <div style={{ display: 'flex' }}>
         <h4 style={{ width: '80px' }}>Large</h4>
-        <Select value={value} change={onChange} size="large" style={{ width: '30%' }}>
+        <Select value={value3} change={onChangeValue3} size="large" style={{ width: '30%' }}>
           <Option key="apple" label="Apple" value="apple" />
           <Option key="orange" label="Orange" value="orange" />
           <Option key="banana" label="Banana" value="banana" />
--- src/select/base/Option.tsx
+++ src/select/base/Option.tsx
@@ -1,3 +1,4 @@
+import { isNumber, isString } from 'util';
 import React from 'react';
 import classNames from 'classnames';
 import useConfig from '../../_util/useConfig';
@@ -5,19 +6,61 @@ import { SelectOption } from '../SelectProps';
 
 const Option = (props: SelectOption) => {
   const { classPrefix } = useConfig();
-  const { disabled, size, value, label } = props;
+  const { disabled, size, value, multiple, selectedValue, onSelect } = props;
+  const label = props.label || value;
   const componentType = 'select';
+  let selected = value === selectedValue;
+  if (multiple && Array.isArray(selectedValue)) {
+    selected = selectedValue.some((item) => {
+      if (isNumber(item) || isString(item)) {
+        return item === value;
+      }
+      return item.value === value;
+    });
+  }
+
+  const handleSelect = () => {
+    if (!disabled) {
+      onSelect(value, label, selected);
+    }
+  };
+
+  const renderItem = () => {
+    if (multiple) {
+      return (
+        <label
+          className={classNames(`${classPrefix}-checkbox`, {
+            [`${classPrefix}-is-disabled`]: disabled,
+            [`${classPrefix}-is-checked`]: selected,
+          })}
+        >
+          <input
+            type="checkbox"
+            className={classNames(`${classPrefix}-checkbox__former`)}
+            value=""
+            disabled={disabled}
+            onClick={(e) => e.stopPropagation()}
+          />
+          <span className={classNames(`${classPrefix}-checkbox__input`)}></span>
+          <span className={classNames(`${classPrefix}-checkbox__label`)}>{label}</span>
+        </label>
+      );
+    }
+    return <React.Fragment>{label}</React.Fragment>;
+  };
 
   return (
     <li
       className={classNames(props.className, `${classPrefix}-${componentType}-option`, {
         [`${classPrefix}-is-disabled`]: disabled,
-        't-size-s': size === 'small',
-        't-size-l': size === 'large',
+        [`${classPrefix}-is-selected`]: selected,
+        [`${classPrefix}-size-s`]: size === 'small',
+        [`${classPrefix}-size-l`]: size === 'large',
       })}
       key={value}
+      onClick={handleSelect}
     >
-      {label}
+      {renderItem()}
     </li>
   );
 };
