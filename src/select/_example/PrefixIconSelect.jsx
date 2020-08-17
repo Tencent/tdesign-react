4bdc2b51eb273f04a34c3009e76be08c0a57eda1
magentaqin <magenta2127@gmail.com>
2020-08-18 07:37:20 +0800

    feat: add PrefixIconExample

---
+++ src/select/_example/PrefixIconSelect.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import { Select, Icon } from '@tdesign/react';
+
+const { Option } = Select;
+
+const PrefixIconSelect = () => {
+  const [value, setValue] = useState('apple');
+  const onChange = (value) => {
+    setValue(value);
+  };
+  return (
+    <div style={{ display: 'flex' }}>
+      <Select
+        value={value}
+        change={onChange}
+        style={{ width: '40%' }}
+        prefixIcon={() => <Icon name="github" />}
+      >
+        <Option key="apple" label="Apple" value="apple" />
+        <Option key="orange" label="Orange" value="orange" disabled />
+        <Option key="banana" label="Banana" value="banana" />
+      </Select>
+    </div>
+  );
+};
+
+export default PrefixIconSelect;
