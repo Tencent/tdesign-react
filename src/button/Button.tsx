4dbe544dbc8034c046f790f88e9881d7fdbdce2c
xughv <vectorxu@qq.com>
2020-08-24 11:54:07 +0800

    feat: add button  support

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -61,51 +61,60 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 /**
  * 按钮组件
  */
-const Button = forwardRef((props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
-  const {
-    theme = 'line',
-    icon,
-    disabled,
-    loading,
-    size,
-    block,
-    children,
-    className,
-    onClick = noop,
-    ...buttonProps
-  } = props;
+const Button = forwardRef(
+  (
+    {
+      theme = 'line',
+      icon,
+      disabled,
+      loading,
+      size,
+      block,
+      children,
+      className,
+      onClick = noop,
+      ...buttonProps
+    }: ButtonProps,
+    ref: React.Ref<HTMLButtonElement>,
+  ) => {
+    const { classPrefix } = useConfig();
 
-  const { classPrefix } = useConfig();
+    const hasChildren = typeof children !== 'undefined';
 
-  const hasChildren = typeof children !== 'undefined';
+    if (loading) {
+      // eslint-disable-next-line no-param-reassign
+      icon = 'loading';
+    }
 
-  return (
-    <button
-      ref={ref}
-      className={classNames(className, `${classPrefix}-button`, {
-        [`${classPrefix}-button--${theme}`]: hasChildren || icon,
-        [`${classPrefix}-button--icon`]: !hasChildren && icon && theme !== 'primary',
-        [`${classPrefix}-button--icon-primary`]: !hasChildren && icon && theme === 'primary',
-        [`${classPrefix}-is-disabled`]: disabled,
-        [`${classPrefix}-size-s`]: size === 'small',
-        [`${classPrefix}-size-l`]: size === 'large',
-        [`${classPrefix}-size-full-width`]: block,
-      })}
-      onClick={!disabled && !loading ? onClick : undefined}
-      disabled={disabled}
-      {...buttonProps}
-    >
-      {icon ? (
-        <>
-          {typeof icon === 'string' ? <Icon name={icon} /> : icon}
-          {hasChildren && <span className={`${classPrefix}-button__text`}>{children}</span>}
-        </>
-      ) : (
-        children
-      )}
-    </button>
-  );
-});
+    return (
+      <button
+        ref={ref}
+        className={classNames(className, `${classPrefix}-button`, {
+          [`${classPrefix}-button--${theme}`]: hasChildren || icon,
+          [`${classPrefix}-button--icon`]: !hasChildren && icon && theme !== 'primary',
+          [`${classPrefix}-button--icon-primary`]: !hasChildren && icon && theme === 'primary',
+          [`${classPrefix}-is-loading`]: loading,
+          [`${classPrefix}-is-disabled`]: disabled,
+          [`${classPrefix}-size-s`]: size === 'small',
+          [`${classPrefix}-size-l`]: size === 'large',
+          [`${classPrefix}-size-full-width`]: block,
+        })}
+        onClick={!disabled && !loading ? onClick : undefined}
+        disabled={disabled}
+        {...buttonProps}
+      >
+        {icon ? (
+          <>
+            {typeof icon === 'string' ? <Icon name={icon} /> : icon}
+            {hasChildren && <span className={`${classPrefix}-button__text`}>{children}</span>}
+          </>
+        ) : (
+          children
+        )}
+      </button>
+    );
+  },
+);
 
 Button.displayName = 'Button';
 
