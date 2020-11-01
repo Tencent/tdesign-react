a36d4790ed3f90e948a8500afa6dcd960bacb6b3
zhoulujun <zhoulujun@live.cn>
2020-11-01 22:05:12 +0800

    style: 样式修改

--- src/progress/Progress.tsx
+++ src/progress/Progress.tsx
@@ -1,6 +1,8 @@
 import React, { forwardRef } from 'react';
 import { PromptFillIcon, SuccessFillIcon, WarningFillIcon, CloseFillIcon } from '../icon';
 import useConfig from '../_util/useConfig';
+import getBackgroundColor from '../_util/linearGradient';
+import { ProgressProps } from './ProgressProps';
 
 const iconMap = {
   success: SuccessFillIcon,
@@ -16,71 +18,9 @@ const colorMap = {
 };
 
 /**
- * 除表格中列出的属性外，支持透传原生 `<button>` 标签支持的属性。
- */
-export interface ProgressProps {
-  /**
-   * 主题类型
-   * @default 'thin'
-   */
-  theme?: 'thin' | 'plump' | 'circle' | 'dashboard';
-
-  /**
-   * 百分比
-   * @default 0
-   */
-  percentage: number;
-
-  /**
-   * 是否显示label，为boolean时，默认true
-   * @default true
-   */
-  label?: React.ReactNode | boolean;
-
-  /**
-   * 进度条当前状态，success、error、warning、active(仅type=line可用）
-   * @default 'normal'
-   */
-  status?: 'success' | 'error' | 'warning' | 'active' | 'normal';
-
-  /**
-   * 进度条的颜色，object的时候是渐变色
-   * @default '#0052D9'
-   */
-  color?: 'string' | object;
-
-  /**
-   * 进度条未完成的颜色——轨道颜色
-   * @default '#F5F5F5'
-   */
-  trackColor?: string;
-
-  /**
-   * 进度条线的宽度（轨道宽度），默认number单位px
-   * @default 'middle'
-   */
-  strokeWidth?: number | string;
-
-  /**
-   * theme为circle，环形尺寸大小
-   * @default 'default'
-   */
-
-  size?: number | 'large' | 'middle' | 'small';
-  /**
-   * 步骤进度条,步数
-   */
-  steps?: number;
-  /**
-   * class
-   */
-  className?: 'string';
-}
-
-/**
  * 按钮组件
  */
-const Progress = forwardRef((props: ProgressProps) => {
+const Progress = forwardRef((props: ProgressProps, ref: React.Ref<HTMLDivElement>) => {
   const {
     theme = 'line',
     percentage = 30,
@@ -93,7 +33,6 @@ const Progress = forwardRef((props: ProgressProps) => {
     className,
   } = props;
 
-  let progress;
   const { classPrefix } = useConfig();
   // 进度条轨道高度
   const getHeight = (): string => {
@@ -101,6 +40,32 @@ const Progress = forwardRef((props: ProgressProps) => {
       return typeof strokeWidth === 'string' ? strokeWidth : `${strokeWidth}px`;
     }
   };
+  // 进度条展示内容
+  const getInfoContent = () => {
+    if (!label) {
+      return '';
+    }
+    let info;
+    // 为布尔值，默认百分百展示，否则之间展示label内容
+    if (typeof label === 'boolean') {
+      info = <div className={`${classPrefix}-progress--info`}>{`${percentage}%`}</div>;
+      if (['success', 'error', 'warning'].includes(status)) {
+        const Icon = iconMap[status];
+        info = (
+          <div className={`${classPrefix}-progress--info`}>
+            <Icon className={`${classPrefix}-progress--icon`} />
+          </div>
+        );
+      }
+    } else {
+      info = <div className={`${classPrefix}-progress--info`}>{label}</div>;
+    }
+    return info;
+  };
+  const trackStyle = { height: getHeight(), backgroundColor: trackColor } as React.CSSProperties;
+  const barStyle = { width: `${percentage}%`, background: getBackgroundColor(color) } as React.CSSProperties;
+  // 进度条颜色
+  const circleBarColor = color || colorMap[status];
   // 获取直径
   const getDiameter = (): number => {
     let diameter = 112;
@@ -123,30 +88,7 @@ const Progress = forwardRef((props: ProgressProps) => {
     }
     return diameter;
   };
-  // 进度条颜色
-  const getColor = (): string => color || colorMap[status];
-  // 进度条展示内容
-  const getInfoContent = () => {
-    if (!label) {
-      return '';
-    }
-    let info;
-    // 为布尔值，默认百分百展示，否则之间展示label内容
-    if (typeof label === 'boolean') {
-      info = <div className={`${classPrefix}-progress--bar--info`}>{`${percentage}%`}</div>;
-      if (['success', 'error', 'warning'].includes(status)) {
-        const Icon = iconMap[status];
-        info = (
-          <div className={`${classPrefix}-progress--bar--info`}>
-            <Icon className={`${classPrefix}-progress--icon`} />
-          </div>
-        );
-      }
-    } else {
-      info = <div className={`${classPrefix}-progress--bar--info`}>{label}</div>;
-    }
-    return info;
-  };
+  // 获取环形进度条 环形宽度
   const getCircleStokeWidth = (): number => {
     if (!strokeWidth) {
       if (size === 'small') {
@@ -158,36 +100,38 @@ const Progress = forwardRef((props: ProgressProps) => {
     }
     return strokeWidth;
   };
-  const stroke = getCircleStokeWidth();
-  const diameter = Number(getDiameter());
+  // 环形进度条 环形宽度
+  const circleStokeWidth = getCircleStokeWidth();
+  const diameter = getDiameter();
   const radius = diameter / 2;
-  const innerRadius = radius - stroke;
+  const innerRadius = radius - circleStokeWidth;
 
-  const perimeter = Math.PI * 2 * (radius - stroke);
+  const perimeter = Math.PI * 2 * (radius - circleStokeWidth);
   const percent = percentage / 100;
   const strokeDasharray = `${perimeter * percent}  ${perimeter * (1 - percent)}`;
-  const fontSize = innerRadius * 0.17;
+  const fontSizeRatio = innerRadius * 0.27;
   const circleStyle = {
     width: diameter,
     height: diameter,
-    fontSize: 6 + fontSize,
+    fontSize: 4 + fontSizeRatio,
   } as React.CSSProperties;
-  const trackStyle = { height: getHeight(), backgroundColor: trackColor } as React.CSSProperties;
-  const barStyle = { width: `${percentage}%`, backgroundColor: color } as React.CSSProperties;
+  let progressDom;
   switch (theme) {
     case 'circle':
-    case 'dashboard':
-      progress = (
+      // case 'dashboard':
+      progressDom = (
         <div
+          ref={ref}
           className={`${classPrefix}-progress--circle ${classPrefix}-progress--status--${status}`}
           style={circleStyle}
         >
+          {getInfoContent()}
           <svg width={diameter} height={diameter} viewBox={`0 0 ${diameter} ${diameter}`}>
             <circle
               cx={radius}
               cy={radius}
               r={innerRadius}
-              strokeWidth={stroke}
+              strokeWidth={circleStokeWidth}
               stroke={trackColor}
               fill="none"
             ></circle>
@@ -195,29 +139,29 @@ const Progress = forwardRef((props: ProgressProps) => {
               cx={radius}
               cy={radius}
               r={innerRadius}
-              strokeWidth={stroke}
-              stroke={getColor()}
+              strokeWidth={circleStokeWidth}
+              stroke={circleBarColor}
               fill="none"
               transform={`matrix(0,-1,1,0,0,${diameter})`}
               strokeDasharray={strokeDasharray}
             ></circle>
           </svg>
-          {getInfoContent()}
         </div>
       );
       break;
     case 'plump':
       if (percentage > 10) {
       }
-      progress = (
+      progressDom = (
         <div
+          ref={ref}
           className={`${classPrefix}-progress--bar ${classPrefix}-progress--plump ${classPrefix}-progress--status--${status}`}
           style={trackStyle}
         >
-          <div className={`${classPrefix}-progress--bar--inner`} style={barStyle}>
+          <div className={`${classPrefix}-progress--inner`} style={barStyle}>
             {label && (
               <div
-                className={`${classPrefix}-progress--bar--info`}
+                className={`${classPrefix}-progress--info`}
                 style={percentage > 10 ? { color: '#fff' } : { right: '-2.5em' }}
               >{`${percentage}%`}</div>
             )}
@@ -227,17 +171,17 @@ const Progress = forwardRef((props: ProgressProps) => {
       break;
     case 'thin':
     default:
-      progress = (
-        <div className={`${classPrefix}-progress--thin ${classPrefix}-progress--status--${status}`}>
+      progressDom = (
+        <div ref={ref} className={`${classPrefix}-progress--thin ${classPrefix}-progress--status--${status}`}>
           <div className={`${classPrefix}-progress--bar`} style={trackStyle}>
-            <div className={`${classPrefix}-progress--bar--inner`} style={barStyle}></div>
+            <div className={`${classPrefix}-progress--inner`} style={barStyle}></div>
           </div>
           {getInfoContent()}
         </div>
       );
       break;
   }
-  return <div className={className}>{progress}</div>;
+  return <div className={className}>{progressDom}</div>;
 });
 
 Progress.displayName = 'Progress';
