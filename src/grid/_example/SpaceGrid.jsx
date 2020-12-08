3c5451035a77745affe66d1346eb99550c5da69f
HQ-Lin <a154571896@gmail.com>
2020-12-08 16:35:40 +0800

    fix: 调整为12栅格

--- src/grid/_example/SpaceGrid.jsx
+++ src/grid/_example/SpaceGrid.jsx
@@ -5,59 +5,59 @@ export default function SpaceGrid() {
   return (
     <>
       <Row gutter={16}>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
       </Row>
 
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32, xl: 32, xxl: 40 }}>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
       </Row>
 
       <Row gutter={[16, 24]}>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
-        <Col span={6}>
-          <div>col-6</div>
+        <Col span={3}>
+          <div>col-3</div>
         </Col>
       </Row>
     </>
