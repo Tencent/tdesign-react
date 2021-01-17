c9d329e49b6f8c436345e8fd6455f3c9a8d22a78
shryzhang <996707719@qq.com>
2021-01-17 16:56:48 +0800

    feat: 树节点过滤逻辑bug修复，显示效果优化

--- src/tree/_example/TreeFilter.jsx
+++ src/tree/_example/TreeFilter.jsx
@@ -9,10 +9,10 @@ const initData = [
         label: '我是节点1-1',
       },
       {
-        label: '我是节点1-2',
+        label: '我是节点1-2aaa',
         children: [
           {
-            label: '我是节点1-2-1',
+            label: '我是节点1-2-1aaa',
           },
           {
             label: '我是节点1-2-2',
@@ -26,7 +26,7 @@ const initData = [
         label: '我是节点1-3',
       },
     ],
-    label: '我是节点1',
+    label: '我是节点1aaa',
   },
   {
     children: [
@@ -72,9 +72,13 @@ const initData = [
 
 function filter(node, value) {
   if (!value) {
+    // 没有关键字时，仅显示根节点
+    if (!node.getParentData()) {
+      return true;
+    }
     return false;
   }
-  return value && node.label.indexOf(value) > 0;
+  return node.data && node.data.label && node.data.label.indexOf(value) > 0;
 }
 
 export default function TreeExample() {
