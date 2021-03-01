5a8fe040f1ad5f657134d61fc80250c054397bc2
HQ-Lin <a154571896@gmail.com>
2021-03-01 10:40:36 +0800

    feat: 调整 alert api改动

--- src/alert/_example/icon.jsx
+++ src/alert/_example/icon.jsx
@@ -4,10 +4,10 @@ import { Alert } from '@tencent/tdesign-react';
 export default function AlertIcon() {
   return (
     <>
-      <Alert theme="success" message="这是一条成功的消息提示" icon />
-      <Alert theme="info" message="这是一条普通的消息提示" icon />
-      <Alert theme="warning" message="这是一条警示消息" icon />
-      <Alert theme="error" message="高危操作/出错信息提示" icon />
+      <Alert theme="success" message="这是一条成功的消息提示" />
+      <Alert theme="info" message="这是一条普通的消息提示" />
+      <Alert theme="warning" message="这是一条警示消息" />
+      <Alert theme="error" message="高危操作/出错信息提示" />
     </>
   );
 }
