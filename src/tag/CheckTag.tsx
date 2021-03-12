b3181046275703e23a8ec8471ddd71e0d40b8873
HQ-Lin <a154571896@gmail.com>
2021-03-12 19:24:57 +0800

    feat: 调整 Tag 组件 api

--- src/tag/CheckTag.tsx
+++ src/tag/CheckTag.tsx
@@ -2,36 +2,13 @@ import React, { forwardRef } from 'react';
 import classNames from 'classnames';
 import useDefault from '../_util/useDefault';
 import useConfig from '../_util/useConfig';
+import { TdCheckTagProps } from '../_type/components/tag';
 import { StyledProps } from '../_type';
 
 /**
  * CheckTag 组件支持的属性。
  */
-export interface CheckTagProps extends StyledProps {
-  /**
-   * 设置按钮为禁用状态
-   *
-   *@default false
-   * */
-  disabled?: boolean;
-
-  /**
-   * 默认选中状态
-   *
-   * @default false
-   */
-  defaultChecked?: boolean;
-
-  /**
-   * 选中状态
-   */
-  checked?: boolean;
-
-  /**
-   * 选中状态变化回调
-   */
-  onChange?: (checked?: boolean) => void;
-
+export interface CheckTagProps extends TdCheckTagProps, StyledProps {
   /**
    * 标签内容
    */
@@ -39,7 +16,7 @@ export interface CheckTagProps extends StyledProps {
 }
 
 const CheckTag = forwardRef((props: CheckTagProps, ref: React.Ref<HTMLSpanElement>) => {
-  const { checked, onChange, disabled, children, className, ...tagOtherProps } = props;
+  const { checked, content, onChange, disabled, children, className, ...tagOtherProps } = props;
   const [value, onValueChange] = useDefault(checked, false, onChange);
 
   const { classPrefix } = useConfig();
@@ -59,7 +36,7 @@ const CheckTag = forwardRef((props: CheckTagProps, ref: React.Ref<HTMLSpanElemen
         !disabled && onValueChange(!value);
       }}
     >
-      {children}
+      {children || content}
     </span>
   );
 });
