b34215767f6f7270cf604b8537a85435f8b2f3ca
xinranzhou <707326224@qq.com>
2021-03-13 18:58:25 +0800

    feat: 更新tooltip组件文档

--- src/popup/_example/PlacementsExample.jsx
+++ src/popup/_example/PlacementsExample.jsx
@@ -1,68 +1,136 @@
 import React from 'react';
 import { Button, Popup } from '@tencent/tdesign-react';
-import './placements.css';
+const styles = {
+  container: {
+    margin: '0 auto',
+    width: '500px',
+    height: '260px',
+    position: 'relative',
+  },
+  placementTop: {
+    position: 'absolute',
+    top: '0',
+    left: '42%',
+  },
+  placementTopLeft: {
+    position: 'absolute',
+    top: '0',
+    left: '70px',
+  },
+  placementTopRight: {
+    position: 'absolute',
+    top: '0',
+    right: '70px',
+  },
+  placementBottom: {
+    position: 'absolute',
+    bottom: '0',
+    left: '42%',
+  },
+  placementBottomLeft: {
+    position: 'absolute',
+    bottom: '0',
+    left: '70px',
+    width: '120px',
+  },
+  placementBottomRight: {
+    position: 'absolute',
+    bottom: '0',
+    right: '70px',
+  },
+  placementLeft: {
+    position: 'absolute',
+    left: '0',
+    top: '42%',
+  },
+  placementLeftTop: {
+    position: 'absolute',
+    left: '0',
+    top: '50px',
+  },
+  placementLeftBottom: {
+    position: 'absolute',
+    left: '0',
+    bottom: '50px',
+  },
+  placementRight: {
+    position: 'absolute',
+    right: '0',
+    top: '42%',
+  },
+  placementRightTop: {
+    position: 'absolute',
+    right: '0',
+    top: '50px',
+  },
+  placementRightBottom: {
+    position: 'absolute',
+    right: '0',
+    bottom: '50px',
+  },
+};
 export default function Placements() {
   return (
-    <div className="container">
-      <Popup content="这是popup内容" placement="top" showArrow destroyOnHide>
-        <Button className="placement-top">top</Button>
+    <div style={styles.container}>
+      <Popup content="这是Popup内容" placement="top" showArrow destroyOnHide>
+        <Button style={styles.placementTop}>top</Button>
       </Popup>
-      <Popup content="这是popup内容 top-left" placement="top-left" showArrow destroyOnHide>
-        <Button className="placement-top-left">top-left</Button>
+      <Popup content="这是Popup内容 top-left" placement="top-left" showArrow destroyOnHide>
+        <Button style={styles.placementTopLeft}>top-left</Button>
       </Popup>
-      <Popup content="这是popup内容top-right" placement="top-right" showArrow destroyOnHide>
-        <Button className="placement-top-right">top-right</Button>
+      <Popup content="这是Popup内容top-right" placement="top-right" showArrow destroyOnHide>
+        <Button style={styles.placementTopRight}>top-right</Button>
       </Popup>
-      <Popup content="这是popup内容" placement="bottom" showArrow destroyOnHide>
-        <Button className="placement-bottom">bottom</Button>
+      <Popup content="这是Popup内容" placement="bottom" showArrow destroyOnHide>
+        <Button style={styles.placementBottom}>bottom</Button>
       </Popup>
-      <Popup content="这是popup内容 bottom-left" placement="bottom-left" showArrow destroyOnHide>
-        <Button className="placement-bottom-left">bottom-left</Button>
+      <Popup content="这是Popup内容 bottom-left" placement="bottom-left" showArrow destroyOnHide>
+        <Button style={styles.placementBottomLeft}>bottom-left</Button>
       </Popup>
-      <Popup content="这是popup内容 bottom-right" placement="bottom-right" showArrow destroyOnHide>
-        <Button className="placement-bottom-right">bottom-right</Button>
+      <Popup content="这是Popup内容 bottom-right" placement="bottom-right" showArrow destroyOnHide>
+        <Button style={styles.placementBottomRight}>bottom-right</Button>
       </Popup>
-      <Popup content="这是popup内容" placement="left" showArrow destroyOnHide>
-        <Button className="placement-left">left</Button>
+      <Popup content="这是Popup内容" placement="left" showArrow destroyOnHide>
+        <Button style={styles.placementLeft}>left</Button>
       </Popup>
       <Popup
-        content="这是popup内容   left-top"
+        content="这是Popup内容   left-top"
         placement="left-top"
         overlayStyle={{ width: '140px' }}
         showArrow
         destroyOnHide
       >
-        <Button className="placement-left-top">left-top</Button>
+        <Button style={styles.placementLeftTop}>left-top</Button>
       </Popup>
       <Popup
-        content="这是popup内容 left-bottom"
+        content="这是Popup内容 left-bottom"
         placement="left-bottom"
         overlayStyle={{ width: '140px' }}
         showArrow
         destroyOnHide
       >
-        <Button className="placement-left-bottom">left-bottom</Button>
+        <Button style={styles.placementLeftBottom}>left-bottom</Button>
       </Popup>
-      <Popup content="这是popup内容" placement="right" showArrow destroyOnHide>
-        <Button className="placement-right">right</Button>
+      <Popup content="这是Popup内容" placement="right" showArrow destroyOnHide>
+        <Button style={styles.placementRight}>right</Button>
       </Popup>
       <Popup
-        content="这是popup内容 right-top"
+        content="这是Popup内容 right-top"
         placement="right-top"
         overlayStyle={{ width: '140px' }}
         showArrow
         destroyOnHide
       >
-        <Button className="placement-right-top">right-top</Button>
+        <Button style={styles.placementRightTop}>right-top</Button>
       </Popup>
       <Popup
-        content="这是popup内容 right-bottom"
+        content="这是Popup内容 right-bottom"
         placement="right-bottom"
         overlayStyle={{ width: '140px' }}
         showArrow
         destroyOnHide
       >
-        <Button className="placement-right-bottom">right-bottom</Button>
+        <Button style={styles.placementRightBottom}>right-bottom</Button>
       </Popup>
     </div>
   );
--- src/tooltip/_example/placement.jsx
+++ src/tooltip/_example/placement.jsx
@@ -1,29 +1,97 @@
 import React from 'react';
 import { Button, Tooltip } from '@tencent/tdesign-react';
-import './placements.css';
+const styles = {
+  container: {
+    margin: '0 auto',
+    width: '500px',
+    height: '260px',
+    position: 'relative',
+  },
+  placementTop: {
+    position: 'absolute',
+    top: '0',
+    left: '42%',
+  },
+  placementTopLeft: {
+    position: 'absolute',
+    top: '0',
+    left: '70px',
+  },
+  placementTopRight: {
+    position: 'absolute',
+    top: '0',
+    right: '70px',
+  },
+  placementBottom: {
+    position: 'absolute',
+    bottom: '0',
+    left: '42%',
+  },
+  placementBottomLeft: {
+    position: 'absolute',
+    bottom: '0',
+    left: '70px',
+    width: '120px',
+  },
+  placementBottomRight: {
+    position: 'absolute',
+    bottom: '0',
+    right: '70px',
+  },
+  placementLeft: {
+    position: 'absolute',
+    left: '0',
+    top: '42%',
+  },
+  placementLeftTop: {
+    position: 'absolute',
+    left: '0',
+    top: '50px',
+  },
+  placementLeftBottom: {
+    position: 'absolute',
+    left: '0',
+    bottom: '50px',
+  },
+  placementRight: {
+    position: 'absolute',
+    right: '0',
+    top: '42%',
+  },
+  placementRightTop: {
+    position: 'absolute',
+    right: '0',
+    top: '50px',
+  },
+  placementRightBottom: {
+    position: 'absolute',
+    right: '0',
+    bottom: '50px',
+  },
+};
 export default function Placements() {
   return (
-    <div className="container">
+    <div style={styles.container}>
       <Tooltip content="这是Tooltip内容" placement="top" showArrow destroyOnHide>
-        <Button className="placement-top">top</Button>
+        <Button style={styles.placementTop}>top</Button>
       </Tooltip>
       <Tooltip content="这是Tooltip内容 top-left" placement="top-left" showArrow destroyOnHide>
-        <Button className="placement-top-left">top-left</Button>
+        <Button style={styles.placementTopLeft}>top-left</Button>
       </Tooltip>
       <Tooltip content="这是Tooltip内容top-right" placement="top-right" showArrow destroyOnHide>
-        <Button className="placement-top-right">top-right</Button>
+        <Button style={styles.placementTopRight}>top-right</Button>
       </Tooltip>
       <Tooltip content="这是Tooltip内容" placement="bottom" showArrow destroyOnHide>
-        <Button className="placement-bottom">bottom</Button>
+        <Button style={styles.placementBottom}>bottom</Button>
       </Tooltip>
       <Tooltip content="这是Tooltip内容 bottom-left" placement="bottom-left" showArrow destroyOnHide>
-        <Button className="placement-bottom-left">bottom-left</Button>
+        <Button style={styles.placementBottomLeft}>bottom-left</Button>
       </Tooltip>
       <Tooltip content="这是Tooltip内容 bottom-right" placement="bottom-right" showArrow destroyOnHide>
-        <Button className="placement-bottom-right">bottom-right</Button>
+        <Button style={styles.placementBottomRight}>bottom-right</Button>
       </Tooltip>
       <Tooltip content="这是Tooltip内容" placement="left" showArrow destroyOnHide>
-        <Button className="placement-left">left</Button>
+        <Button style={styles.placementLeft}>left</Button>
       </Tooltip>
       <Tooltip
         content="这是Tooltip内容   left-top"
@@ -32,7 +100,7 @@ export default function Placements() {
         showArrow
         destroyOnHide
       >
-        <Button className="placement-left-top">left-top</Button>
+        <Button style={styles.placementLeftTop}>left-top</Button>
       </Tooltip>
       <Tooltip
         content="这是Tooltip内容 left-bottom"
@@ -41,10 +109,10 @@ export default function Placements() {
         showArrow
         destroyOnHide
       >
-        <Button className="placement-left-bottom">left-bottom</Button>
+        <Button style={styles.placementLeftBottom}>left-bottom</Button>
       </Tooltip>
       <Tooltip content="这是Tooltip内容" placement="right" showArrow destroyOnHide>
-        <Button className="placement-right">right</Button>
+        <Button style={styles.placementRight}>right</Button>
       </Tooltip>
       <Tooltip
         content="这是Tooltip内容 right-top"
@@ -53,7 +121,7 @@ export default function Placements() {
         showArrow
         destroyOnHide
       >
-        <Button className="placement-right-top">right-top</Button>
+        <Button style={styles.placementRightTop}>right-top</Button>
       </Tooltip>
       <Tooltip
         content="这是Tooltip内容 right-bottom"
@@ -62,7 +130,7 @@ export default function Placements() {
         showArrow
         destroyOnHide
       >
-        <Button className="placement-right-bottom">right-bottom</Button>
+        <Button style={styles.placementRightBottom}>right-bottom</Button>
       </Tooltip>
     </div>
   );
