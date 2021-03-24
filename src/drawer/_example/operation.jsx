9e22eab2e6f32fd39afdc9bd8f809fa573b77bab
HQ-Lin <a154571896@gmail.com>
2021-03-24 14:23:23 +0800

    feat: 规范文档

---
+++ src/drawer/_example/operation.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Drawer, Button, Input } from '@tencent/tdesign-react';
+
+export default function () {
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    setVisible(true);
+  };
+  const handleClose = () => {
+    setVisible(false);
+  };
+  return (
+    <div>
+      <Button theme="primary" onClick={handleClick}>
+        Open
+      </Button>
+      <Drawer title="Drawer" visible={visible} onClose={handleClose}>
+        <div>
+          <span>Label A</span>
+          <Input />
+        </div>
+        <div>
+          <span>Label b</span>
+          <Input />
+        </div>
+      </Drawer>
+    </div>
+  );
+}
