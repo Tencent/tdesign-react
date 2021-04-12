d448361a470393f73c10976593a4ad90bf71a320
chrrg <caohongchrrg@qq.com>
2021-04-12 10:41:33 +0800

    feat(timepicker): timepicker fix add key control, display demo

--- src/time-picker/_example/keyboard.jsx
+++ src/time-picker/_example/keyboard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { TimePicker } from '@tencent/tdesign-react';
 
 export default function TimePickerExample() {
-  const [value, setValue] = useState('14:23');
+  const [value, setValue] = useState(() => new Date());
   return (
     <div className="tdesign-demo-block">
       <TimePicker value={value} onChange={setValue} format="hh:mm:ss" timeType="time" allowInput />
