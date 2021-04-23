1980bde228dac570841589fc7f03079aece58457
HQ-Lin <a154571896@gmail.com>
2021-04-23 16:06:57 +0800

    fix: demo error

--- src/time-picker/_example/disabled.jsx
+++ src/time-picker/_example/disabled.jsx
@@ -5,13 +5,7 @@ export default function TimePickerExample() {
   const [value, setValue] = useState('16:47');
   return (
     <div className="tdesign-demo-block">
-      <TimePicker
-        value={value}
-        onChange={setValue}
-        format="hh:mm:ss"
-        timeType="time"
-        disabled
-      />
+      <TimePicker value={value} onChange={setValue} format="hh:mm:ss" timeType="time" disabled />
     </div>
   );
 }
--- src/time-picker/_example/range.jsx
+++ src/time-picker/_example/range.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { TimePicker } from '@tencent/tdesign-react';
-import noop from '../../../es/_util/noop';
 
 export default function TimePickerExample() {
   return (
     <div className="tdesign-demo-block">
-      <TimePicker value={['00:00:00', '23:59:59']} onChange={noop} format="HH:mm:ss" timeType="timeRange" />
+      <TimePicker value={['00:00:00', '23:59:59']} onChange={(e) => console.log(e)} format="HH:mm:ss" />
     </div>
   );
 }
