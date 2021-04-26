b174bf393439eb074cff861ad6a44858cf5153e5
HQ-Lin <a154571896@gmail.com>
2021-04-26 14:55:34 +0800

    feat: 调整 Checkbox 组件 api (merge request !140)
    Squash merge branch 'feat/api/checkbox' into 'develop'
    
    
    * feat: 调整 Checkbox 组件 api

--- src/checkbox/_example/link.jsx
+++ src/checkbox/_example/link.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import { Checkbox, Button } from '@tencent/tdesign-react';
 
 export default function CheckboxControlledExample() {
-  const [value, setValue] = useState(false);
+  const [checked, setChecked] = useState(false);
   return (
     <>
-      <Checkbox value={value}>腾讯云A</Checkbox>
-      <Checkbox value={value}>腾讯云B</Checkbox>
-      <Checkbox value={value}>腾讯云C</Checkbox>
+      <Checkbox checked={checked}>腾讯云A</Checkbox>
+      <Checkbox checked={checked}>腾讯云B</Checkbox>
+      <Checkbox checked={checked}>腾讯云C</Checkbox>
 
       <div style={{ margin: '16px 0' }}>
-        <Button onClick={() => setValue(false)}>删除</Button>
-        <Button style={{ marginLeft: 16 }} onClick={() => setValue(true)}>
+        <Button onClick={() => setChecked(false)}>删除</Button>
+        <Button style={{ marginLeft: 16 }} onClick={() => setChecked(true)}>
           开启
         </Button>
       </div>
