6191cc32a71672b445ec1ee0489c4e222c0a496e
HQ-Lin <a154571896@gmail.com>
2021-04-30 17:58:34 +0800

    feat: 调整 Loading 组件 Api (merge request !145)
    Squash merge branch 'feat/api/loading' into 'develop'
    
    * feat: 调整 Loading 组件 Api

--- src/loading/loading.tsx
+++ src/loading/loading.tsx
@@ -4,64 +4,9 @@ import { addClass, removeClass } from '../_util/dom';
 import useConfig from '../_util/useConfig';
 import { StyledProps } from '../_type';
 import { LoadingIcon } from '../icon';
+import { TdLoadingProps } from '../_type/components/loading';
 
-enum SIZE {
-  LARGE = 'large',
-  MIDDLE = 'middle',
-  SMALL = 'small',
-}
-export interface LoadingProps extends StyledProps {
-  /**
-   * 加载指示符（支持function、slot、reactNode。不支持string）
-   *
-   *  @default 圆圈图标
-   */
-  indicator?: Function | string;
-  /**
-   * 当作为包裹元素时，自定义的加载提示文案
-   */
-  text?: string;
-  /**
-   * 是否为加载中状态
-   *
-   *  @default true
-   */
-  loading: boolean;
-  /**
-   * 加载组件大小，包括图标和文字。可选值为 large | middle | small
-   *
-   *  @default 'middle'
-   */
-  size?: SIZE;
-  /**
-   * 延迟显示加载效果的时间（防止闪烁）  毫秒
-   *
-   */
-  delay?: number;
-  /**
-   * 是否全屏遮罩，遮罩会插入至 body 上
-   *
-   *  @default false
-   */
-  fullscreen?: boolean;
-  /**
-   * 是否需要锁定屏幕的滚动
-   *
-   *  @default false
-   */
-  preventScrollThrough?: boolean;
-  /**
-   * 包裹器的自定义类名
-   */
-  className?: string;
-  /**
-   * 是否需要遮罩层
-   *
-   *  @default true
-   */
-  showOverlay?: boolean;
-  children?: HTMLSpanElement[];
-}
+export interface LoadingProps extends TdLoadingProps, StyledProps {}
 
 /**
  * Loading组件
