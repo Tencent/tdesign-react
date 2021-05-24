a59b21c48cb28707b64869bc8588343bde0c543d
v_pengphe <v_pengphe>
2021-05-24 10:01:40 +0800

    docs(anchor): 演示案例 href 修改

--- src/anchor/_example/sizeLarge.jsx
+++ src/anchor/_example/sizeLarge.jsx
@@ -3,15 +3,15 @@ import { Anchor, AnchorItem } from '@tencent/tdesign-react';
 
 export default function AnchorSizeLarge() {
   return (
-    <div id="/components/anchor/#size-large">
+    <div id="size-large">
       <Anchor bounds={100} targetOffset={50}>
-        <AnchorItem href="/#/components/anchor/#default" title="基础锚点" />
-        <AnchorItem href="/#/components/anchor/#multiple" title="多级锚点" />
-        <AnchorItem href="/#/components/anchor/#size" title="尺寸大小">
-          <AnchorItem href="/#/components/anchor/#size" title="size-medium"></AnchorItem>
-          <AnchorItem href="/#/components/anchor/#size-large" title="size-large"></AnchorItem>
+        <AnchorItem href="#default" title="基础锚点" />
+        <AnchorItem href="#multiple" title="多级锚点" />
+        <AnchorItem href="#size" title="尺寸大小">
+          <AnchorItem href="#size" title="size-medium"></AnchorItem>
+          <AnchorItem href="#size-large" title="size-large"></AnchorItem>
         </AnchorItem>
-        <AnchorItem href="/#/components/anchor/#attach" title="指定容器" />
+        <AnchorItem href="#attach" title="指定容器" />
       </Anchor>
     </div>
   );
