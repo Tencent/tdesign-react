5ba995334c1d38515a5d1a8917ec595ee973a242
xiaosansiji <799552341@qq.com>
2021-06-07 16:19:38 +0800

    feat: 增加 input-number 默认导出

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { default as Button } from './button';
 export { default as Checkbox } from './checkbox';
 export { default as Input } from './input';
 export { default as InputGroup } from './input-group';
+export { default as InputNumber } from './input-number';
 export { default as ConfigProvider } from './config-provider';
 export * from './icon';
 export { default as Popup } from './popup';
