5a252603ce08f510594b775834c54601c460a9b2
andyjxli <CuteBlackCat9@gmail.com>
2021-07-13 21:59:25 +0800

    fix: react menu style

--- src/menu/SubMenu.tsx
+++ src/menu/SubMenu.tsx
@@ -1,13 +1,17 @@
-import React, { FC, useContext, useState, ReactElement } from 'react';
+import React, { FC, useContext, useState, ReactElement, useMemo } from 'react';
 import classNames from 'classnames';
-import ChevronDownIcon from '../icon/icons/ChevronDownIcon';
-import ChevronRightIcon from '../icon/icons/ChevronRightIcon';
 import { StyledProps } from '../_type';
 import { TdSubmenuProps } from '../_type/components/menu';
 import useConfig from '../_util/useConfig';
 import { MenuContext } from '../menu/MenuContext';
+import { getSubMenuMaxHeight } from './_util/getSubMenuChildStyle';
 import checkSubMenuChildrenActive from './_util/checkSubMenuChildrenActive';
-import { getSubMenuMaxHight } from './_util/getSubMenuMaxHight';
+
+const ArrawSvg = ({ className }: { className: string }) => (
+  <svg className={className} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3.75 5.7998L7.99274 10.0425L12.2361 5.79921" stroke="black" strokeOpacity="0.9" strokeWidth="1.3" />
+  </svg>
+);
 
 export interface SubMenuProps extends TdSubmenuProps, StyledProps {}
 
@@ -33,8 +37,6 @@ const SubAccordion: FC<SubMenuProps> = (props) => {
     else if (type === 'leave') setOpen(false);
   };
 
-  const Icon = isPopUp ? ChevronRightIcon : ChevronDownIcon;
-
   const childrens = React.Children.map(children, (child) => React.cloneElement(child as ReactElement, {
     className: classNames(
       `${classPrefix}-menu__item--plain`,
@@ -44,12 +46,22 @@ const SubAccordion: FC<SubMenuProps> = (props) => {
   }));
 
   // 计算有多少子节点并设置最大高度，为做出动画效果
-  const childStyle = { maxHeight: isExpand || (open && isPopUp) ? getSubMenuMaxHight(children) * 50 : 0 };
+  const childStyle = {
+    maxHeight: isExpand || (open && isPopUp) ? getSubMenuMaxHeight(children) : 0,
+  };
+
+  // 是否展开（popup 与 expand 两种状态）
+  const isOpen = useMemo(() => {
+    if (disabled) return false;
+    if (isPopUp) return open;
+    return isExpand;
+  }, [disabled, isPopUp, open, isExpand]);
 
   return (
     <li
       className={classNames(className, `${classPrefix}-submenu`, {
         [`${classPrefix}-is-disabled`]: disabled,
+        [`${classPrefix}-is-opened`]: isOpen,
       })}
       onClick={handleClick}
       style={style}
@@ -58,23 +70,33 @@ const SubAccordion: FC<SubMenuProps> = (props) => {
     >
       <div
         className={classNames(`${classPrefix}-menu__item`, {
-          [`${classPrefix}-is-opened`]: isExpand && !disabled,
+          [`${classPrefix}-is-opened`]: isOpen,
           [`${classPrefix}-is-active`]: checkSubMenuChildrenActive(children, active),
         })}
       >
         {icon} <span className={`${classPrefix}-menu__content`}>{title}</span>
-        <Icon className={`${classPrefix}-submenu-icon`} />
+        <ArrawSvg
+          className={classNames(`${classPrefix}-fake-arrow`, {
+            [`${classPrefix}-fake-arrow--active`]: isOpen,
+          })}
+        />
       </div>
       {isPopUp ? (
-        <ul
-          className={classNames(`${classPrefix}-menu__popup`, `${classPrefix}-menu__popup-scale`, {
-            [`${classPrefix}-is-opened`]: isPopUp && open,
+        <div
+          className={classNames(`${classPrefix}-menu__popup`, `${classPrefix}-is-vertical`, {
+            [`${classPrefix}-is-opened`]: isOpen,
           })}
-          key="popup"
-          style={childStyle}
         >
-          {childrens}
-        </ul>
+          <ul
+            className={classNames(`${classPrefix}-menu__popup-wrapper`, {
+              [`${classPrefix}-is-opened`]: isOpen,
+            })}
+            key="popup"
+            style={childStyle}
+          >
+            {childrens}
+          </ul>
+        </div>
       ) : (
         <ul key="normal" style={childStyle} className={`${classPrefix}-menu__sub`}>
           {childrens}
@@ -104,34 +126,41 @@ const SubTitleMenu: FC<SubMenuProps> = (props) => {
   };
 
   return (
-    <>
-      <li
+    <li
+      className={classNames(`${classPrefix}-submenu`, {
+        [`${classPrefix}-is-opened`]: open,
+      })}
+      onMouseEnter={() => handleMouseEvent('enter')}
+      onMouseLeave={() => handleMouseEvent('leave')}
+    >
+      <div
         className={classNames(className, `${classPrefix}-menu__item`, {
           [`${classPrefix}-is-active`]: isActive,
+          [`${classPrefix}-is-opened`]: open,
         })}
         onClick={handleClick}
         style={style}
-        onMouseEnter={() => handleMouseEvent('enter')}
-        onMouseLeave={() => handleMouseEvent('leave')}
       >
         <span>{title}</span>
-        {isPopUp && <ChevronDownIcon className={`${classPrefix}-submenu-icon`} />}
         {isPopUp && (
-          <ul
-            className={classNames(
-              `${classPrefix}-menu__popup`,
-              `${classPrefix}-menu__popup-scale`,
-              `${classPrefix}-menu__popup-inner`,
-              {
-                [`${classPrefix}-is-opened`]: open,
-              },
-            )}
-          >
-            {children}
-          </ul>
+          <ArrawSvg
+            className={classNames(`${classPrefix}-fake-arrow`, {
+              [`${classPrefix}-fake-arrow--active`]: open,
+            })}
+          />
         )}
-      </li>
-    </>
+      </div>
+      {isPopUp && (
+        <div
+          className={classNames(`${classPrefix}-menu__popup`, {
+            [`${classPrefix}-is-opened`]: open,
+          })}
+          style={{ '--popup-max-height': getSubMenuMaxHeight(children) } as any}
+        >
+          <ul className={classNames(`${classPrefix}-menu__popup-wrapper`)}>{children}</ul>
+        </div>
+      )}
+    </li>
   );
 };
 
--- src/menu/_example/multi-side.jsx
+++ src/menu/_example/multi-side.jsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import React, { useState } from 'react';
-import { Menu, SubMenu, MenuItem, UserIcon, InternetIcon } from '@tencent/tdesign-react';
+import { Menu, SubMenu, MenuItem, AppIcon, CodeIcon, FileIcon, UserIcon } from '@tencent/tdesign-react';
 
 function MultiSide() {
   const [active, setActive] = useState('0');
@@ -16,12 +16,10 @@ function MultiSide() {
       width={265}
       onChange={(v) => setActive(v)}
       logo={
-        collapsed ? null : (
-          <img src="https://www.tencent.com/img/index/menu_logo_hover.png" width="136" alt="logo" />
-        )
+        collapsed ? null : <img src="https://www.tencent.com/img/index/menu_logo_hover.png" width="136" alt="logo" />
       }
     >
-      <SubMenu value="0" title={<span>菜单1</span>} icon={<UserIcon />}>
+      <SubMenu value="0" title="仪表盘" icon={<AppIcon />}>
         <MenuItem value="0-1">
           <span>子菜单1</span>
         </MenuItem>
@@ -29,24 +27,27 @@ function MultiSide() {
           <span>子菜单2</span>
         </MenuItem>
       </SubMenu>
-      <MenuItem value="7" disabled>
-        <span>菜单二</span>
-      </MenuItem>
-      <SubMenu value="sub-2" title={<span>菜单三</span>} icon={<UserIcon />}>
-        <SubMenu value="sub-3" title={<span>二级菜单-1</span>} icon={<UserIcon />}>
-          <MenuItem value="5">
-            <span>三级菜单-1</span>
-          </MenuItem>
-        </SubMenu>
-        <SubMenu value="sub-4" title={<span>二级菜单-2</span>} icon={<InternetIcon />}>
-          <MenuItem value="6">
-            <span>三级菜单-2</span>
-          </MenuItem>
-        </SubMenu>
+      <SubMenu value="1" title={<span>菜单1</span>} icon={<CodeIcon />}>
+        <MenuItem value="1-1" disabled>
+          <span>菜单二</span>
+        </MenuItem>
       </SubMenu>
-      <MenuItem value="9">
-        <span>菜单四</span>
-      </MenuItem>
+      <SubMenu value="2" title={<span>菜单三</span>} icon={<FileIcon />}>
+        <MenuItem value="2-1">
+          <span>三级菜单-1</span>
+        </MenuItem>
+        <MenuItem value="2-2">
+          <span>三级菜单-2</span>
+        </MenuItem>
+      </SubMenu>
+      <SubMenu value="3" title={<span>菜单三</span>} icon={<UserIcon />}>
+        <MenuItem value="3-1">
+          <span>三级菜单-1</span>
+        </MenuItem>
+        <MenuItem value="3-2">
+          <span>三级菜单-2</span>
+        </MenuItem>
+      </SubMenu>
     </Menu>
   );
 }
