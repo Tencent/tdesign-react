bd06198a9b73938ed0a2d92271f779b312c774e5
wookaoer <wuaocarl@gmail.com>
2021-07-23 18:00:52 +0800

    fix/menu-operation (merge request !192)
    Squash merge branch 'fix/menu-operation' into 'develop'
    
    * fix: 更新snapshot
    
    * Merge remote-tracking branch 'origin/develop' into fix/menu-operations
    
    * fix(menu): 修复operations参数实现不一致问题 & 更新api文档并去除废弃API  #95

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import classNames from 'classnames';
-import ViewListIcon from '../icon/icons/ViewListIcon';
 import useConfig from '../_util/useConfig';
 import { StyledProps } from '../_type';
 import { TdMenuProps } from '../_type/components/menu';
@@ -10,14 +9,14 @@ import useMenuContext from './hooks/useMenuContext';
 export interface MenuProps extends TdMenuProps, StyledProps {}
 
 const Menu: FC<MenuProps> = (props) => {
-  const { className, width = DEFAULT_MENU_WIDTH, children, theme, logo } = props;
+  const { className, width = DEFAULT_MENU_WIDTH, children, theme, logo, operations } = props;
   const { classPrefix } = useConfig();
   const { value } = useMenuContext({ ...props, children, mode: 'accordion' });
 
   // 菜单宽度
   const menuWidthArr = Array.isArray(width) ? width : [width, DEFAULT_MENU_WIDTH[1]];
 
-  const { collapsed, onCollapsed } = value;
+  const { collapsed } = value;
 
   return (
     <MenuContext.Provider value={value}>
@@ -32,12 +31,7 @@ const Menu: FC<MenuProps> = (props) => {
           {logo && <div className={`${classPrefix}-menu__logo`}>{logo}</div>}
           <ul className={`${classPrefix}-menu`}>{children}</ul>
         </div>
-        <div className={`${classPrefix}-menu__options`}>
-          <ViewListIcon
-            className={`${classPrefix}-collapsed-icon`}
-            onClick={(e: React.MouseEvent<SVGSVGElement, MouseEvent>) => onCollapsed({ collapsed: !collapsed, e })}
-          />
-        </div>
+        {operations && <div className={`${classPrefix}-menu__options`}>{operations}</div>}
       </div>
     </MenuContext.Provider>
   );
