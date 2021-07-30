bd7c3fd768de086705fa51884ed99868d45dd479
HQ-Lin <a154571896@gmail.com>
2021-07-30 10:31:04 +0800

    fix: confict

--- src/steps/index.ts
+++ src/steps/index.ts
@@ -1,10 +1,8 @@
 import Steps from './Steps';
-import StepItem from './StepItem';
 
 import './style/index.js';
 
 export type { StepsProps } from './Steps';
 export type { StepItemProps } from './StepItem';
 
-export { StepItem };
 export default Steps;
