19febd2e53bd33ee670ab471633875a603dca6cf
uyarn <uyarnchen@gmail.com>
2021-08-11 14:24:30 +0800

    chore: eslint-config-prettier

--- src/menu/_util/checkSubMenuActive.ts
+++ src/menu/_util/checkSubMenuActive.ts
@@ -3,8 +3,10 @@ import { MenuValue } from '../../_type/components/menu/index';
 import { checkIsSubMenu } from './checkMenuType';
 import checkSubMenuChildrenActive from './checkSubMenuChildrenActive';
 
-const checkSubMenuActive = (children: React.ReactNode, active: MenuValue): ReactElement => React.Children.toArray(children).find(
-  (child: React.ReactElement) => checkIsSubMenu(child)
-      && (child.props.value === active || checkSubMenuChildrenActive(child.props.children, active)),
-) as ReactElement;
+const checkSubMenuActive = (children: React.ReactNode, active: MenuValue): ReactElement =>
+  React.Children.toArray(children).find(
+    (child: React.ReactElement) =>
+      checkIsSubMenu(child) &&
+      (child.props.value === active || checkSubMenuChildrenActive(child.props.children, active)),
+  ) as ReactElement;
 export default checkSubMenuActive;
