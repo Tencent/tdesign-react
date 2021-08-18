4c2e41ebe07511b0973858d453c3c6a7875001d7
HQ-Lin <a154571896@gmail.com>
2021-08-18 11:16:30 +0800

    feat: 优化 layout demo (merge request !224)
    Squash merge branch 'fix/layout/demo' into 'develop'
    
    * feat: 优化 layout demo
    
    * feat: 优化 layout demo

--- src/layout/_example/base.jsx
+++ src/layout/_example/base.jsx
@@ -1,21 +1,5 @@
 import React from 'react';
-import { Layout, Menu } from '@tencent/tdesign-react';
-
-const { MenuItem } = Menu;
-
-const Logo = () => <span>LOGO</span>;
-
-function BasicUsage(props) {
-  return (
-    <Menu style={{ width: '100%', height: '100%', boxShadow: 'none' }} logo={<Logo />} {...props}>
-      <MenuItem name="1">侧边内容一</MenuItem>
-      <MenuItem name="2">侧边内容二</MenuItem>
-      <MenuItem name="3">侧边内容三</MenuItem>
-      <MenuItem name="4">侧边内容四</MenuItem>
-      <MenuItem name="5">侧边内容无</MenuItem>
-    </Menu>
-  );
-}
+import { Layout } from '@tencent/tdesign-react';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -25,24 +9,18 @@ export default function BasicDivider() {
       <h4>顶部导航布局</h4>
       <Layout>
         <Header>Header</Header>
-        <Content>
-          <div>Content</div>
-        </Content>
-        <Footer>Copyright @ 2019-2020 Tencent. All Rights Reserved</Footer>
+        <Content>Content</Content>
+        <Footer>Copyright @ 2019-2021 Tencent. All Rights Reserved</Footer>
       </Layout>
 
       <br />
 
       <h4>侧边导航布局</h4>
       <Layout>
-        <Sider>
-          <BasicUsage />
-        </Sider>
+        <Sider>Aside</Sider>
         <Layout>
-          <Content>
-            <div>Content</div>
-          </Content>
-          <Footer>Copyright @ 2019-2020 Tencent. All Rights Reserved</Footer>
+          <Content>Content</Content>
+          <Footer>Copyright @ 2019-2021 Tencent. All Rights Reserved</Footer>
         </Layout>
       </Layout>
 
@@ -52,14 +30,10 @@ export default function BasicDivider() {
       <Layout>
         <Header>Header</Header>
         <Layout>
-          <Sider>
-            <BasicUsage />
-          </Sider>
+          <Sider>Aside</Sider>
           <Layout>
-            <Content>
-              <div>Content</div>
-            </Content>
-            <Footer>Copyright @ 2019-2020 Tencent. All Rights Reserved</Footer>
+            <Content>Content</Content>
+            <Footer>Copyright @ 2019-2021 Tencent. All Rights Reserved</Footer>
           </Layout>
         </Layout>
       </Layout>
@@ -70,14 +44,10 @@ export default function BasicDivider() {
         <Header>Header</Header>
         <Layout>
           <Layout>
-            <Content>
-              <div>Content</div>
-            </Content>
-            <Footer>Copyright @ 2019-2020 Tencent. All Rights Reserved</Footer>
+            <Content>Content</Content>
+            <Footer>Copyright @ 2019-2021 Tencent. All Rights Reserved</Footer>
           </Layout>
-          <Sider>
-            <BasicUsage />
-          </Sider>
+          <Sider>Aside</Sider>
         </Layout>
       </Layout>
 
@@ -86,14 +56,10 @@ export default function BasicDivider() {
       <Layout>
         <Header>Header</Header>
         <Layout>
-          <Sider width={80}>
-            <BasicUsage collapsed={true} />
-          </Sider>
+          <Sider width={80}>Aside</Sider>
           <Layout>
-            <Content>
-              <div>Content</div>
-            </Content>
-            <Footer>Copyright @ 2019-2020 Tencent. All Rights Reserved</Footer>
+            <Content>Content</Content>
+            <Footer>Copyright @ 2019-2021 Tencent. All Rights Reserved</Footer>
           </Layout>
         </Layout>
       </Layout>
--- src/layout/_example/combine.jsx
+++ src/layout/_example/combine.jsx
@@ -8,11 +8,11 @@ const Logo = () => <span>LOGO</span>;
 function BasicUsage(props) {
   return (
     <Menu style={{ width: '100%', height: '100%', boxShadow: 'none' }} logo={<Logo />} {...props}>
-      <MenuItem name="1">侧边内容一</MenuItem>
-      <MenuItem name="2">侧边内容二</MenuItem>
-      <MenuItem name="3">侧边内容三</MenuItem>
-      <MenuItem name="4">侧边内容四</MenuItem>
-      <MenuItem name="5">侧边内容无</MenuItem>
+      <MenuItem value="1">侧边内容一</MenuItem>
+      <MenuItem value="2">侧边内容二</MenuItem>
+      <MenuItem value="3">侧边内容三</MenuItem>
+      <MenuItem value="4">侧边内容四</MenuItem>
+      <MenuItem value="5">侧边内容无</MenuItem>
     </Menu>
   );
 }
