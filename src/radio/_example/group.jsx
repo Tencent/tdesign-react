5a66bc2d039c990db74f32d19a7d31a80f9f718e
HQ-Lin <a154571896@gmail.com>
2021-08-26 11:27:08 +0800

    feat: 对齐 radio demo展示 (merge request !249)
    Squash merge branch 'feat/radio/demo' into 'develop'
    
    Co-author: HQ-Lin <a154571896@gmail.com>
    Reviewed-by: uyarn <uyarnchen@gmail.com>

--- src/radio/_example/group.jsx
+++ src/radio/_example/group.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Radio } from '@tencent/tdesign-react';
 
 export default function RadioExample() {
-  const [gender, setGender] = useState('male');
+  const [city, setCity] = useState('bj');
 
   return (
-    <Radio.Group value={gender} onChange={(value) => setGender(value)}>
-      <Radio value="male">男性</Radio>
-      <Radio value="female">女性</Radio>
+    <Radio.Group value={city} onChange={(value) => setCity(value)}>
+      <Radio value="bj">北京</Radio>
+      <Radio value="sh">上海</Radio>
+      <Radio value="gz">广州</Radio>
+      <Radio value="sz">深圳</Radio>
     </Radio.Group>
   );
 }
--- src/radio/_example/size.jsx
+++ src/radio/_example/size.jsx
@@ -7,27 +7,58 @@ export default function RadioExample() {
   const [gender3, setGender3] = useState('gz2');
 
   return (
-    <>
-      <Radio.Group size="small" value={gender1} onChange={(value) => setGender1(value)}>
-        <Radio.Button value="bj">北京</Radio.Button>
-        <Radio.Button value="sh">上海</Radio.Button>
-        <Radio.Button value="gz">广州</Radio.Button>
-        <Radio.Button value="sz">深圳</Radio.Button>
-      </Radio.Group>
-      <br />
-      <Radio.Group value={gender2} onChange={(value) => setGender2(value)}>
-        <Radio.Button value="bj1">北京</Radio.Button>
-        <Radio.Button value="sh1">上海</Radio.Button>
-        <Radio.Button value="gz1">广州</Radio.Button>
-        <Radio.Button value="sz1">深圳</Radio.Button>
-      </Radio.Group>
-      <br />
-      <Radio.Group size="large" value={gender3} onChange={(value) => setGender3(value)}>
-        <Radio.Button value="bj2">北京</Radio.Button>
-        <Radio.Button value="sh2">上海</Radio.Button>
-        <Radio.Button value="gz2">广州</Radio.Button>
-        <Radio.Button value="sz2">深圳</Radio.Button>
-      </Radio.Group>
-    </>
+    <div className="tdesign-demo-block-row">
+      <div className="tdesign-demo-block-column">
+        <Radio.Group size="small" value={gender1} onChange={(value) => setGender1(value)}>
+          <Radio.Button value="bj">北京</Radio.Button>
+          <Radio.Button value="sh">上海</Radio.Button>
+          <Radio.Button value="gz">广州</Radio.Button>
+          <Radio.Button value="sz">深圳</Radio.Button>
+        </Radio.Group>
+
+        <Radio.Group value={gender2} onChange={(value) => setGender2(value)}>
+          <Radio.Button value="bj1">北京</Radio.Button>
+          <Radio.Button value="sh1">上海</Radio.Button>
+          <Radio.Button value="gz1">广州</Radio.Button>
+          <Radio.Button value="sz1">深圳</Radio.Button>
+        </Radio.Group>
+
+        <Radio.Group size="large" value={gender3} onChange={(value) => setGender3(value)}>
+          <Radio.Button value="bj2">北京</Radio.Button>
+          <Radio.Button value="sh2">上海</Radio.Button>
+          <Radio.Button value="gz2">广州</Radio.Button>
+          <Radio.Button value="sz2">深圳</Radio.Button>
+        </Radio.Group>
+      </div>
+
+      <div className="tdesign-demo-block-column">
+        <div>
+          <Radio.Group buttonStyle="solid" size="small" value={gender1} onChange={(value) => setGender1(value)}>
+            <Radio.Button value="bj">北京</Radio.Button>
+            <Radio.Button value="sh">上海</Radio.Button>
+            <Radio.Button value="gz">广州</Radio.Button>
+            <Radio.Button value="sz">深圳</Radio.Button>
+          </Radio.Group>
+        </div>
+
+        <div>
+          <Radio.Group buttonStyle="solid" value={gender2} onChange={(value) => setGender2(value)}>
+            <Radio.Button value="bj1">北京</Radio.Button>
+            <Radio.Button value="sh1">上海</Radio.Button>
+            <Radio.Button value="gz1">广州</Radio.Button>
+            <Radio.Button value="sz1">深圳</Radio.Button>
+          </Radio.Group>
+        </div>
+
+        <div>
+          <Radio.Group buttonStyle="solid" size="large" value={gender3} onChange={(value) => setGender3(value)}>
+            <Radio.Button value="bj2">北京</Radio.Button>
+            <Radio.Button value="sh2">上海</Radio.Button>
+            <Radio.Button value="gz2">广州</Radio.Button>
+            <Radio.Button value="sz2">深圳</Radio.Button>
+          </Radio.Group>
+        </div>
+      </div>
+    </div>
   );
 }
