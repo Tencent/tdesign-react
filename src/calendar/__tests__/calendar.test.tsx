eb1796ec524b9e7b5796497e1c4d39ceef66ce55
uyarn <uyarnchen@gmail.com>
2021-08-30 13:35:06 +0800

    chore: use mock date to fix the calendar date in jest

--- src/calendar/__tests__/calendar.test.tsx
+++ src/calendar/__tests__/calendar.test.tsx
@@ -1,4 +1,8 @@
 import { testExamples } from '@test/utils';
+import MockDate from 'mockdate';
+
+// 固定时间，当使用 new Date() 时，返回固定时间，防止“当前时间”的副作用影响，导致 snapshot 变更，mockdate 插件见 https://github.com/boblauer/MockDate
+MockDate.set('2021-08-27');
 
 // 测试组件代码 Example 快照
 testExamples(__dirname);
