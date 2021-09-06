865efcd75409cadf972d543241cd83692a773273
uyarn <uyarnchen@gmail.com>
2021-09-06 16:16:02 +0800

    feat(notification): notification API调整为数组格式

--- src/notification/_example/icon.jsx
+++ src/notification/_example/icon.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
-import { Notification, IconFont } from '@tencent/tdesign-react';
+import { Notification } from '@tencent/tdesign-react';
 
 export default function NotificationExample() {
-  return <Notification title="自定义icon" content="这是一条消息通知" icon={<IconFont name="more" />} />;
+  return (
+    <>
+      <div style={{ marginBottom: '16px' }}>
+        <Notification
+          theme="info"
+          title="普通通知"
+          content="这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知"
+        />
+      </div>
+      <div style={{ marginBottom: '16px' }}>
+        <Notification
+          theme="error"
+          title="危险通知"
+          content="这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知"
+        />
+      </div>
+      <div style={{ marginBottom: '16px' }}>
+        <Notification
+          theme="warning"
+          title="告警通知"
+          content="这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知"
+        />
+      </div>
+      <div style={{ marginBottom: '16px' }}>
+        <Notification
+          theme="success"
+          title="成功通知"
+          content="这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知这是一条消息通知"
+        />
+      </div>
+    </>
+  );
 }
