57cfcdf5c1c7f4c30d221b3329a26336630e56fc
yunfeic <chenyunfeijia@gmail.com>
2021-09-14 15:26:10 +0800

    feat/table-merge-cells (merge request !288)
    Squash merge branch 'feat/table-merge-cells' into 'develop'
    #### 修改点简述
    Table支持合并单元格功能，和vue保持一致
    
    实现相关api: TdBaseTableProps中rowspanAndColspan
    
    相关demo: 合并单元格

--- src/table/base/TableBody.tsx
+++ src/table/base/TableBody.tsx
@@ -1,13 +1,20 @@
 import React, { forwardRef } from 'react';
+import isFunction from 'lodash/isFunction';
 import { TdBaseTableProps } from '../../_type/components/table';
 import useConfig from '../../_util/useConfig';
 import getRowKeyFromRowKey from '../util/getRowKeyFromRowKey';
 
 import TableRow from './TableRow';
 
+export interface RowSkipTdSpanColIndexsMap {
+  [key: number]: number[];
+}
+
 const TableBody = forwardRef((props: TdBaseTableProps, ref: React.Ref<HTMLTableSectionElement>): any => {
   const { classPrefix } = useConfig();
-  const { data = [], rowKey, rowClassName } = props;
+  const { data = [], rowKey, rowClassName, rowspanAndColspan } = props;
+  const rowSkipTdSpanColIndexsMap = {}; // 引用，不可重置。eg: { 0: [1, 3] } 表示第1行，第2、4列两个cell不渲染
+  const isRowspanAndColspanFn = isFunction(rowspanAndColspan);
 
   // 键值的获取方式
   const getRowKey = getRowKeyFromRowKey(rowKey);
@@ -15,7 +22,23 @@ const TableBody = forwardRef((props: TdBaseTableProps, ref: React.Ref<HTMLTableS
   // ==================== render ====================
   const rows = data.map((record, index) => {
     const rowKey = getRowKey(record, index);
-    return <TableRow record={record} rowIndex={index} key={rowKey} rowClassName={rowClassName} />;
+
+    return (
+      <TableRow
+        record={record}
+        rowIndex={index}
+        key={rowKey}
+        rowClassName={rowClassName}
+        {...(isRowspanAndColspanFn
+          ? {
+              isRowspanAndColspanFn,
+              rowspanAndColspan,
+              rowSkipTdSpanColIndexsMap,
+              dataLength: data.length,
+            }
+          : {})}
+      />
+    );
   });
 
   return (
