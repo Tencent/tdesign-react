86aa27d3c5330790b1e054f136380876e5ec7268
psaren <18718574603@163.com>
2021-09-27 15:49:57 +0800

    fix: #149

--- src/radio/_example/group.jsx
+++ src/radio/_example/group.jsx
@@ -3,13 +3,44 @@ import { Radio } from '@tencent/tdesign-react';
 
 export default function RadioExample() {
   const [city, setCity] = useState('bj');
-
+  const [city2, setCity2] = useState('sz');
+  const [city3, setCity3] = useState('深圳');
+  const objOptions = [
+    {
+      value: 'bj',
+      label: '北京',
+    },
+    {
+      value: 'sh',
+      label: '上海',
+    },
+    {
+      value: 'gz',
+      label: '广州',
+      disabled: true,
+    },
+    {
+      value: 'sz',
+      label: '深圳',
+    },
+  ];
+  const itemOptions = ['北京', '上海', '广州', '深圳'];
   return (
-    <Radio.Group value={city} onChange={(value) => setCity(value)}>
-      <Radio value="bj">北京</Radio>
-      <Radio value="sh">上海</Radio>
-      <Radio value="gz">广州</Radio>
-      <Radio value="sz">深圳</Radio>
-    </Radio.Group>
+    <>
+      <div>
+        <Radio.Group value={city} onChange={setCity}>
+          <Radio value="bj">北京</Radio>
+          <Radio value="sh">上海</Radio>
+          <Radio value="gz">广州</Radio>
+          <Radio value="sz">深圳</Radio>
+        </Radio.Group>
+      </div>
+      <div>
+        <Radio.Group value={city2} options={objOptions} onChange={setCity2} />
+      </div>
+      <div>
+        <Radio.Group value={city3} options={itemOptions} onChange={setCity3} />
+      </div>
+    </>
   );
 }
