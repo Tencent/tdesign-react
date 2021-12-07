558e4cf0f2add2edf48ed062a0a46bd0b6b6e2bf
kenzyyang <827506072@qq.com>
2021-12-07 21:26:41 +0800

    feat: message 组件去除多余代码以修复组件自动关闭时控制态的异常和 onDurationEnd 事件执行两次的 bug (#22)

--- src/message/MessageComponent.tsx
+++ src/message/MessageComponent.tsx
@@ -3,7 +3,7 @@
  * @date 2021-05-11 19:25:08
  * @desc Message 纯组件实现.(Message.info Message.close 等函数用法封装在 message.tsx)
  */
-import React, { useState } from 'react';
+import React from 'react';
 
 import classNames from 'classnames';
 import { TdMessageProps } from './type';
@@ -23,28 +23,10 @@ interface MessageComponentProps extends TdMessageProps {
 const MessageComponent: React.FC<MessageComponentProps> = (props) => {
   // 样式相关变量和函数
   const { tdMessagePrefix, tdClassIsGenerator } = useMessageClass();
-  const [mount, setMount] = useState(true);
 
-  const {
-    theme = 'info',
-    children,
-    closeBtn,
-    onCloseBtnClick = noop,
-    style,
-    icon,
-    content,
-    onDurationEnd = noop,
-    duration = 3000,
-  } = props;
+  const { theme = 'info', children, closeBtn, onCloseBtnClick = noop, style, icon, content } = props;
 
-  if (duration > 0) {
-    setTimeout(() => {
-      setMount(false);
-      onDurationEnd();
-    }, duration);
-  }
-
-  return mount ? (
+  return (
     <div
       key="message"
       style={style}
@@ -58,7 +40,7 @@ const MessageComponent: React.FC<MessageComponentProps> = (props) => {
       {content ? content : children}
       <MessageClose {...props} />
     </div>
-  ) : null;
+  );
 };
 
 export default MessageComponent;
