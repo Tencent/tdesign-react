d62e864b8c0a373145c6c02bf3a1818b64b26cf4
xiaosansiji <xiaosansiji@gmail.com>
2021-12-18 18:36:17 +0800

    feat: remove useless props setting in demo

--- src/date-picker/_example/date-range.jsx
+++ src/date-picker/_example/date-range.jsx
@@ -4,8 +4,8 @@ import { DatePicker } from 'tdesign-react';
 export default function YearDatePicker() {
   return (
     <div className="tdesign-demo-item--datepicker tdesign-demo-block-column">
-      <DatePicker theme="primary" mode="date" range></DatePicker>
-      <DatePicker theme="primary" mode="date" enableTimePicker range></DatePicker>
+      <DatePicker mode="date" range></DatePicker>
+      <DatePicker mode="date" enableTimePicker range></DatePicker>
     </div>
   );
 }
