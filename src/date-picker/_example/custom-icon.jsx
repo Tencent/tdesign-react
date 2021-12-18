f061d2e0fa1af02d3f65b4a20d6d88c264407f28
xiaosansiji <xiaosansiji@gmail.com>
2021-12-18 18:07:20 +0800

    feat: add prefix & suffix icon demo  for datepicker

---
+++ src/date-picker/_example/custom-icon.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { DatePicker } from 'tdesign-react';
+import { BrowseIcon, LockOnIcon } from 'tdesign-icons-react';
+
+export default function YearDatePicker() {
+  function handleChange(value) {
+    console.log(value);
+  }
+
+  return (
+    <div className="tdesign-demo-item--datepicker">
+      <DatePicker
+        theme="primary"
+        prefixIcon={<BrowseIcon />}
+        suffixIcon={<LockOnIcon />}
+        mode="date"
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
