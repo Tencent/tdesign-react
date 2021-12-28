b2f09ebb55e8716610e6ef9c5c8b9f8f561bf9d8
Kyrielin <a154571896@gmail.com>
2021-12-28 13:27:57 +0800

    feat: 同步组件最新 api & 改动 AvatarGroup 使用方式 & 优化部分组件细节 (#100)
    * feat: 同步组件最新 api & 改动 AvatarGroup 使用方式 & 优化部分组件细节
    
    * fix: divider api
    
    * fix: markdown layout
    
    * chore: remove icon type
    
    * fix: 回退 swiper api
    
    * feat: update upload api
    
    Co-authored-by: Uyarn <uyarnchen@gmail.com>

--- src/affix/type.ts
+++ src/affix/type.ts
@@ -2,7 +2,7 @@
 
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-12-12 18:01:23
+ * updated at 2021-12-27 17:08:43
  * */
 
 import { ScrollContainer } from '../common';
--- src/popup/type.ts
+++ src/popup/type.ts
@@ -2,7 +2,7 @@
 
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-12-20 13:35:48
+ * updated at 2021-12-27 17:08:43
  * */
 
 import { TNode, ClassName, Styles, AttachNode } from '../common';
