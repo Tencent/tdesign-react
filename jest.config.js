551936ffa6a4e009b5d99ec1e8a744a450dc7a86
Kyrielin <a154571896@gmail.com>
2021-12-30 15:09:41 +0800

    feat: update jest & fix select test (#126)
    * feat: update jest & fix select test
    
    * fix: ts exclude

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,13 @@ process.env.TZ = 'Asia/Shanghai';
 module.exports = {
   rootDir: path.resolve(__dirname, './'),
   setupFilesAfterEnv: ['./test/setup-framework.js'],
+  testEnvironment: 'jsdom',
   preset: 'ts-jest',
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx?$': 'babel-jest',
+    '^.+\\.(j|t)sx?$': 'babel-jest',
     '^.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
-  transformIgnorePatterns: ['node_modules/?!(popper.js)'],
+  transformIgnorePatterns: ['node_modules'],
   verbose: false,
   moduleNameMapper: {
     // 测试工具便捷引入
