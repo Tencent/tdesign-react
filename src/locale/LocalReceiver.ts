cb8452043863e9417208a01b02fa260eec422dfa
iseekTo <1214046231@qq.com>
2021-12-31 17:58:02 +0800

    fix(type): improve the type of input and output of the useLocaleReceiver function

--- src/locale/LocalReceiver.ts
+++ src/locale/LocalReceiver.ts
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Locale, ComponentLocale, LocalRule } from './type';
+import { Locale } from './type';
 import { ConfigContext } from '../config-provider';
+
+import type { Config } from '../config-provider';
 
 export interface Placement {
   [propName: string]: string | number;
 }
 
-export type LocaleComponentName = Exclude<keyof Locale, 'locale'>;
-
-export function useLocaleReceiver<T extends LocaleComponentName>(
-  componentName: string,
+export function useLocaleReceiver<T extends keyof Config['locale']>(
+  componentName: T,
   defaultLocale?: Locale[T] | Function,
-): [Locale[T], Function] {
+) {
   const { locale: tdLocale } = React.useContext(ConfigContext);
 
-  function transformLocale(pattern: LocalRule<Placement>, placement?: Placement): string | Array<string> {
+  // @TODO: Check type of { pattern }
+  function transformLocale(pattern: Config['locale'][T], placement?: Placement): string | Array<string> {
     const REGX = /\{\s*([\w-]+)\s*\}/g;
 
     if (typeof pattern === 'string') {
@@ -24,7 +25,8 @@ export function useLocaleReceiver<T extends LocaleComponentName>(
         return '';
       });
       return translated;
-    } if (Array.isArray(pattern)) {
+    }
+    if (Array.isArray(pattern)) {
       return pattern.map((p, index) => {
         const translated = p.replace(REGX, (_: string, key: string) => {
           if (placement) return String(placement[index][key]);
@@ -32,21 +34,25 @@ export function useLocaleReceiver<T extends LocaleComponentName>(
         });
         return translated;
       });
-    } if (typeof pattern === 'function') {
+    }
+    if (typeof pattern === 'function') {
       return pattern(placement);
     }
     return '';
   }
 
-  const componentLocale: ComponentLocale = React.useMemo(() => {
+  /** @TypeA => 确保此参数是属于 tdLocale[componentName] 下的子属性 */
+  const componentLocale = React.useMemo<Config['locale'][T] | Function>(() => {
     const locale = defaultLocale || {};
-    const localeFromContext = componentName && tdLocale ? tdLocale[componentName] : {};
+    const connectLocaleByName = tdLocale[componentName];
+
+    const localeFromContext = componentName && tdLocale ? connectLocaleByName : {};
 
     return {
       ...(typeof locale === 'function' ? (locale as Function)() : locale),
-      ...(localeFromContext || {}),
+      ...((localeFromContext || {}) as typeof connectLocaleByName),
     };
   }, [componentName, defaultLocale, tdLocale]);
 
-  return [componentLocale, transformLocale];
+  return [componentLocale, transformLocale] as [Config['locale'][T], Function];
 }
