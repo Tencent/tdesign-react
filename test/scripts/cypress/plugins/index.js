a413f77f6e26b5ede09f70c372cf72b42c5a2f4a
carolin913 <carolin913@126.com>
2022-01-07 12:06:36 +0800

    chore: 更新测试覆盖率分组件，分unit,ssr,e2e；优化cypress调用方法

--- test/scripts/cypress/plugins/index.js
+++ test/scripts/cypress/plugins/index.js
@@ -16,27 +16,13 @@
  * @type {Cypress.PluginConfig}
  */
 
-const path = require("path");
-const { startDevServer } = require("@cypress/vite-dev-server");
-const codeCoverageTask = require("@cypress/code-coverage/task");
-const istanbul = require("vite-plugin-istanbul");
-
 module.exports = (on, config) => {
-  on("dev-server:start", (options) => startDevServer({
-    options,
-    viteConfig: {
-      logLevel: "warn",
-      configFile: path.resolve(__dirname, '../../../../site/vite.config.js'),
-      plugins: [istanbul({
-        include: ['src/**/_example/*.spec.*'],
-        extension: ['.js', '.ts', '.jsx', '.tsx'],
-        requireEnv: false,
-        cypress: true,
-      })],
-    },
-  })
-  );
-
-  codeCoverageTask(on, config);
+  // on('dev-server:start', (options) => {
+  //   return startDevServer({
+  //     options,
+  //     viteConfig,
+  //   })
+  // });
+  require('@cypress/code-coverage/task')(on, config)
   return config;
 };
