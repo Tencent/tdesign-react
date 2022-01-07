01802896e483a83d96adbd8963edbdf6b5ecf81a
lihuanIT <996203586@qq.com>
2022-01-07 16:58:09 +0800

    feat(generate-coverage): 合并子目录单侧百分比

--- test/scripts/generate-coverage.js
+++ test/scripts/generate-coverage.js
@@ -6,6 +6,8 @@ const DomParser = require('dom-parser');
 
 const parser = new DomParser();
 const result = {};
+const EXAMPLE_FILE = '/Example';
+const DECIMAL = 2;
 
 const resolveCwd = (...args) => {
   args.unshift(process.cwd());
@@ -36,14 +38,105 @@ const generateReportJson = async (filepath, type) => {
   }
 }
 
-exec('npm run test:coverage', async (err, stdout, stderr) => {
-  await Promise.all([])
-  const [resultunit = '{}', resulte2e = '{}', resultssr = '{}'] = await Promise.all([
+function calculate(start, end) {
+  try {
+    return `${(((start / end) || 0) * 100).toFixed(DECIMAL)}%`;
+  } catch (err) {
+    return '0%';
+  }
+}
+
+// 格式化处理value，四舍五入保留两位小数
+function formatValue(value) {
+  const [start, end] = value.split('/');
+  return calculate(start, end);
+}
+
+function formatCoverageResult(result) {
+  return result.map((coverageOld) => {
+    let coverage = coverageOld;
+
+    try {
+      coverage = JSON.parse(coverage);
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+
+    const statistics = {};
+
+    coverage = Object.entries(coverage).reduce((covs, [keyPath, value]) => {
+      const newCovs = covs;
+      newCovs[keyPath] = formatValue(value);
+
+      // EXAMPLE_FILE直接保留返回
+      if (keyPath.endsWith(EXAMPLE_FILE)) {
+        return newCovs;
+      }
+
+      const [root, sub] = keyPath.split('/');
+
+      // 根文件
+      if (!sub) {
+        statistics[keyPath] = value;
+        return newCovs;
+      }
+
+      const preValue = statistics[root];
+      // 根文件存在子目录
+      if (preValue) {
+        const [preStart, preEnd] = preValue.split('/').map(item => Number(item));
+        const [start, end] = value.split('/').map(item => Number(item));
+        statistics[root] = `${preStart + start} / ${preEnd + end}`;
+        newCovs[root] = calculate((preStart + start), (preEnd + end));
+        delete newCovs[keyPath];
+        return newCovs;
+      }
+
+      return newCovs;
+    }, {});
+
+    return JSON.stringify(coverage);
+  });
+}
+
+function getTypeName(result) {
+  let type = 'unit';
+  if (!result[0]) {
+    if (result[1]) {
+      type = 'e2e';
+    } else if (result[2]) {
+      type = 'ssr';
+    } else {
+      type = '';
+    }
+  }
+
+  return type;
+}
+
+const coverageExec = exec('npm run test:coverage', async () => {
+  let result = await Promise.all([
     generateReportJson('test/unit/coverage/index.html', 'unit'),
     generateReportJson('test/e2e/cy-report/coverage/lcov-report/index.html', 'e2e'),
     generateReportJson('test/ssr/coverage/index.html', 'ssr'),
   ]);
+
+  result = formatCoverageResult(result);
+  const [resultunit = '{}', resulte2e = '{}', resultssr = '{}'] = result;
   const finalRes = `module.exports = { unit: ${resultunit}, e2e: ${resulte2e}, ssr: ${resultssr}}`;
+
   fs.writeFileSync(resolveCwd('site/test-coverage.js'), finalRes);
-  console.log(`已成功生成${resultunit !== '{}' ? ' unit ' : resulte2e !== '{}' ? ' e2e ' : resultssr !== '{}' ? ' ssr ' : ''} 覆盖率报告，请于site/test-coverage.js查看`);
+
+  const type = getTypeName(result);
+  type && console.log(`已成功生成${type} 覆盖率报告，请于site/test-coverage.js查看`);
+});
+
+let data = 0;
+coverageExec.stdout.on('data', () => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  data += 1;
+
+  process.stdout.write(data % 2 ? 'loading...' : '...');
 });
