9e7677bb2fa90183137943780c5e64ade3cfbb0e
Shan <haishanb@163.com>
2022-01-10 20:20:09 +0800

    Divider组件完善 (#208)
    * feat: 新增Divider content属性实现,完善单测
    
    新增Divider content属性实现,完善单测
    
    * fix: 修复coverage输出格式
    
    修复coverage输出格式

--- src/divider/Divider.tsx
+++ src/divider/Divider.tsx
@@ -18,20 +18,36 @@ export interface DividerProps extends TdDividerProps, StyledProps {
  * 分割线组件
  */
 const Divider = (props: DividerProps) => {
-  const { layout = 'horizontal', dashed, align = 'center', className, style, children, ...otherDividerProps } = props;
+  const {
+    layout = 'horizontal',
+    dashed,
+    align = 'center',
+    className,
+    style,
+    children,
+    content,
+    ...otherDividerProps
+  } = props;
 
   const { classPrefix } = useConfig();
+  const hasChildren = typeof children !== 'undefined';
+  const hasContent = typeof content !== 'undefined';
+  let childrenNode = children;
+
+  if (!hasChildren && hasContent) {
+    childrenNode = content;
+  }
   const dividerClassNames = classNames(`${classPrefix}-divider`, className, {
     [`${classPrefix}-divider--horizontal`]: layout === 'horizontal',
     [`${classPrefix}-divider--vertical`]: layout === 'vertical',
     [`${classPrefix}-divider--dashed`]: !!dashed,
-    [`${classPrefix}-divider--with-text`]: !!children,
-    [`${classPrefix}-divider--with-text-${align}`]: !!children,
+    [`${classPrefix}-divider--with-text`]: !!childrenNode,
+    [`${classPrefix}-divider--with-text-${align}`]: !!childrenNode,
   });
 
   return (
     <div className={dividerClassNames} style={style} {...otherDividerProps}>
-      {children ? <span className={`${classPrefix}-divider__inner-text`}>{children}</span> : null}
+      {childrenNode ? <span className={`${classPrefix}-divider__inner-text`}>{childrenNode}</span> : null}
     </div>
   );
 };
