0be6b0c4bd8bfa8027c1b15211b61f6941ab095f
Shan <haishanb@163.com>
2022-01-17 17:10:03 +0800

    popconfirm组件demo同步,添加e2e单测 (#250)
    * feat(popconfirm): popconfirm组件demo同步,e2e单 测
    
    popconfirm组件demo同步,e2e单 测
    
    * feat(popconfirm): popconfirm组件demo同步,e2e单测
    
    popconfirm组件demo同步,e2e单测

--- src/popconfirm/_example/button.jsx
+++ src/popconfirm/_example/button.jsx
@@ -10,15 +10,15 @@ export default function ButtonExample() {
         confirmBtn={'确认提交'}
         cancelBtn={'我再想想'}
       >
-        <Button theme="primary">按钮样式（属性-字符串）</Button>
+        <Button theme="default" variant="outline">按钮样式（属性-字符串）</Button>
       </PopConfirm>
       <PopConfirm
         theme={'default'}
         content="您确定要提交吗"
-        confirmBtn={<Button theme={'primary'} size={'small'}>确定提交</Button>}
+        confirmBtn={<Button theme={'warning'} size={'small'}>确定提交</Button>}
         cancelBtn={<Button theme={'default'} size={'small'} variant={'outline'}>我再想想</Button>}
       >
-        <Button theme="primary">按钮样式（TNode）</Button>
+        <Button theme="default" variant="outline">按钮样式（TNode）</Button>
       </PopConfirm>
     </div>
   );
