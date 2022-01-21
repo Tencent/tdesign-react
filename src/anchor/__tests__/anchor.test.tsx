898f557499257b55182e9343722b0b0776bb42fa
samhou1988 <samhou1988@gmail.com>
2022-01-21 12:11:01 +0800

    Anchor component e2e and UT (#279)
    * test(anchor component): anchor e2e and ut
    
    * test(anchor component): anchor component small demo
    
    * test(test ssr): anchor demo test ssr

--- src/anchor/__tests__/anchor.test.tsx
+++ src/anchor/__tests__/anchor.test.tsx
@@ -1,4 +1,24 @@
-import { testExamples } from '@test/utils';
+import React from 'react';
+import { testExamples, render } from '@test/utils';
+import Anchor from '../Anchor';
+
+const { AnchorItem } = Anchor;
 
 // 测试组件代码 Example 快照
 testExamples(__dirname);
+
+describe('Anchor', () => {
+  const rootTestID = 'anchorRoot';
+  const childTestID = 'anchorChild';
+  it('title props', () => {
+    const el = (
+      <Anchor data-testid={rootTestID}>
+        <AnchorItem href="#基础锚点" title="基础锚点" data-testid={childTestID} />
+      </Anchor>
+    );
+    const wrapper = render(el);
+    const root = wrapper.getByTestId(rootTestID);
+    const title = wrapper.getByText('基础锚点');
+    expect(root).toContainElement(title);
+  });
+});
