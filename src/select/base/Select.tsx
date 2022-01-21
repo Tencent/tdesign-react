694709f6e2d4abc8b58e48eb4780db4f23758bfe
Uyarn <uyarnchen@gmail.com>
2022-01-21 16:27:30 +0800

    feat(select): support panel content and showArrow and inputProps apis

--- src/select/base/Select.tsx
+++ src/select/base/Select.tsx
@@ -81,6 +81,10 @@ const Select = forwardRefWithStatics(
       valueDisplay,
       onEnter,
       onVisibleChange,
+      showArrow = true,
+      inputProps,
+      panelBottomContent,
+      panelTopContent,
     } = useDefaultValue(props);
 
     const { classPrefix } = useConfig();
@@ -324,6 +328,7 @@ const Select = forwardRefWithStatics(
         onFocus={(_, context) => onFocus?.({ value, e: context?.e })}
         onBlur={(_, context) => onBlur?.({ value, e: context?.e })}
         onEnter={(_, context) => onEnter?.({ inputValue: inputVal, value, e: context?.e })}
+        {...inputProps}
       />
     );
 
@@ -365,7 +370,9 @@ const Select = forwardRefWithStatics(
           />
         );
       }
-      return <FakeArrow overlayClassName={`${name}__right-icon`} isActive={showPopup} disabled={disabled} />;
+      return (
+        showArrow && <FakeArrow overlayClassName={`${name}__right-icon`} isActive={showPopup} disabled={disabled} />
+      );
     };
 
     const popupContentProps = {
@@ -383,6 +390,8 @@ const Select = forwardRefWithStatics(
       loading,
       valueType,
       keys,
+      panelBottomContent,
+      panelTopContent,
     };
 
     const renderContent = () => <PopupContent {...popupContentProps}>{children}</PopupContent>;
