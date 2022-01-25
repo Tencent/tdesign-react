dd70718e32b0399d529e2b4387f4e3d1c317953f
HQ-Lin <a154571896@gmail.com>
2022-01-25 13:38:29 +0800

    feat: 调整config 逻辑 & 支持 globalConfig

--- src/config-provider/index.ts
+++ src/config-provider/index.ts
@@ -1,7 +1,7 @@
 import ConfigProvider from './ConfigProvider';
 import ConfigContext from './ConfigContext';
 
-export type { Config } from './ConfigContext';
+export type { Config, Locale } from './ConfigContext';
 export type { ConfigProviderProps } from './ConfigProvider';
 
 export { ConfigContext, ConfigProvider };
