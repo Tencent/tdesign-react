ba0b2ec97887ccc1e38f84831e1bb298722b9dce
zj2015262624 <2269710833@qq.com>
2022-01-26 23:10:53 +0800

    feat: Transfer组件支持使用Tree组件

--- src/transfer/utils.ts
+++ src/transfer/utils.ts
@@ -2,6 +2,7 @@ import isEmpty from 'lodash/isEmpty';
 import isFunction from 'lodash/isFunction';
 import isString from 'lodash/isString';
 import React from 'react';
+import { DataOption, TransferValue } from './type';
 import { TNode } from '../common';
 
 export const getDefaultValue = <T>(value: T | T[], defaultValue?: T): T[] => {
@@ -22,4 +23,34 @@ export const getJSX = (value: string | TNode) => {
   if (React.isValidElement(value)) return value;
 
   return value;
+};
+
+// 获取所有叶子节点
+export const getLeafNodes = (nodes: DataOption[], resData: DataOption[]): DataOption[] => {
+  nodes.forEach((child) => {
+    if (child.children && child.children.length > 0) {
+      return getLeafNodes(child.children, resData);
+    }
+    resData.push(child);
+  });
+  return resData;
+};
+
+// tree过滤checkeds数组中的key
+export const filterCheckedTreeNodes = (nodes: DataOption[], checkeds: TransferValue[]): DataOption[] =>
+  nodes
+    .filter((item) => !checkeds.includes(item.value))
+    .map((item) => {
+      const newItem = { ...item };
+      if (item.children && item.children.length > 0) {
+        newItem.children = filterCheckedTreeNodes(item.children, checkeds);
+      }
+      return newItem;
+    })
+    .filter((item) => (item.children && item.children.length !== 0) || !item.children);
+
+// 获取目的树结构
+export const getTargetNodes = (sourceNodes: DataOption[], data: DataOption[]): DataOption[] => {
+  const source = getLeafNodes(sourceNodes, []).map((item) => item.value);
+  return filterCheckedTreeNodes(data, source);
 };
