7e5e94484f51209f614cdf1020704592783d258b
tianyuuxie <tianyuuxie@tencent.com>
2022-01-27 16:16:33 +0800

    feat(checktag): 支持size

--- src/tag/_example/size.jsx
+++ src/tag/_example/size.jsx
@@ -1,19 +1,29 @@
 ﻿import React from 'react';
 import { Tag } from 'tdesign-react';
 
+const { CheckTag } = Tag;
+
 export default function Size() {
   const style = { marginRight: 30 };
+  const styleTop = { marginTop: 10 };
   return (
-    <div className="tdesign-demo-block-row">
-      <Tag size="small" style={style}>
-        小型标签
-      </Tag>
-      <Tag size="medium" style={style}>
-        默认标签
-      </Tag>
-      <Tag size="large" style={style}>
-        大型标签
-      </Tag>
-    </div>
+    <>
+      <div className="tdesign-demo-block-row">
+        <Tag size="small" style={style}>
+          小型标签
+        </Tag>
+        <Tag size="medium" style={style}>
+          默认标签
+        </Tag>
+        <Tag size="large" style={style}>
+          大型标签
+        </Tag>
+      </div>
+      <div className="tdesign-demo-block-row" style={styleTop}>
+        <CheckTag size="small">小型标签</CheckTag>
+        <CheckTag size="medium">默认标签</CheckTag>
+        <CheckTag size="large">大型标签</CheckTag>
+      </div>
+    </>
   );
 }
