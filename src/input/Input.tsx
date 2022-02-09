69340c4558647f99feeea68c1b8dbbd354df3304
HQ-Lin <a154571896@gmail.com>
2022-02-09 17:33:47 +0800

    feat: input 支持 onWheel、onCompositionstart、onCompositionend 事件

--- src/input/Input.tsx
+++ src/input/Input.tsx
@@ -8,10 +8,7 @@ import { StyledProps, TNode } from '../common';
 import InputGroup from './InputGroup';
 import useDefaultValue from '../_util/useDefaultValue';
 
-export interface InputProps extends TdInputProps, StyledProps {
-  onCompositionStart?: Function;
-  onCompositionEnd?: Function;
-}
+export interface InputProps extends TdInputProps, StyledProps {}
 
 export interface InputRefInterface extends React.RefObject<unknown> {
   currentElement: HTMLDivElement;
@@ -53,11 +50,16 @@ const Input = forwardRefWithStatics(
       onClear,
       onEnter,
       onKeydown,
+      onKeyup,
+      onKeypress,
       onFocus,
       onBlur,
       onPaste,
-      onCompositionStart,
-      onCompositionEnd,
+      onMouseenter,
+      onMouseleave,
+      onWheel,
+      onCompositionstart,
+      onCompositionend,
       autofocus,
       readonly,
       ...restProps
@@ -108,6 +110,8 @@ const Input = forwardRefWithStatics(
         autoFocus={autofocus}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
+        onKeyPress={handleKeyPress}
         onCompositionStart={handleCompositionStart}
         onCompositionEnd={handleCompositionEnd}
         onFocus={handleFocus}
@@ -131,8 +135,9 @@ const Input = forwardRefWithStatics(
           [`${classPrefix}-input--suffix`]: suffixIconContent,
           [`${classPrefix}-input--focused`]: isFocused,
         })}
-        onMouseEnter={() => toggleIsHover(true)}
-        onMouseLeave={() => toggleIsHover(false)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onWheel={handleWheel}
       >
         {prefixIconContent}
         {renderInput}
@@ -160,12 +165,24 @@ const Input = forwardRefWithStatics(
       key === 'Enter' && onEnter?.(value, { e });
       onKeydown?.(value, { e });
     }
+    function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
+      const {
+        currentTarget: { value },
+      } = e;
+      onKeyup?.(value, { e });
+    }
+    function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+      const {
+        currentTarget: { value },
+      } = e;
+      onKeypress?.(value, { e });
+    }
     function handleCompositionStart(e: React.CompositionEvent<HTMLInputElement>) {
       composingRef.current = true;
       const {
         currentTarget: { value },
       } = e;
-      onCompositionStart?.(value, { e });
+      onCompositionstart?.(value, { e });
     }
     function handleCompositionEnd(e: React.CompositionEvent<HTMLInputElement>) {
       const {
@@ -176,7 +193,7 @@ const Input = forwardRefWithStatics(
         handleChange(e);
       }
       setComposingValue('');
-      onCompositionEnd?.(value, { e });
+      onCompositionend?.(value, { e });
     }
 
     function handleFocus(e: React.FocusEvent<HTMLInputElement>) {
@@ -199,6 +216,20 @@ const Input = forwardRefWithStatics(
       const clipData = e.clipboardData;
       const pasteValue = clipData?.getData('text/plain');
       onPaste?.({ e, pasteValue });
+    }
+
+    function handleMouseEnter(e: React.MouseEvent<HTMLDivElement>) {
+      toggleIsHover(true);
+      onMouseenter?.({ e });
+    }
+
+    function handleMouseLeave(e: React.MouseEvent<HTMLDivElement>) {
+      toggleIsHover(false);
+      onMouseleave?.({ e });
+    }
+
+    function handleWheel(e: React.WheelEvent<HTMLDivElement>) {
+      onWheel?.({ e });
     }
 
     useImperativeHandle(ref as InputRefInterface, () => ({
