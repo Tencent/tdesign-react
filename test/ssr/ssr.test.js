2a78049e6d466e96f78f50aed0abc80e66136886
insekkei <1183448115@qq.com>
2022-02-14 17:50:27 +0800

    test: ssr.test config

--- test/ssr/ssr.test.js
+++ test/ssr/ssr.test.js
@@ -1,6 +1,5 @@
 import glob from 'glob';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { renderToString } from 'react-dom/server';
 
 function ssrSnapshotTest() {
@@ -8,14 +7,6 @@ function ssrSnapshotTest() {
   describe('ssr snapshot test', () => {
     beforeAll(() => {
       jest.useFakeTimers().setSystemTime(new Date('2021-12-31').getTime());
-      jest.mock('react', () => ({
-        ...jest.requireActual('react'),
-        useLayoutEffect: jest.requireActual('react').useEffect, // mock uselayout in ssr
-      }));
-      // ReactDOM.createPortal = jest.fn((element) => element); // mock createPortal in ssr
-    });
-    afterEach(() => {
-      // ReactDOM.createPortal.mockClear();
     });
     files.forEach((file) => {
       if (file.indexOf('temp') > -1) {
