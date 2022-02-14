0944e345835a69940b134836377eec453a452eae
carolin913 <carolin913@126.com>
2022-02-14 20:25:48 +0800

    chore: sync

--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -7,7 +7,6 @@ import Select from '../select';
 import Radio from '../radio';
 import noop from '../_util/noop';
 import useConfig from '../_util/useConfig';
-import useLayoutEffect from '../_util/useLayoutEffect';
 import { TdCalendarProps, ControllerOptions, CalendarCell, CalendarValue } from './type';
 import { StyledProps } from '../common';
 import { createDateList, createMonthList } from './_util';
