b10f832247e7b489e8d1dc36302e24109e988535
carolin913 <carolin913@126.com>
2022-02-14 20:00:09 +0800

    test: jest config

--- test/config/jest.ssr.conf.js
+++ test/config/jest.ssr.conf.js
@@ -2,6 +2,7 @@ const baseConfig = require('./jest.base.conf.js');
 
 module.exports = {
   ...baseConfig,
+  testEnvironment: 'node',
   coverageDirectory: '<rootDir>/test/ssr/coverage',
   modulePathIgnorePatterns: ['<rootDir>/test/ssr/coverage/'],
   testRegex: 'test/ssr/.*\\.test\\.js$',
