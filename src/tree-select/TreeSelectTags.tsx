16cf5660f5c171a3579264cb4e7ff23a46eddf04
tcsurewang <984507810@qq.com>
2022-02-15 20:30:40 +0800

    feat(tree-select): support valueDisplay property

--- src/tree-select/TreeSelectTags.tsx
+++ src/tree-select/TreeSelectTags.tsx
@@ -2,69 +2,75 @@ import React, { Fragment, useMemo } from 'react';
 import { TdTreeSelectProps, TreeSelectValue } from './type';
 import { TreeOptionData } from '../common';
 import Tag from '../tag';
+import { NodeOptions } from './TreeSelect';
 
 export interface TreeSelectTagsProps extends TdTreeSelectProps {
-  tagList: Array<TreeSelectValue>;
+  tagList: Array<NodeOptions>;
 }
 
 export default function TreeSelectTags(props: TreeSelectTagsProps) {
-  const { tagList, minCollapsedNum, multiple, collapsedItems, disabled, size, value, data, onRemove, onChange } = props;
+  const { minCollapsedNum, multiple, collapsedItems, disabled, size, value, data } = props;
+  const { tagList, onRemove, onChange, valueDisplay } = props;
+
+  const displaySelectedItems = useMemo(
+    () => tagList.slice(0, minCollapsedNum > 0 ? minCollapsedNum : undefined),
+    [minCollapsedNum, tagList],
+  );
 
   const collapsedSelectedItems = useMemo(() => {
-    if (!multiple || minCollapsedNum <= 0 || !Array.isArray(value)) return [];
-    const optionsMap: Record<string | number, TreeOptionData> = {};
-    function walk(data: TreeOptionData[]) {
-      Array.isArray(data) &&
-        data.forEach((option) => {
-          optionsMap[option.value] = option;
-          walk(option.children);
-        });
-    }
-    walk(data);
-    return value.slice(minCollapsedNum).map((value) => optionsMap[value as number | string]);
-  }, [value, minCollapsedNum, data, multiple]);
+    if (!multiple || minCollapsedNum <= 0 || !Array.isArray(tagList)) return [];
+    return tagList.slice(minCollapsedNum);
+  }, [multiple, minCollapsedNum, tagList]);
 
   if (!tagList.length) return null;
 
-  function removeTag(index: number, data: TreeOptionData, e: React.MouseEvent<SVGElement, MouseEvent>) {
-    e.stopPropagation();
+  function removeTag(index: number, data: TreeOptionData, e?: React.MouseEvent<SVGElement | HTMLDivElement>) {
+    e?.stopPropagation();
     if (disabled || !Array.isArray(value)) return;
 
     onRemove?.({ value: value[index], data, e });
     onChange?.(value.filter((_, i) => i !== index) as Array<TreeSelectValue>, null);
   }
 
-  const tags = tagList.slice(0, minCollapsedNum > 0 ? minCollapsedNum : undefined).map((label, index) => (
-    <Tag
-      key={index}
-      size={size}
-      closable={!disabled}
-      disabled={disabled}
-      onClose={({ e }) => removeTag(index, data, e)}
-    >
-      {label}
-    </Tag>
-  ));
+  const tags = valueDisplay
+    ? valueDisplay({
+        value: displaySelectedItems,
+        onClose: (index) => {
+          index ?? removeTag(index, data);
+        },
+      })
+    : displaySelectedItems.map((tag, index) => (
+        <Tag
+          key={index}
+          size={size}
+          closable={!disabled}
+          disabled={disabled}
+          onClose={({ e }) => removeTag(index, data, e)}
+        >
+          {tag.label}
+        </Tag>
+      ));
 
-  if (minCollapsedNum > 0) {
-    const nums = tagList.length - minCollapsedNum;
-    nums > 0 &&
-      tags.push(
-        <Fragment key={`collapsed-${nums}`}>
-          {typeof collapsedItems === 'function'
-            ? collapsedItems({
-                value: data,
-                collapsedSelectedItems,
-                count: nums,
-              })
-            : collapsedItems ?? (
-                <Tag size={size} disabled={disabled}>
-                  +{nums}
-                </Tag>
-              )}
-        </Fragment>,
-      );
-  }
+  const collapsedTags = collapsedSelectedItems.length > 0 && (
+    <Fragment key={`collapsed-${collapsedSelectedItems.length}`}>
+      {typeof collapsedItems === 'function'
+        ? collapsedItems({
+            value: tagList,
+            collapsedSelectedItems,
+            count: collapsedSelectedItems.length,
+          })
+        : collapsedItems ?? (
+            <Tag size={size} disabled={disabled}>
+              +{collapsedSelectedItems.length}
+            </Tag>
+          )}
+    </Fragment>
+  );
 
-  return <>{tags}</>;
+  return (
+    <>
+      {tags}
+      {collapsedTags}
+    </>
+  );
 }
