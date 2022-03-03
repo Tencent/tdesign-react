c3441ab6b7afa7fb1b6d9a3000a9759e650dae2b
HQ-Lin <a154571896@gmail.com>
2022-03-03 15:27:43 +0800

    fix: 修复 plugin 导出问题

--- src/dialog/index.tsx
+++ src/dialog/index.tsx
@@ -1,5 +1,5 @@
 import _Dialog from './Dialog';
-import { DialogPlugin as _DialogPlugin, dialog as _dialog } from './plugin';
+import { DialogPlugin as _DialogPlugin } from './plugin';
 
 import './style/index.js';
 
@@ -7,6 +7,7 @@ export type { DialogProps } from './Dialog';
 export * from './type';
 
 export const Dialog = _Dialog;
+export const dialog = _DialogPlugin;
 export const DialogPlugin = _DialogPlugin;
-export const dialog = _dialog;
+
 export default Dialog;
