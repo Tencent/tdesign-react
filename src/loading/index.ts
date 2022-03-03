76ed857f1764543b1351693af68d95cc7426b3c9
HQ-Lin <a154571896@gmail.com>
2022-03-03 15:06:51 +0800

    fix: 修复 loading plugin 导出问题

--- src/loading/index.ts
+++ src/loading/index.ts
@@ -1,12 +1,13 @@
 import _Loading from './loading';
+import { LoadingPlugin as _LoadingPlugin, loading as _loading } from './plugin';
 
 import './style/index.js';
 
 export type { LoadingProps } from './loading';
 export * from './type';
 
-export { LoadingPlugin, loading } from './plugin';
-
 export const Loading = _Loading;
+export const loading = _loading;
+export const LoadingPlugin = _LoadingPlugin;
 
 export default Loading;
