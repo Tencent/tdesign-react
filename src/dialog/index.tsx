db25a12a97f4aee4b4273749613e3232be0508dd
HQ-Lin <a154571896@gmail.com>
2022-03-03 15:08:24 +0800

    fix: 修复 dialog plugin 导出问题

--- src/dialog/index.tsx
+++ src/dialog/index.tsx
@@ -1,10 +1,12 @@
 import _Dialog from './Dialog';
+import { DialogPlugin as _DialogPlugin, dialog as _dialog } from './plugin';
 
 import './style/index.js';
 
 export type { DialogProps } from './Dialog';
 export * from './type';
 
-export { DialogPlugin, dialog } from './plugin';
 export const Dialog = _Dialog;
+export const DialogPlugin = _DialogPlugin;
+export const dialog = _dialog;
 export default Dialog;
