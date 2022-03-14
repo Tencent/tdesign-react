fb9ac4ee7b0077bdd2f70fa0581e4b1b16073c73
HQ-Lin <a154571896@gmail.com>
2022-03-14 18:41:21 +0800

    feat: grid 支持获取 css vars 做响应式判断

--- src/_util/dom.ts
+++ src/_util/dom.ts
@@ -200,3 +200,11 @@ export const getAttach = (node: any): HTMLElement => {
   }
   return document.body;
 };
+
+// 获取 css vars
+export const getCssVarsValue = (name: string, element?: HTMLElement) => {
+  if (!canUseDocument) return;
+
+  const el = element || document.documentElement;
+  return getComputedStyle(el).getPropertyValue(name);
+};
