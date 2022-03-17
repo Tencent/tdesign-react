fa10e227ef0f9bd9cc90950bafe42d0b195d3253
carolin913 <carolin913@126.com>
2022-03-17 11:45:08 +0800

    fix(anchor): 外部传入className问题修复

--- src/anchor/_example/base.jsx
+++ src/anchor/_example/base.jsx
@@ -13,7 +13,7 @@ export default function AnchorBase() {
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ flex: '1' }}></div>
-      <Anchor onClick={handleClick} onChange={handleChange} targetOffset={150} affixProps={{ offsetTop: 150 }}>
+      <Anchor className="abc" onClick={handleClick} onChange={handleChange} targetOffset={150} affixProps={{ offsetTop: 150 }}>
         <AnchorItem href="#基础锚点" title="基础锚点" />
         <AnchorItem href="#多级锚点" title="多级锚点" />
         <AnchorItem href="#指定容器锚点" title="指定容器锚点" />
