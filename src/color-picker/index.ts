323b60f9f57192bff6ca7a4b3b15b66041ac6745
carolin913 <carolin913@126.com>
2022-03-18 16:21:05 +0800

    feat(colorpicker): add common utils

---
+++ src/color-picker/index.ts
@@ -0,0 +1,11 @@
+import _ColorPickerPanel from './ColorPickerPanel';
+import _ColorPicker from './ColorPicker';
+import { TdColorPickerProps } from './type';
+
+import './style';
+
+export * from './type';
+export type ColorPickerProps = TdColorPickerProps;
+
+export const ColorPickerPanel = _ColorPickerPanel;
+export const ColorPicker = _ColorPicker;
---
+++ src/color-picker/utils/click-outsider.ts
@@ -0,0 +1,97 @@
+import { ReactNode } from 'react';
+import useConfig from '../../_util/useConfig';
+import { on } from '../../_util/dom';
+
+type Handler = (...args: unknown[]) => unknown;
+
+interface ElementHandler {
+  elements: HTMLElement[];
+  handler: Handler;
+}
+
+type FlushList = Map<number, ElementHandler>;
+
+const nodeList: FlushList = new Map();
+
+let startClick: MouseEvent;
+
+let uid = 0;
+
+if (window && window.document) {
+  on(document, 'mousedown', (e: MouseEvent) => (startClick = e));
+  on(document, 'mouseup', (e: MouseEvent) => {
+    for (const { handler } of nodeList.values()) {
+      handler(e);
+    }
+  });
+}
+
+type NodeElement = HTMLElement | ReactNode;
+
+const createDocumentHandler =
+  (elements: HTMLElement[], handler: Handler, includePopup = true, selector: string) =>
+  (e: MouseEvent) => {
+    if (includePopup) {
+      document.querySelectorAll(selector).forEach((ele: Element) => {
+        elements.push(ele as HTMLElement);
+      });
+    }
+    // eslint-disable-next-line no-param-reassign
+    elements = Array.from(new Set(elements));
+    const mouseUpTarget = e.target as Node;
+    const mouseDownTarget = startClick?.target as Node;
+    const isTargetUnExists = !mouseUpTarget || !mouseDownTarget;
+    if (isTargetUnExists) {
+      return;
+    }
+    const isContained = elements.some((el) => {
+      const isSelf = el === mouseUpTarget;
+      const isContainedByEl = el.contains(mouseUpTarget) || el.contains(mouseDownTarget);
+      return isSelf || isContainedByEl;
+    });
+    if (isContained) {
+      return;
+    }
+    handler();
+  };
+
+/**
+ * 元素外面点击
+ *
+ * @example
+ *  onMounted(() => {
+ *    // 确保元素已挂载
+ *    addClickOutsider(refs.value, () => {
+ *      visible.value = false
+ *    });
+ *  });
+ *
+ *  onBeforeUnmount(() => removeClickOutsider);
+ */
+export const useClickOutsider = () => {
+  uid += 1;
+
+  const { classPrefix } = useConfig();
+  const POPUP_SELECTOR = `.${classPrefix}-popup`;
+  const clickOutsiderId = uid;
+  const addClickOutsider = (els: NodeElement[], handler: Handler) => {
+    const elements = Array.from(new Set(els.filter((el) => el))).map((el: any) => {
+      const node = (el.el || el.$el || el) as HTMLElement;
+      return node;
+    });
+    const documentHandler = createDocumentHandler(elements, handler, true, POPUP_SELECTOR);
+    nodeList.set(clickOutsiderId, {
+      elements,
+      handler: documentHandler,
+    });
+  };
+
+  const removeClickOutsider = () => {
+    nodeList.has(clickOutsiderId) && nodeList.delete(clickOutsiderId);
+  };
+  return {
+    clickOutsiderId,
+    addClickOutsider,
+    removeClickOutsider,
+  };
+};
