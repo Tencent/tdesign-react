c39ea20ae83e00b426366688d1e04d80a20d3be7
Uyarn <uyarnchen@gmail.com>
2022-04-14 16:34:58 +0800

    fix(globalConfig): fix ts error

--- src/config-provider/ConfigProvider.tsx
+++ src/config-provider/ConfigProvider.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import _mergeWith from 'lodash/mergeWith';
-import ConfigContext, { Config, defaultGlobalConfig, GlobalConfig } from './ConfigContext';
+import ConfigContext, { Config, defaultGlobalConfig } from './ConfigContext';
+import { GlobalConfigProvider } from './type';
 
 export interface ConfigProviderProps extends Config {
   children: React.ReactNode;
 }
 
 // deal with https://github.com/lodash/lodash/issues/1313
-export const merge = (src: GlobalConfig, config: GlobalConfig) =>
+export const merge = (src: GlobalConfigProvider, config: GlobalConfigProvider) =>
   _mergeWith(src, config, (objValue, srcValue) => {
     if (Array.isArray(objValue)) {
       return srcValue;
