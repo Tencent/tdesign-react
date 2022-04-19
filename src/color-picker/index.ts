ec4c634d3148c3a47cb797f8fb5c67282c667ead
carolin913 <carolin913@126.com>
2022-04-19 17:30:48 +0800

    fix(colorpicker): type export

--- src/color-picker/index.ts
+++ src/color-picker/index.ts
@@ -1,14 +1,12 @@
 import _ColorPickerPanel from './ColorPickerPanel';
 import _ColorPicker from './ColorPicker';
-import { TdColorPickerProps } from './type';
 
 import './style';
 
+export type { ColorPickerProps } from './interface';
 export * from './type';
-
-export type ColorPickerProps = TdColorPickerProps;
 
 export const ColorPickerPanel = _ColorPickerPanel;
 export const ColorPicker = _ColorPicker;
 
-export default { ColorPicker, ColorPickerPanel };
+export default ColorPicker;
