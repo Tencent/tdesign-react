f4215c76389ffb508ab13337772238f3643ad191
liamhuo <liamhuo@tencent.com>
2022-05-17 20:19:24 +0800

    feat:优化dialog定位问题导致子节点的 fixed 属性定位失效 1、问题/原因： 当前Dialog 是通过transform 做动画和位移的，transform 属性会导致浏览器创建一个 layer 层从而使内部子节点的 fixed 属性定位失效
    调整项目：
    使用flex布局解决之前居中布局使用transform位移导致子节点的 fixed 失效
    
    2、问题/原因：
    没有适配dialog长度超出屏幕，使用最大高度100vh，然后内部body进行滚动，不支持mask滚动
    
    调整项目：
    新增wrap、position两层div，用于dialog内容过长出现滚动场景
    
    3、问题原因：
    没有适配top定位时的拖拽功能
    
    调整项目：
    优化拖拽，重写定位位置计算，适配top定位
    当dialog内容超出屏幕，不能进行拖拽
    
    4、问题/原因：
    没有实现top API
    
    调整项目：
    新增top API的实现

--- src/dialog/_example/modal.jsx
+++ src/dialog/_example/modal.jsx
@@ -3,21 +3,44 @@ import { Dialog, Button } from 'tdesign-react';
 
 export default function NotModalExample() {
   const [visible, setVisible] = useState(false);
-
+  const [modal, setModal] = useState('modal');
+  
   const handleClick = () => {
     setVisible(true);
+    setModal('modal');
   };
   const handleClose = () => {
     setVisible(false);
   };
   return (
     <div>
-      <Button theme="primary" onClick={handleClick}>
-        Open Modal
-      </Button>
+      <div>
+        <Button theme="primary" onClick={handleClick} style={{ marginRight: 16 }}>
+          模态对话框
+        </Button>
+        <Button
+          theme="primary"
+          onClick={() => {
+            setVisible(true);
+            setModal('modeless');
+          }}
+          style={{ marginRight: 16 }}
+        >
+          非模态框
+        </Button>
+        <Button theme="primary" 
+          onClick={() => {
+            setVisible(true);
+            setModal('normal');
+          }}>
+          普通对话框
+        </Button>
+      </div>
       <Dialog
-        mode="modeless"
-        header="非模态框"
+        mode={modal}
+        placement="top"
+        header="对话框"
+        draggable="{draggable}"
         visible={visible}
         onClose={handleClose}
         onOpened={() => {
