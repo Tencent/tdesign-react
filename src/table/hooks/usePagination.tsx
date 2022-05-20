f947428ef6e65937dc394cab550dd25619d96406
yuyang <uyarnchen@gmail.com>
2022-05-20 17:22:22 +0800

    fix(table): row num is total when async loading (#778)
    * fix(table): pageSize is total when async loading
    
    * chore: update common

--- src/table/hooks/usePagination.tsx
+++ src/table/hooks/usePagination.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import useConfig from '../../_util/useConfig';
 import Pagination, { PageInfo } from '../../pagination';
 import { TdBaseTableProps, TableRowData } from '../type';
@@ -11,32 +11,34 @@ export default function usePagination(props: TdBaseTableProps) {
   const [dataSource, setDataSource] = useState<TableRowData[]>([]);
   const [isPaginateData, setIsPaginateData] = useState(false);
 
-  const updateDataSourceAndPaginate = (current = 1, pageSize = 10) => {
-    // data 数据数量超出分页大小时，则自动启动本地数据分页
-    const isPaginateData = Boolean(!disableDataPage && data.length > pageSize);
-    setIsPaginateData(isPaginateData);
-    if (isPaginateData) {
-      const start = (current - 1) * pageSize;
-      const end = current * pageSize;
-      setDataSource([...data.slice(start, end)]);
-    } else {
-      setDataSource(data);
-    }
-  };
+  const updateDataSourceAndPaginate = useCallback(
+    (current = 1, pageSize = 10) => {
+      // data 数据数量超出分页大小时，则自动启动本地数据分页
+      const isPaginateData = Boolean(!disableDataPage && data.length > pageSize);
+      setIsPaginateData(isPaginateData);
+      if (isPaginateData) {
+        const start = (current - 1) * pageSize;
+        const end = current * pageSize;
+        setDataSource([...data.slice(start, end)]);
+      } else {
+        setDataSource(data);
+      }
+    },
+    [data, disableDataPage],
+  );
 
   // 受控情况，只有 pagination.current 或者 pagination.pageSize 变化，才对数据进行排序
   useEffect(() => {
     if (!pagination || !pagination.current) return;
     updateDataSourceAndPaginate(pagination.current, pagination.pageSize);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pagination?.current, pagination?.pageSize, data, disableDataPage]);
+  }, [pagination, updateDataSourceAndPaginate]);
 
-  // 非受控情况，只执行一次 Props 数据更新（pagination.defaultCurrent 和 pagination.defaultPageSize）
+  // 非受控情况
   useEffect(() => {
     if (!pagination || !pagination.defaultCurrent) return;
     updateDataSourceAndPaginate(pagination.defaultCurrent, pagination.defaultPageSize);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [updateDataSourceAndPaginate]);
 
   const renderPagination = () => {
     if (!pagination) return null;
