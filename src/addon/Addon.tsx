0a6cbc287d7f5b41cae62ea87b8a9ecf156ef711
Kyrielin <a154571896@gmail.com>
2022-05-27 14:12:27 +0800

    fix: 修复addon 在form表单下数据劫持失败问题 (#802)

--- src/addon/Addon.tsx
+++ src/addon/Addon.tsx
@@ -26,18 +26,29 @@ const renderAddon = (type, classPrefix, Content) => {
 
 const Addon = forwardRef((props: AddonProps, ref: React.Ref<HTMLDivElement>) => {
   const { classPrefix } = useConfig();
-  const { prepend, append, children, className, ...wrapperProps } = props;
+  const { prepend, append, children, className, style, ...wrapperProps } = props;
+
+  function renderChildren() {
+    return React.Children.map(children, (child) => {
+      if (!child) return null;
+      if (React.isValidElement(child)) {
+        return React.cloneElement(child, { ...wrapperProps });
+      }
+      return child;
+    });
+  }
+
   return (
     <div
       ref={ref}
+      style={style}
       className={classNames(className, `${classPrefix}-addon`, {
         [`${classPrefix}-addon--prepend`]: !!prepend,
         [`${classPrefix}-addon--append`]: !!append,
       })}
-      {...wrapperProps}
     >
       {renderAddon('prepend', classPrefix, prepend)}
-      {children}
+      {renderChildren()}
       {renderAddon('append', classPrefix, append)}
     </div>
   );
