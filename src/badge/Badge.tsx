0c4160a1b93bc29cafefb64685a23327c9820b2d
southorange1228 <15280970040@163.com>
2022-06-14 11:12:51 +0800

    feat(anchor,collapse,badge): add some components ref props

--- src/badge/Badge.tsx
+++ src/badge/Badge.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import classNames from 'classnames';
 import { StyledProps } from '../common';
 import useConfig from '../_util/useConfig';
@@ -7,72 +7,63 @@ import { badgeDefaultProps } from './defaultProps';
 
 export interface BadgeProps extends TdBadgeProps, StyledProps {}
 
-const Badge: React.FC<BadgeProps> = ({
-  color,
-  dot,
-  maxCount,
-  count,
-  size,
-  shape,
-  showZero,
-  offset,
-  className,
-  content,
-  children,
-  style,
-  ...restProps
-}) => {
-  const { classPrefix } = useConfig();
+const Badge: React.FC<BadgeProps> = forwardRef<HTMLSpanElement, BadgeProps>(
+  (
+    { color, dot, maxCount, count, size, shape, showZero, offset, className, content, children, style, ...restProps },
+    ref,
+  ) => {
+    const { classPrefix } = useConfig();
 
-  const childNode = children || content;
+    const childNode = children || content;
 
-  const badgeClassName = classNames(
-    !childNode && `${classPrefix}-badge--static`,
-    dot ? `${classPrefix}-badge--dot` : shape && `${classPrefix}-badge--${shape}`,
-    size === 'small' && `${classPrefix}-size-s`,
-    !childNode && className,
-  );
-  const getDisplayCount = () => {
-    if (typeof count === 'number' && count > maxCount) {
-      return `${maxCount}+`;
-    }
-    return count;
-  };
-
-  let isHidden = !count;
-  if (typeof count === 'number') {
-    isHidden = count < 1 && !showZero;
-  }
-
-  const getStyle = () => {
-    const mergedStyle: React.CSSProperties = { ...style };
-    if (color) mergedStyle.backgroundColor = color;
-    if (offset) {
-      if (offset[0]) {
-        mergedStyle.right = +offset[0];
+    const badgeClassName = classNames(
+      !childNode && `${classPrefix}-badge--static`,
+      dot ? `${classPrefix}-badge--dot` : shape && `${classPrefix}-badge--${shape}`,
+      size === 'small' && `${classPrefix}-size-s`,
+      !childNode && className,
+    );
+    const getDisplayCount = () => {
+      if (typeof count === 'number' && count > maxCount) {
+        return `${maxCount}+`;
       }
-      if (offset[1]) {
-        mergedStyle.marginTop = +offset[1];
-      }
+      return count;
+    };
+
+    let isHidden = !count;
+    if (typeof count === 'number') {
+      isHidden = count < 1 && !showZero;
     }
-    return mergedStyle;
-  };
 
-  const badge = !isHidden ? (
-    <span {...(childNode ? {} : restProps)} className={badgeClassName} style={getStyle()}>
-      {!dot ? getDisplayCount() : null}
-    </span>
-  ) : null;
+    const getStyle = () => {
+      const mergedStyle: React.CSSProperties = { ...style };
+      if (color) mergedStyle.backgroundColor = color;
+      if (offset) {
+        if (offset[0]) {
+          mergedStyle.right = +offset[0];
+        }
+        if (offset[1]) {
+          mergedStyle.marginTop = +offset[1];
+        }
+      }
+      return mergedStyle;
+    };
 
-  if (!childNode) return badge;
+    const badge = !isHidden ? (
+      <span {...(childNode ? {} : restProps)} className={badgeClassName} style={getStyle()}>
+        {!dot ? getDisplayCount() : null}
+      </span>
+    ) : null;
 
-  return (
-    <span {...restProps} className={classNames(`${classPrefix}-badge`, className)}>
-      {childNode}
-      {badge}
-    </span>
-  );
-};
+    if (!childNode) return badge;
+
+    return (
+      <span {...restProps} className={classNames(`${classPrefix}-badge`, className)} ref={ref}>
+        {childNode}
+        {badge}
+      </span>
+    );
+  },
+);
 
 Badge.displayName = 'Badge';
 Badge.defaultProps = badgeDefaultProps;
