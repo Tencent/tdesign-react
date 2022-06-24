ec6aff73e6580d768cc566fb4d5339cfa8887030
southorange1228 <15280970040@163.com>
2022-06-24 11:08:19 +0800

    style(message): 统一使用Space组件实现example

--- src/message/_example/closeAll.jsx
+++ src/message/_example/closeAll.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { MessagePlugin, Button } from "tdesign-react";
+import React from 'react';
+import { MessagePlugin, Button, Space } from 'tdesign-react';
 
 export default function () {
   return (
-    <div className="tdesign-demo-block-row">
+    <Space>
       <Button
         onClick={() => {
-          MessagePlugin.info("这是第一条消息");
-          MessagePlugin.warning("这是第二条消息");
-          MessagePlugin.error("这是第三条消息");
+          MessagePlugin.info('这是第一条消息');
+          MessagePlugin.warning('这是第二条消息');
+          MessagePlugin.error('这是第三条消息');
         }}
       >
         点击打开多个消息
@@ -21,6 +21,6 @@ export default function () {
       >
         点击关闭所有消息
       </Button>
-    </div>
+    </Space>
   );
 }
