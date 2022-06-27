ba610be368005b62323a45fc62cb2e5904fc0158
southorange1228 <15280970040@163.com>
2022-06-27 11:02:09 +0800

    style(tag-input): 统一使用Space组件实现example

--- src/tag-input/_example/theme.jsx
+++ src/tag-input/_example/theme.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { TagInput } from 'tdesign-react';
+import { TagInput, Space } from 'tdesign-react';
 
 export default function TagInputThemeExample() {
   const [tags, setTags] = useState(['Vue', 'React', 'Miniprogram']);
   return (
-    <div className="tdesign-demo-block-column" style={{ width: '80%' }}>
+    <Space direction="vertical" style={{ width: '80%' }}>
       <TagInput value={tags} onChange={setTags} tagProps={{ theme: 'primary' }} />
       <TagInput value={tags} onChange={setTags} tagProps={{ theme: 'success' }} />
       <TagInput value={tags} onChange={setTags} tagProps={{ theme: 'warning' }} />
       <TagInput value={tags} onChange={setTags} tagProps={{ theme: 'danger' }} />
-    </div>
+    </Space>
   );
 }
