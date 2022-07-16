85ad2bb8a2d4e255cb1bbe30d96e4138c8e9460e
chaishi <yaoyanhuoyi@qq.com>
2022-07-16 21:25:32 +0800

    fix(table): table editable cell support ReactNode Props

--- src/table/BaseTable.tsx
+++ src/table/BaseTable.tsx
@@ -116,7 +116,7 @@ const BaseTable = forwardRef((props: TBaseTableProps, ref) => {
 
   const [lastLeafColumns, setLastLeafColumns] = useState(props.columns || []);
   useEffect(() => {
-    if (JSON.stringify(lastLeafColumns) !== JSON.stringify(spansAndLeafNodes.leafColumns)) {
+    if (lastLeafColumns.map((t) => t.colKey).join() !== spansAndLeafNodes.leafColumns.map((t) => t.colKey).join()) {
       props.onLeafColumnsChange?.(spansAndLeafNodes.leafColumns);
       // eslint-disable-next-line react-hooks/exhaustive-deps
       setLastLeafColumns(spansAndLeafNodes.leafColumns);
