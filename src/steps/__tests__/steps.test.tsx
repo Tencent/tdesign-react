73e4c335b59f96ff60760f286d26fc67f6095ef2
insekkei <1183448115@qq.com>
2022-08-17 18:48:29 +0800

    test(add steps test): steps test #1280

--- src/steps/__tests__/steps.test.tsx
+++ src/steps/__tests__/steps.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { testExamples, render } from '@test/utils';
+import { testExamples, render, waitFor, fireEvent } from '@test/utils';
 import Steps from '../Steps';
 
 const { StepItem } = Steps;
@@ -21,5 +21,43 @@ describe('Steps 组件测试', () => {
     expect(() => {
       wrapper.unmount();
     }).not.toThrow();
+  });
+
+  test('options 测试', async () => {
+    const testId = 'step options test';
+    const handleChange = jest.fn();
+
+    const { getByTestId } = render(
+      <div data-testid={testId}>
+        <Steps
+          current={2}
+          options={[
+            {
+              title: '1',
+              content: '这里是提示文字',
+              value: 1,
+            },
+            {
+              title: '2',
+              content: '这里是提示文字',
+              value: 2,
+            },
+            {
+              title: '3',
+              content: '这里是提示文字',
+              value: 3,
+            },
+          ]}
+          onChange={handleChange}
+        />
+      </div>,
+    );
+
+    const stepsInstance = await waitFor(() => getByTestId(testId));
+    const stepsItems = stepsInstance.querySelectorAll('.t-steps-item');
+    expect(stepsItems.length).toBe(3);
+
+    fireEvent.click(stepsInstance.querySelector('.t-steps-item__inner'));
+    expect(handleChange).toBeCalledTimes(1);
   });
 });
