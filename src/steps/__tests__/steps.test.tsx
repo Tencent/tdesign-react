eb935b43957d02a1fdeb0ebcd5ea2f271da40261
insekkei <1183448115@qq.com>
2022-08-22 11:16:27 +0800

    test(steps): test case

--- src/steps/__tests__/steps.test.tsx
+++ src/steps/__tests__/steps.test.tsx
@@ -7,6 +7,24 @@ const { StepItem } = Steps;
 // 测试组件代码 Example 快照
 testExamples(__dirname);
 
+const stepOptions = [
+  {
+    title: '1',
+    content: '这里是提示文字',
+    value: 1,
+  },
+  {
+    title: '2',
+    content: '这里是提示文字',
+    value: 2,
+  },
+  {
+    title: '3',
+    content: '这里是提示文字',
+    value: 3,
+  },
+];
+
 describe('Steps 组件测试', () => {
   test('mount, unmount 测试', () => {
     const wrapper = render(
@@ -29,27 +47,7 @@ describe('Steps 组件测试', () => {
 
     const { getByTestId } = render(
       <div data-testid={testId}>
-        <Steps
-          current={2}
-          options={[
-            {
-              title: '1',
-              content: '这里是提示文字',
-              value: 1,
-            },
-            {
-              title: '2',
-              content: '这里是提示文字',
-              value: 2,
-            },
-            {
-              title: '3',
-              content: '这里是提示文字',
-              value: 3,
-            },
-          ]}
-          onChange={handleChange}
-        />
+        <Steps current={2} options={stepOptions} onChange={handleChange} />
       </div>,
     );
 
@@ -59,5 +57,33 @@ describe('Steps 组件测试', () => {
 
     fireEvent.click(stepsInstance.querySelector('.t-steps-item__inner'));
     expect(handleChange).toBeCalledTimes(1);
+  });
+
+  test('layout vertical 测试', async () => {
+    const testId = 'step layout test';
+
+    const { getByTestId } = render(
+      <div data-testid={testId}>
+        <Steps layout="vertical" options={stepOptions} />
+      </div>,
+    );
+
+    const stepsInstance = await waitFor(() => getByTestId(testId));
+    const stepsItems = stepsInstance.querySelectorAll('.t-steps--vertical');
+    expect(stepsItems.length).toBe(1);
+  });
+
+  test('layout readonly 测试', async () => {
+    const testId = 'step readonly test';
+
+    const { getByTestId } = render(
+      <div data-testid={testId}>
+        <Steps options={stepOptions} readonly />
+      </div>,
+    );
+
+    const stepsInstance = await waitFor(() => getByTestId(testId));
+    const stepsItems = stepsInstance.querySelectorAll('.t-steps-item--clickable');
+    expect(stepsItems.length).toBe(0);
   });
 });
