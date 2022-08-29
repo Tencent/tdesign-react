f55dc61c732d9065fe55504c23985d441174e75f
carolin913 <carolin913@126.com>
2022-08-29 12:25:06 +0800

    fix(select): option设置content未生效问题

--- src/select/_example/base.jsx
+++ src/select/_example/base.jsx
@@ -18,7 +18,7 @@ export default function SingleSelect() {
         { label: '大数据', value: '2' },
         { label: '区块链', value: '3' },
         { label: '物联网', value: '4', disabled: true },
-        { label: '人工智能', value: '5' },
+        { label: '人工智能', value: '5', content: <span>人工智能（新）</span> },
       ]}
     ></Select>
   );
