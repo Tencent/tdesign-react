126eafbb31a3f942a9851aca5257cf61f0c76622
Kyrielin <a154571896@gmail.com>
2022-09-13 11:06:26 +0800

    fix: 修复 ref 透传丢失属性问题 (#1468)
    * fix: 修复 ref 透传丢失属性问题
    
    * fix: 修复 lint

--- src/_util/composeRefs.ts
+++ src/_util/composeRefs.ts
@@ -5,10 +5,7 @@ export default function composeRefs<T>(...refs: Ref<T>[]) {
   return (instance: T) => {
     // eslint-disable-next-line no-restricted-syntax
     for (const ref of refs) {
-      // 兼容 useImperativeHandle 屏蔽获取当前 dom 场景
-      if (instance && (instance as any).currentElement && typeof ref === 'function') {
-        ref((instance as any).currentElement);
-      } else if (typeof ref === 'function') {
+      if (typeof ref === 'function') {
         ref(instance);
       } else if (ref) {
         (ref as any).current = instance;
--- src/popup/utils/ref.ts
+++ src/popup/utils/ref.ts
@@ -1,24 +1,5 @@
 import { isMemo } from 'react-is';
 
-// 合并 refs
-export function composeRef<T>(...refs: React.Ref<T>[]): React.Ref<T> {
-  return (node: T) => {
-    refs.forEach((ref) => {
-      if (typeof ref === 'function') {
-        ref(node);
-      } else if (typeof ref === 'object' && ref && 'current' in ref) {
-        if (node && 'currentElement' in node) {
-          // eslint-disable-next-line
-          (ref as any).current = node['currentElement'];
-        } else {
-          // eslint-disable-next-line
-          (ref as any).current = node;
-        }
-      }
-    });
-  };
-}
-
 // 判断是否支持 ref 透传
 export function supportRef(nodeOrComponent: any): boolean {
   const type = isMemo(nodeOrComponent) ? nodeOrComponent.type.type : nodeOrComponent.type;
@@ -34,4 +15,12 @@ export function supportRef(nodeOrComponent: any): boolean {
   }
 
   return true;
+}
+
+// 获取 ref 中的 dom 元素
+export function getRefDom(domRef: React.RefObject<any>) {
+  if (domRef.current && typeof domRef.current === 'object' && 'currentElement' in domRef.current) {
+    return domRef.current.currentElement;
+  }
+  return domRef.current;
 }
