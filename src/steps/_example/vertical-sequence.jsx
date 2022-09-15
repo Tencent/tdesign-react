2599521222c99225b429aa8842e28db3d79935c7
Micro-sun <1306714976@qq.com>
2022-09-15 15:07:12 +0800

    fix: 修复step步骤条

--- src/steps/_example/vertical-sequence.jsx
+++ src/steps/_example/vertical-sequence.jsx
@@ -14,11 +14,10 @@ export default function HorizontalStepsWithNumbers() {
       </Steps>
       <Steps layout="vertical" sequence="reverse" defaultCurrent={1}>
         <StepItem title="已完成的步骤" content="这里是提示文字" />
+        <StepItem title="已完成的步骤" content="这里是提示文字" />
         <StepItem title="进行中的步骤" content="这里是提示文字" />
-        <StepItem title="未进行的步骤" content="这里是提示文字" />
         <StepItem title="未进行的步骤" content="这里是提示文字" />
       </Steps>
     </div>
-
   );
 }
