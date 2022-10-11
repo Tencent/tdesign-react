15f0219fb63c761206de2b1d5e6066aa8d101d63
duanbaosheng <1065140483@qq.com>
2022-10-11 16:50:37 +0800

    fix(collapse): 1. defaultExpandAll 属性没有生效 2. 包含form表单的时候样式出现溢出问题 (#1579)
    * fix(collapse): 1. defaultExpandAll 属性没有生效 2. 包含form表单的时候样式出现溢出问题
    
    * fix(collapse): defaultExpandAll没有生效,包含form组件的时候样式出现问题

--- src/collapse/__tests__/collapse.test.tsx
+++ src/collapse/__tests__/collapse.test.tsx
@@ -25,14 +25,14 @@ describe('Collapse Unit Test', () => {
   // 渲染测试
   test('create', async () => {
     const { container, queryByText } = render(<CollapseTestComponent />);
-    expect(container.firstChild.classList.contains('t-collapse')).toBeTruthy();
+    expect(container.firstElementChild.classList.contains('t-collapse')).toBeTruthy();
     expect(queryByText(panelContent)).toBeInTheDocument();
   });
 
   // 测试点击事件
   test('click event', async () => {
     const CollapseTest = () => (
-      <Collapse defaultExpandAll defaultValue="default">
+      <Collapse defaultExpandAll defaultValue={['default']}>
         <Panel header="这是一个折叠标题" value="default">
           这部分是每个折叠面板折叠或展开的内容，可根据不同业务或用户的使用诉求，进行自定义填充。可以是纯文本、图文、子列表等内容形式。
         </Panel>
