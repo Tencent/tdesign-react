fac5be55a586f56f4e0f6e8e10e5e096231fcfa5
Jessica Shao <sxybupt@163.com>
2022-10-24 15:27:50 +0800

    fix: [Avatar]demo中头像类型展示，删除重复头像并调整展示顺序 (#1585)
    * fix: [Avatar]demo中头像类型展示，删除重复头像并调整展示顺序
    
    fix Tencent/tdesign #242
    
    * fix(avatar): update snapshot
    
    Tencent/tdesign#242
    
    * test: update snapshot
    
    * test: update snapshot
    
    Co-authored-by: xiaosansiji <xiaosansiji@gmail.com>

--- src/avatar/_example/base.jsx
+++ src/avatar/_example/base.jsx
@@ -5,16 +5,13 @@ import { UserIcon } from 'tdesign-icons-react';
 export default function BasicAvatar() {
   return (
     <Space>
+      <Avatar icon={<UserIcon />} style={{ marginRight: '40px' }} />
       <Avatar
         image="https://tdesign.gtimg.com/site/avatar.jpg"
         hideOnLoadFailed="false"
         style={{ marginRight: '40px' }}
-      ></Avatar>
+      />
       <Avatar style={{ marginRight: '40px' }}>W</Avatar>
-      <Avatar icon={<UserIcon />} style={{ marginRight: '40px' }}></Avatar>
-      <Avatar style={{ marginRight: '40px' }}>
-        <UserIcon />
-      </Avatar>
     </Space>
   );
 }
