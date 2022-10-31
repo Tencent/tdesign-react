69fe8785d3e15372f2f425a47a3859522fb50488
YilunSun <osirisnero@gmail.com>
2022-10-31 19:16:31 +0800

    fix: handle pr

--- src/guide/_example/custom-popup.jsx
+++ src/guide/_example/custom-popup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
+import { ArrowUpIcon } from 'tdesign-icons-react';
 import { Button, Drawer, Guide, Input, Row } from 'tdesign-react';
-import MyPopup from './my-popup';
 
 const classStyles = `
 <style>
@@ -44,8 +44,74 @@ const classStyles = `
 .action button:first-child {
   margin-right: 10px;
 }
+
+
+.my-popup {
+  width: 240px;
+}
+
+.pop-icon {
+  margin-top: 10px;
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.popup-desc {
+  margin-top: 10px;
+  color: rgba(255, 255, 255, 0.9);
+  font-size: 12px;
+  font-weight: 400;
+  text-align: left;
+  line-height: 20px;
+}
+
+.popup-action {
+  margin-top: 10px;
+  text-align: right;
+}
+
+.popup-action button {
+  margin-left: 8px;
+}
 </style>
 `;
+
+function MyPopup(props) {
+  useEffect(() => {
+    // 添加示例代码所需样式
+    document.head.insertAdjacentHTML('beforeend', classStyles);
+  }, []);
+
+  const { handlePrev, handleNext, handleSkip, handleFinish, current, total } = props;
+
+  return (
+    <div className="my-popup">
+      <ArrowUpIcon className="pop-icon" />
+      <p className="popup-desc">自定义的图形或说明文案，用来解释或指导该功能使用。</p>
+      <div className="popup-action">
+        <Button theme="default" size="small" onClick={handleSkip}>
+          跳过
+        </Button>
+        {current !== 0 && (
+          <Button theme="default" size="small" onClick={handlePrev}>
+            上一步
+          </Button>
+        )}
+        {current + 1 < total && (
+          <Button theme="primary" size="small" onClick={handleNext}>
+            下一步
+          </Button>
+        )}
+        {current + 1 === total && (
+          <Button theme="primary" size="small" onClick={handleFinish}>
+            完成
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
 
 export default function CustomPopupGuide() {
   useEffect(() => {
--- src/guide/_example/popup-dialog.jsx
+++ src/guide/_example/popup-dialog.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { Button, Drawer, Guide, Input, Row } from 'tdesign-react';
-import DialogBody from './dialog-body';
 
 const classStyles = `
 <style>
@@ -44,8 +43,43 @@ const classStyles = `
 .action button:first-child {
   margin-right: 10px;
 }
+
+.dialog-body .img-wrapper {
+  border-radius: var(--td-radius-default);
+  overflow: hidden;
+}
+
+.dialog-body p {
+  margin-top: 24px;
+  color: var(--td-text-color-secondary);
+  font-size: 14px;
+  font-weight: 400;
+  text-align: left;
+  line-height: 22px;
+}
+
+.dialog-body .img-wrapper img {
+  vertical-align: bottom;
+  width: 100%;
+}
 </style>
 `;
+
+function DialogBody() {
+  useEffect(() => {
+    // 添加示例代码所需样式
+    document.head.insertAdjacentHTML('beforeend', classStyles);
+  }, []);
+
+  return (
+    <div className="dialog-body">
+      <div className="img-wrapper">
+        <img className="img" src="https://tdesign.gtimg.com/demo/demo-image-1.png" alt="demo" />
+      </div>
+      <p>此处显示本页引导的说明文案，可按需要撰写，如内容过多可折行显示。图文也可按需自由设计。</p>
+    </div>
+  );
+}
 
 export default function PopupDialogGuide() {
   useEffect(() => {
