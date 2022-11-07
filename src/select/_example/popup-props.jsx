a83fa9663430f5096a457885463947889206bb7e
yuyang <uyarnchen@gmail.com>
2022-11-07 17:01:35 +0800

    fix(Select): option ellipsis (#1676)
    * fix(Select): option ellipsis
    
    * chore: update snapshot

--- src/select/_example/popup-props.jsx
+++ src/select/_example/popup-props.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Select } from 'tdesign-react';
+import { Select, Space } from 'tdesign-react';
 
 const SelectPopupProps = () => {
   const [value, setValue] = useState('1');
@@ -7,22 +7,39 @@ const SelectPopupProps = () => {
     setValue(value);
   };
   return (
-    <Select
-      value={value}
-      onChange={onChange}
-      style={{ width: '40%' }}
-      popupProps={{ overlayStyle: { width: '600px' } }}
-      options={[
-        {
-          label: '已选择的选项',
-          value: '1',
-        },
-        {
-          label: '短的选项二',
-          value: '2',
-        },
-      ]}
-    ></Select>
+    <Space>
+      <Select
+        value={value}
+        onChange={onChange}
+        style={{ width: '300px', display: 'inline-block' }}
+        options={[
+          {
+            label: '固定300px宽度',
+            value: '1',
+          },
+          {
+            label: '选项内容超长超长超长超长超长超长超长超长超长超长超长的选项',
+            value: '2',
+          },
+        ]}
+      ></Select>
+      <Select
+        value={value}
+        onChange={onChange}
+        style={{ width: '300px' }}
+        popupProps={{ overlayInnerStyle: { width: '300px' } }}
+        options={[
+          {
+            label: '下拉框强制和输入框同宽',
+            value: '1',
+          },
+          {
+            label: '选项内容超长超长超长超长超长超长超长超长超长超长超长的选项',
+            value: '2',
+          },
+        ]}
+      ></Select>
+    </Space>
   );
 };
 
