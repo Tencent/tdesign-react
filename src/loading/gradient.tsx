d4b63476a126a847833b21921ab8018248ab9282
yuyang <uyarnchen@gmail.com>
2022-11-10 10:38:35 +0800

    fix(Loading): fix loading in dialog (#1694)

--- src/loading/gradient.tsx
+++ src/loading/gradient.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, FC } from 'react';
+import React, { useEffect, FC } from 'react';
 import classnames from 'classnames';
-
+import useDomRefCallback from '../hooks/useDomRefCallback';
 import useConfig from '../hooks/useConfig';
 import circleAdapter from '../_common/js/loading/circle-adapter';
 
@@ -9,14 +9,13 @@ import circleAdapter from '../_common/js/loading/circle-adapter';
  */
 const GradientLoading: FC = () => {
   const { classPrefix } = useConfig();
-  const conicRef = useRef();
-
+  const [conicRef, setConicRef] = useDomRefCallback();
   const gradientClass = `${classPrefix}-loading__gradient`;
 
   useEffect(() => {
-    const el = conicRef?.current;
+    const el = conicRef;
     circleAdapter(el);
-  }, []);
+  }, [conicRef]);
 
   return (
     <svg
@@ -28,7 +27,7 @@ const GradientLoading: FC = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <foreignObject x="1" y="1" width="12" height="12">
-        <div className={`${gradientClass}-conic`} ref={conicRef} />
+        <div className={`${gradientClass}-conic`} ref={setConicRef} />
       </foreignObject>
     </svg>
   );
