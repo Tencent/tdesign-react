60cecc044ebd03d641c01ecb84c77f3da9c96f18
v_pengmcao <cpm0505@163.com>
2022-11-23 11:17:46 +0800

    fix: pagination添加space

--- src/pagination/_example/more.jsx
+++ src/pagination/_example/more.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Pagination } from 'tdesign-react';
+import { Pagination,Space } from 'tdesign-react';
 
 export default function PaginationExample() {
   const [pageSize, changePageSize] = useState(5);
@@ -10,16 +10,18 @@ export default function PaginationExample() {
 
   return (
     <div>
-      <span>展示首尾页码省略</span>
-      <Pagination total={100} pageSize={pageSize} onChange={onChange} onPageSizeChange={(v) => changePageSize(v)} />
-      <span>不展示首尾页码省略</span>
-      <Pagination
-        total={100}
-        pageSize={pageSize}
-        onChange={onChange}
-        pageEllipsisMode="both-ends"
-        onPageSizeChange={(v) => changePageSize(v)}
-      />
+      <Space direction="vertical" size="large" style={{width: '100%'}}>
+        <span>展示首尾页码省略</span>
+        <Pagination total={100} pageSize={pageSize} onChange={onChange} onPageSizeChange={(v) => changePageSize(v)} />
+        <span>不展示首尾页码省略</span>
+        <Pagination
+          total={100}
+          pageSize={pageSize}
+          onChange={onChange}
+          pageEllipsisMode="both-ends"
+          onPageSizeChange={(v) => changePageSize(v)}
+          />
+      </Space>
     </div>
   );
 }
