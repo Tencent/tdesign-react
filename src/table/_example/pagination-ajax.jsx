c49603347fe8e8bfb21994c092e2baa2ec749c48
pengYYY <pengyue970715@gmail.com>
2022-12-08 18:07:55 +0800

    chore: snap update

--- src/table/_example/pagination-ajax.jsx
+++ src/table/_example/pagination-ajax.jsx
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { Table } from 'tdesign-react';
+import { Table, Tag } from 'tdesign-react';
+import { ErrorCircleFilledIcon, CheckCircleFilledIcon, CloseCircleFilledIcon } from 'tdesign-icons-react';
 
+const statusNameListMap = {
+  0: { label: '审批通过', theme: 'success', icon: <CheckCircleFilledIcon /> },
+  1: { label: '审批失败', theme: 'danger', icon: <CloseCircleFilledIcon /> },
+  2: { label: '审批过期', theme: 'warning', icon: <ErrorCircleFilledIcon /> },
+};
 const columns = [
   {
     colKey: 'row-select',
     type: 'multiple',
-    width: 64,
-  },
-  {
-    colKey: 'serial-number',
-    title: '序号',
-    width: 60,
+    width: 46,
   },
   {
     width: 200,
     colKey: 'name',
     title: '姓名',
-    render({ row: { name } }) {
-      return name ? `${name?.first} ${name?.last}` : 'UNKNOWN_USER';
+    render({ type, row: { name } }) {
+      if (type === 'title') return '申请人';
+      return name ? `${name.first} ${name.last}` : 'UNKNOWN_USER';
     },
   },
   {
-    width: 200,
-    colKey: 'gender',
-    title: '性别',
+    colKey: 'status',
+    title: '申请状态',
+    width: '150',
+    cell: ({ rowIndex }) => {
+      const status = rowIndex % 3;
+      return (
+        <Tag shape="round" theme={statusNameListMap[status].theme} variant="light-outline">
+          {statusNameListMap[status].icon}
+          {statusNameListMap[status].label}
+        </Tag>
+      );
+    },
   },
   {
     width: 200,
@@ -34,9 +45,9 @@ const columns = [
     },
   },
   {
-    width: 260,
     colKey: 'email',
     title: '邮箱',
+    width: 180,
     ellipsis: true,
   },
 ];
@@ -59,7 +70,7 @@ export default function TableBasic() {
 
   // 模拟远程请求
   async function fetchData(pageInfo) {
-    setIsLoading(true);
+    // setIsLoading(true);
     try {
       const { current, pageSize } = pageInfo;
       // 请求可能存在跨域问题
