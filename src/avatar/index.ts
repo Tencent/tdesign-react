17f7e9c8b126e813cc1c79e0dd903d4bc1b38e20
HQ-Lin <a154571896@gmail.com>
2023-01-13 17:22:12 +0800

    feat: 移除 use-resize-observer 依赖

--- src/avatar/index.ts
+++ src/avatar/index.ts
@@ -3,7 +3,7 @@ import _Avatar from './Avatar';
 import './style/index.js';
 
 export type { AvatarProps } from './Avatar';
-export type { AvatarGroupProps } from './AvararGroup';
+export type { AvatarGroupProps } from './AvatarGroup';
 export * from './type';
 
 export const Avatar = _Avatar;
