cc8ca24e5bf4c2dab88a2599a55783b44174c52a
sheepluo <yaoyanhuoyi@qq.com>
2023-01-16 16:06:54 +0800

    [Avatar] Unit Tests (#1887)
    * test(avatar): add unit tests
    
    * test(avatar): update tests
    
    * avatar组件测试用例完善 (#1894)
    
    * feat(popconfirm): popconfirm组件demo同步,e2e单 测
    
    popconfirm组件demo同步,e2e单 测
    
    * feat(popconfirm): popconfirm组件demo同步,e2e单测
    
    popconfirm组件demo同步,e2e单测
    
    * fix: fix watermark removeable
    
    * fix: fix watermask
    
    * feat: mock ResizeObserver
    
    * feat: mock
    
    * feat: 输出完整测试用例,覆盖率达95%+
    
    * feat: 输出完整测试用例,覆盖率达95%+
    
    * feat(输出完整测试用例,覆盖率达95%+): 输出完整测试用例,覆盖率达95%+
    
    * feat: test(avatar):输出完整测试用例,覆盖率 达95%+
    
    * feat: test(avatar):输出完整测试用例,覆盖率 达95%+
    
    * feat: fix commit
    
    Co-authored-by: Shan <haishanb@163.com>

---
+++ src/avatar/__tests__/mount.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@test/utils';
+import { Avatar } from '..';
+
+// Avatar 数量变化会影响测试用例结果
+export function getAvatarGroupDefaultMount(AvatarGroup, props, events) {
+  return render(
+    <AvatarGroup {...props} {...events}>
+      <Avatar image="https://tdesign.gtimg.com/site/avatar.jpg" />
+      <Avatar>W</Avatar>
+      <Avatar icon={() => <i class="custom-node"></i>} />
+      <Avatar size="small">TD</Avatar>
+      <Avatar size="large">ME</Avatar>
+    </AvatarGroup>
+  );
+}
+
+export default getAvatarGroupDefaultMount;
