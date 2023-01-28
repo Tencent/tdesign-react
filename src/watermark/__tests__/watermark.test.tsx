9a31e8ba125264611984568834676040f5f3b5ee
duanbaosheng <1065140483@qq.com>
2023-01-28 21:54:01 +0800

    test(watermark): add test to wartermark

--- src/watermark/__tests__/watermark.test.tsx
+++ src/watermark/__tests__/watermark.test.tsx
@@ -3,6 +3,8 @@ import { render } from '@test/utils';
 import Watermark from '../Watermark';
 
 describe('Watermark 组件测试', () => {
+  const childTestID = 'childTestID';
+
   function renderWatermark(watermark) {
     const { container } = render(watermark);
     return container.firstChild;
@@ -14,7 +16,32 @@ describe('Watermark 组件测试', () => {
         <div style={{ height: 300 }}></div>
       </Watermark>,
     );
+
     expect(watermark).toHaveClass('t-watermark');
     expect(watermark.lastChild).toHaveStyle({ 'background-repeat': 'repeat' });
+  });
+
+  test('movable ', async () => {
+    const watermark = renderWatermark(
+      <Watermark movable watermarkContent={{ text: '@水印' }} y={100}>
+        <div style={{ height: 300 }}></div>
+      </Watermark>,
+    );
+
+    expect(watermark.lastChild).toHaveStyle({ 'background-repeat': 'no-repeat' });
+  });
+
+  test('base', async () => {
+    const wrapper = render(
+      <Watermark watermarkContent={{ text: '@水印' }} y={100}>
+        <div data-testid={childTestID} style={{ height: 300 }}></div>
+      </Watermark>,
+    );
+    const child = document.createElement('div');
+    child.innerText = 'testing';
+    child.id = 'test';
+    const targetDom = wrapper.getByTestId(childTestID);
+    targetDom.append(child);
+    expect(targetDom.firstChild).toEqual(child);
   });
 });
