2aaa3968c0cde8b915d5ff0a9d77e17d93b81deb
duanbaosheng <1065140483@qq.com>
2023-01-31 16:53:58 +0800

    test(image): add lazyload test (#1926)
    * docs(merge codes): merge codes
    
    * feat(merge codes): merge new codes
    
    * test(image): test the lazy property of the image component
    
    * test(image): test the lazy property of the image component
    
    N
    
    * test(image): add test to the image
    
    * test(image): add tests the image component
    
    * test(image): add test to image
    
    * test(image): add test to image

--- src/image/__tests__/vitest-image.test.jsx
+++ src/image/__tests__/vitest-image.test.jsx
@@ -5,11 +5,30 @@
  * If you need to modify this file, contact PMC first please.
  */
 import React from 'react';
-import { fireEvent, vi, render, mockDelay, simulateImageEvent } from '@test/utils';
+import { fireEvent, vi, render, mockDelay, simulateImageEvent, mockIntersectionObserver } from '@test/utils';
 import { Image } from '..';
 import { getOverlayImageMount } from './mount';
+import Space from '../../space';
 
 describe('Image Component', () => {
+  
+  beforeAll(() => {
+    // 用于判断是否已经触发回调
+    window.observeCallbackhasCalled = false;
+    const observe = (element, callback) => {
+      // 监控图片容器的滚动事件，后面会触发容器的滚动。
+      element.parentNode.parentNode.addEventListener('scroll', () => {
+        if (window.observeCallbackhasCalled) {
+          callback([{ isIntersecting: false }]);
+        } else {
+          window.observeCallbackhasCalled= true;
+          callback([{ isIntersecting: true }]);
+        }
+      });
+    }
+    mockIntersectionObserver({}, { observe });
+  });
+
   it('props.alt works fine', () => {
     const wrapper = render(<Image alt="text image load failed" src="https://www.error.img.com"></Image>);
     const container = wrapper.container.querySelector('img');
@@ -131,4 +150,31 @@ describe('Image Component', () => {
     expect(onLoadFn1).toHaveBeenCalled(1);
     expect(onLoadFn1.mock.calls[0][0].e.type).toBe('load');
   });
+
+  it('props.lazy works fine', () => {
+    document.body.height = 250;
+
+    const { container } = render(
+      <Space style={{height: 240, width: 240, overflow: 'hidden', overflowY: 'scroll', paddingTop: 500}}>
+        {
+          Array.from({length: 16}).map((_, index) => (
+            <Image
+              key={index}
+              src="https://tdesign.gtimg.com/demo/demo-image-1.png"
+              style={{width: 230, height: 120}}
+              lazy
+            />
+          ))
+        }
+      </Space>
+    );
+    
+    const spaceElement = container.querySelector('.t-space');
+    fireEvent.scroll(spaceElement, { target: { scrollY: 400 } });
+    // 滚动后，第一张图片会加载，但后面的图片不会加载
+    expect(spaceElement.firstChild.querySelector('img')).not.toBeNull()
+    expect(spaceElement.lastChild.querySelector('img')).toBeNull()
+    
+  });
+
 });
