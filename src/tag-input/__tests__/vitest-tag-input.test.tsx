78001a2fbab679a71a70122d3f031e18382dbae9
duanbaosheng <1065140483@qq.com>
2023-02-02 16:04:52 +0800

    test(tag): tag/tag input drag test (#1937)
    * docs(merge codes): merge codes
    
    * feat(merge codes): merge new codes
    
    * test(tag-inputy): tag-input
    
    * test(tag-input): add test to tag-input
    
    * test(tag-input): add test to tag-input

--- src/tag-input/__tests__/vitest-tag-input.test.tsx
+++ src/tag-input/__tests__/vitest-tag-input.test.tsx
@@ -10,6 +10,9 @@ import { TagInput } from '..';
 import { getTagInputValueMount, getTagInputDefaultMount } from './mount';
 
 describe('TagInput Component', () => {
+  const mockFn = vi.spyOn(HTMLSpanElement.prototype, 'getBoundingClientRect');
+  mockFn.mockImplementation(() => ({ width: 20, x: 5, clientX: 5 }));
+
   it('props.clearable: empty TagInput does not need clearIcon', async () => {
     const { container } = render(<TagInput clearable={true}></TagInput>);
     fireEvent.mouseEnter(container.querySelector('.t-input'));
@@ -412,5 +415,36 @@ describe('TagInput Component', () => {
     expect(onRemoveFn.mock.calls[0][0].trigger).toBe('tag-remove');
     expect(onRemoveFn.mock.calls[0][0].item).toBe('tdesign-vue');
     expect(onRemoveFn.mock.calls[0][0].e.type).toBe('click');
+  });
+
+  it('events.drag: dragSort', () => {
+    const defaultValue = ['Vue', 'React', 'Angular'];
+    const onDragSort = vi.fn(() => {
+      // 模拟顺序交换
+      const tagBox = document.querySelectorAll('.t-input__prefix').item(0);
+      const vueTag = document.querySelectorAll('.t-tag').item(0);
+      const reactTag = document.querySelectorAll('.t-tag').item(1);
+      const cloneReact = reactTag.cloneNode(true);
+      tagBox.insertBefore(cloneReact, vueTag);
+      tagBox.removeChild(reactTag);
+    });
+    const { container } = getTagInputValueMount(TagInput, { dragSort: true, value: defaultValue }, { onDragSort });
+
+    fireEvent.dragStart(container.querySelectorAll('.t-tag').item(1), {
+      dataTransfer: {
+        currentIndex: 1,
+        targetIndex: 0,
+      },
+    });
+
+    fireEvent.dragOver(container.querySelectorAll('.t-tag').item(0), {
+      dataTransfer: {
+        currentIndex: 1,
+        targetIndex: 0,
+      },
+    });
+    expect(onDragSort).toHaveBeenCalled(1);
+    expect(onDragSort.mock.calls[0][0].target).toEqual('Vue');
+    expect(container.querySelectorAll('.t-tag').item(0).firstChild.title).toEqual('React');
   });
 });
