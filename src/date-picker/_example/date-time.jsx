98ea1109b64730e68928c40c01a02e434e3d8b8e
Uyarn <uyarnchen@gmail.com>
2023-03-29 21:26:38 +0800

    fix(date-picker): fix 12 hour format

--- src/date-picker/_example/date-time.jsx
+++ src/date-picker/_example/date-time.jsx
@@ -2,18 +2,26 @@ import React, { useState } from 'react';
 import { DatePicker, Space } from 'tdesign-react';
 
 export default function YearDatePicker() {
+  const [value, setValue] = useState('2022-02-02 12:11:11');
+  const [value2, setValue2] = useState('2022-02-02 am 12:11:11');
+
   function handleChange(value) {
     console.log(value);
     setValue(value);
   }
 
-  const [value, setValue] = useState('2022-02-02 12:11:11');
-
   return (
     <Space direction="vertical">
       <DatePicker enableTimePicker value={value} onChange={handleChange} allowInput clearable />
 
-      <DatePicker enableTimePicker value={value} onChange={handleChange} allowInput clearable format="YYYY-MM-DD a hh:mm:ss" />
+      <DatePicker
+        enableTimePicker
+        value={value2}
+        onChange={(v) => setValue2(v)}
+        allowInput
+        clearable
+        format="YYYY-MM-DD a hh:mm:ss"
+      />
     </Space>
   );
 }
