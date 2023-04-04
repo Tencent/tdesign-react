1864391fa6dbd0b60282c2c0ae077cb5fe1fc8da
Kyrielin <a154571896@gmail.com>
2023-04-04 16:18:32 +0800

    fix: 修复React 类型问题 (#2125)

--- src/breadcrumb/BreadcrumbProps.ts
+++ src/breadcrumb/BreadcrumbProps.ts
@@ -1,13 +1,10 @@
 import React from 'react';
+import { StyledProps } from '../common';
 import { TdBreadcrumbItemProps, TdBreadcrumbProps } from './type';
 
-export interface BreadcrumbProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    React.PropsWithChildren<TdBreadcrumbProps> {}
+export interface BreadcrumbProps extends StyledProps, React.PropsWithChildren<TdBreadcrumbProps> {}
 
-export interface BreadcrumbItemProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    React.PropsWithChildren<TdBreadcrumbItemProps> {
+export interface BreadcrumbItemProps extends StyledProps, React.PropsWithChildren<TdBreadcrumbItemProps> {
   separator?: TdBreadcrumbProps['separator'];
   maxItemWidth?: TdBreadcrumbProps['maxItemWidth'];
 }
