3e2a7c8012cedd3c7b695cc56bcee0dbf0ccda4e
Uyarn <uyarnchen@gmail.com>
2023-04-12 10:01:40 +0800

    fix(Select): multiple group style

--- src/select/_example/group.jsx
+++ src/select/_example/group.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
-import { Select } from 'tdesign-react';
+import { Select, Space } from 'tdesign-react';
 
 const { Option, OptionGroup } = Select;
 
 const OptionGroupSelect = () => {
   const [value, setValue] = useState('');
+  const [value2, setValue2] = useState([1]);
+
   const onChange = (value) => {
     setValue(value);
   };
 
+  const onChange2 = (value) => {
+    setValue2(value);
+  };
   const options1 = [
     { label: '选项一', value: 1 },
     { label: '选项二', value: 2 },
@@ -27,23 +32,42 @@ const OptionGroupSelect = () => {
   ];
 
   return (
-    <Select value={value} onChange={onChange} style={{ width: '40%' }}>
-      <OptionGroup label="分组一" divider={true}>
-        {options1.map((item, index) => (
-          <Option label={item.label} value={item.value} key={index} />
-        ))}
-      </OptionGroup>
-      <OptionGroup label="分组二" divider={true}>
-        {options2.map((item, index) => (
-          <Option label={item.label} value={item.value} key={index} />
-        ))}
-      </OptionGroup>
-      <OptionGroup label="分组三" divider={true}>
-        {options3.map((item, index) => (
-          <Option label={item.label} value={item.value} key={index} />
-        ))}
-      </OptionGroup>
-    </Select>
+    <Space breakLine style={{ width: '100%' }}>
+      <Select value={value} onChange={onChange} style={{ width: '40%' }}>
+        <OptionGroup label="分组一" divider={true}>
+          {options1.map((item, index) => (
+            <Option label={item.label} value={item.value} key={index} />
+          ))}
+        </OptionGroup>
+        <OptionGroup label="分组二" divider={true}>
+          {options2.map((item, index) => (
+            <Option label={item.label} value={item.value} key={index} />
+          ))}
+        </OptionGroup>
+        <OptionGroup label="分组三" divider={true}>
+          {options3.map((item, index) => (
+            <Option label={item.label} value={item.value} key={index} />
+          ))}
+        </OptionGroup>
+      </Select>
+      <Select value={value2} onChange={onChange2} style={{ width: '40%' }} multiple>
+        <OptionGroup label="分组一" divider={true}>
+          {options1.map((item, index) => (
+            <Option label={item.label} value={item.value} key={index} />
+          ))}
+        </OptionGroup>
+        <OptionGroup label="分组二" divider={true}>
+          {options2.map((item, index) => (
+            <Option label={item.label} value={item.value} key={index} />
+          ))}
+        </OptionGroup>
+        <OptionGroup label="分组三" divider={true}>
+          {options3.map((item, index) => (
+            <Option label={item.label} value={item.value} key={index} />
+          ))}
+        </OptionGroup>
+      </Select>
+    </Space>
   );
 };
 
