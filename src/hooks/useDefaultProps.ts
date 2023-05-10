20df05a2d7dce33474bca0298e400d590b587415
lijianan <574980606@qq.com>
2023-05-10 09:36:44 +0800

    refactor: support ref props & remove backTopDefaultProps (#2202)
    * refactor: 💡 remove backTopDefaultProps
    
    remove backTopDefaultProps
    
    BREAKING CHANGE: 🧨 none
    
    ✅ Closes: none
    
    * feat: 🎸 support ref props
    
    support ref props
    
    BREAKING CHANGE: 🧨 none
    
    ✅ Closes: none

--- src/hooks/useDefaultProps.ts
+++ src/hooks/useDefaultProps.ts
@@ -2,8 +2,8 @@ import { useMemo } from 'react';
 
 // defaultProps 将于 18.3.0 废弃，故需实现 hook 在组件内部兼容
 // https://github.com/facebook/react/pull/16210
-export default function useDefaultProps<T>(originalProps: T, defaultProps: Record<string, any>): T {
-  return useMemo(() => {
+export default function useDefaultProps<T>(originalProps: T, defaultProps: Record<PropertyKey, any>): T {
+  return useMemo<T>(() => {
     // eslint-disable-next-line
     const props = Object.assign({}, originalProps);
     Object.keys(defaultProps).forEach((key) => {
