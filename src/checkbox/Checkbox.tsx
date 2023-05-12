d3ed2f06c2a555ffce62f21cf3fb42cd4ae5ae30
栗嘉男 <574980606@qq.com>
2023-05-12 14:13:48 +0800

    fix: 🐛 remove useDefaultProps
    remove useDefaultProps

--- src/checkbox/Checkbox.tsx
+++ src/checkbox/Checkbox.tsx
@@ -3,15 +3,17 @@ import forwardRefWithStatics from '../_util/forwardRefWithStatics';
 import Check, { CheckProps } from '../common/Check';
 import CheckboxGroup from './CheckboxGroup';
 import { checkboxDefaultProps } from './defaultProps';
+import useDefaultProps from '../hooks/useDefaultProps';
 
 export type CheckboxProps = Omit<CheckProps, 'type'>;
 
 const Checkbox = forwardRefWithStatics(
-  (props: CheckboxProps, ref: React.Ref<HTMLLabelElement>) => <Check ref={ref} type="checkbox" {...props} />,
+  (props: CheckboxProps, ref: React.Ref<HTMLLabelElement>) => (
+    <Check ref={ref} type="checkbox" {...useDefaultProps<CheckboxProps>(props, checkboxDefaultProps)} />
+  ),
   { Group: CheckboxGroup },
 );
 
 Checkbox.displayName = 'Checkbox';
-Checkbox.defaultProps = checkboxDefaultProps;
 
 export default Checkbox;
