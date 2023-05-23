20cf46fedf37dd5e0d6d0f806be0bb84d06bf2b7
栗嘉男 <574980606@qq.com>
2023-05-23 12:26:17 +0800

    fix: 🐛 fix
    fix

--- src/drawer/__tests__/drawer.test.tsx
+++ src/drawer/__tests__/drawer.test.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { fireEvent, render, vi } from '@test/utils';
-import Drawer from '../index';
+import Drawer, { DrawerProps } from '../index';
 
 function DrawerDemo(props) {
   const [visible, setVisible] = useState(false);
@@ -11,25 +11,16 @@ function DrawerDemo(props) {
   const handleClose = () => {
     setVisible(false);
   };
+  const drawerProps: DrawerProps = {};
+  Object.keys(props).forEach((key) => {
+    if (props[key] !== undefined) {
+      drawerProps[key] = props[key];
+    }
+  });
   return (
     <>
       <div onClick={handleClick}>Open</div>
-      <Drawer
-        header={props.header || '这是标题'}
-        footer={props.footer}
-        visible={visible}
-        onClose={handleClose}
-        closeBtn={props.closeBtn}
-        confirmBtn={props.confirmBtn}
-        cancelBtn={props.cancelBtn}
-        showOverlay={props.notShowOverlay ? false : true}
-        mode={props.mode || 'overlay'}
-        placement={props.placement || 'right'}
-        attach={props.attach}
-        destroyOnClose={props.destroyOnClose || false}
-        size={props.size || 'small'}
-        onCancel={props.onCancel}
-      >
+      <Drawer visible={visible} onClose={handleClose} showOverlay={!props.notShowOverlay} {...drawerProps}>
         <p>This is a drawer</p>
       </Drawer>
     </>
