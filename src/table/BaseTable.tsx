d607c2eb066cc9ccec8a67bcb0595c3fd782d1df
chaishi <974383157@qq.com>
2023-06-18 22:07:19 +0800

    fix(table): fixed header style error in virtual scroll

--- src/table/BaseTable.tsx
+++ src/table/BaseTable.tsx
@@ -293,6 +293,7 @@ const BaseTable = forwardRef<BaseTableRef, BaseTableProps>((props, ref) => {
    * Affixed Header
    */
   const renderFixedHeader = () => {
+    if (!props.showHeader) return null;
     // IE浏览器需要遮挡header吸顶滚动条，要减去getBoundingClientRect.height的滚动条高度4像素
     const IEHeaderWrap = getIEVersion() <= 11 ? 4 : 0;
     const barWidth = isWidthOverflow ? scrollbarWidth : 0;
@@ -301,14 +302,14 @@ const BaseTable = forwardRef<BaseTableRef, BaseTableProps>((props, ref) => {
     // 两类场景：1. 虚拟滚动，永久显示表头，直到表头消失在可视区域； 2. 表头吸顶，根据滚动情况判断是否显示吸顶表头
     const headerOpacity = headerAffixedTop ? Number(showAffixHeader) : 1;
     const affixHeaderWrapHeightStyle = {
-      width: `${tableWidth.current}px`,
+      width: `${tableWidth.current - barWidth}px`,
       height: `${affixHeaderWrapHeight}px`,
       opacity: headerOpacity,
     };
     const affixedHeader = Boolean((props.headerAffixedTop || virtualConfig.isVirtualScroll) && tableWidth.current) && (
       <div
         ref={affixHeaderRef}
-        style={{ width: `${tableWidth.current - affixedLeftBorder}px`, opacity: headerOpacity }}
+        style={{ width: `${tableWidth.current - barWidth - affixedLeftBorder}px`, opacity: headerOpacity }}
         className={classNames([
           'scrollbar',
           {
