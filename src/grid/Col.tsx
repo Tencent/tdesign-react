f94e7d636e482aa27a0b81cbc71483f96130ef01
栗嘉男 <574980606@qq.com>
2023-06-24 13:11:57 +0800

    fix: fix type

--- src/grid/Col.tsx
+++ src/grid/Col.tsx
@@ -102,10 +102,10 @@ const Col = forwardRef<HTMLElement, ColProps>((props, ref) => {
     className,
     {
       [`${classPrefix}-col-${span}`]: span !== undefined,
-      [`${classPrefix}-col-offset-${offset}`]: parseInt(offset, 10) >= 0,
-      [`${classPrefix}-col-pull-${pull}`]: parseInt(pull, 10) >= 0,
-      [`${classPrefix}-col-push-${push}`]: parseInt(push, 10) >= 0,
-      [`${classPrefix}-col-order-${order}`]: parseInt(order, 10) >= 0,
+      [`${classPrefix}-col-offset-${offset}`]: parseInt(offset as unknown as string, 10) >= 0,
+      [`${classPrefix}-col-pull-${pull}`]: parseInt(pull as unknown as string, 10) >= 0,
+      [`${classPrefix}-col-push-${push}`]: parseInt(push as unknown as string, 10) >= 0,
+      [`${classPrefix}-col-order-${order}`]: parseInt(order as unknown as string, 10) >= 0,
     },
     sizeClasses,
   );
