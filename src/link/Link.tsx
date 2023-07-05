b087d7f9d10001bd06087a448d697f76c48b5b6b
栗嘉男 <574980606@qq.com>
2023-07-05 10:44:47 +0800

    fix: 🐛 fix: 🐛 replace defaultProps with useDefaultProps

--- src/link/Link.tsx
+++ src/link/Link.tsx
@@ -4,62 +4,59 @@ import useConfig from '../hooks/useConfig';
 import { TdLinkProps } from './type';
 import parseTNode from '../_util/parseTNode';
 import { StyledProps } from '../common';
+import useDefaultProps from '../hooks/useDefaultProps';
+import { linkDefaultProps } from './defaultProps';
 
 export interface LinkProps extends TdLinkProps, StyledProps {}
 
-const Link = React.forwardRef(
-  (
-    {
-      children,
-      content,
-      className,
-      underline,
-      prefixIcon,
-      suffixIcon,
-      theme = 'default',
-      disabled,
-      hover = 'underline',
-      onClick,
-      href,
-      size,
-      ...props
-    }: LinkProps,
-    ref: React.RefObject<HTMLAnchorElement>,
-  ) => {
-    const { classPrefix } = useConfig();
+const Link = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
+  const {
+    children,
+    content,
+    className,
+    underline,
+    prefixIcon,
+    suffixIcon,
+    theme,
+    disabled,
+    hover,
+    onClick,
+    href,
+    size,
+    ...otherProps
+  } = useDefaultProps<LinkProps>(props, linkDefaultProps);
 
-    const childNode = content || children;
+  const { classPrefix } = useConfig();
 
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      if (disabled) return;
-      onClick?.(e);
-    };
+  const childNode = content || children;
 
-    return (
-      <a
-        {...props}
-        href={disabled || !href ? undefined : href}
-        ref={ref}
-        className={classNames(className, [`${classPrefix}-link`, `${classPrefix}-link--theme-${theme}`], {
-          [`${classPrefix}-size-s`]: size === 'small',
-          [`${classPrefix}-size-l`]: size === 'large',
-          [`${classPrefix}-is-disabled`]: !!disabled,
-          [`${classPrefix}-is-underline`]: !!underline,
-          [`${classPrefix}-link--hover-${hover}`]: !disabled,
-        })}
-        onClick={handleClick}
-      >
-        {prefixIcon && (
-          <span className={classNames([`${classPrefix}-link__prefix-icon`])}>{parseTNode(prefixIcon)}</span>
-        )}
-        {childNode}
-        {suffixIcon && (
-          <span className={classNames([`${classPrefix}-link__suffix-icon`])}>{parseTNode(suffixIcon)}</span>
-        )}
-      </a>
-    );
-  },
-);
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (disabled) {
+      return;
+    }
+    onClick?.(e);
+  };
+
+  return (
+    <a
+      {...otherProps}
+      href={disabled || !href ? undefined : href}
+      ref={ref}
+      className={classNames(className, [`${classPrefix}-link`, `${classPrefix}-link--theme-${theme}`], {
+        [`${classPrefix}-size-s`]: size === 'small',
+        [`${classPrefix}-size-l`]: size === 'large',
+        [`${classPrefix}-is-disabled`]: !!disabled,
+        [`${classPrefix}-is-underline`]: !!underline,
+        [`${classPrefix}-link--hover-${hover}`]: !disabled,
+      })}
+      onClick={handleClick}
+    >
+      {prefixIcon && <span className={classNames([`${classPrefix}-link__prefix-icon`])}>{parseTNode(prefixIcon)}</span>}
+      {childNode}
+      {suffixIcon && <span className={classNames([`${classPrefix}-link__suffix-icon`])}>{parseTNode(suffixIcon)}</span>}
+    </a>
+  );
+});
 
 Link.displayName = 'Link';
 
