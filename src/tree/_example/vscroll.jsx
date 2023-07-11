d90a7793c234ad35a15d4ae237d0e6df9dd20bf4
Uyarn <uyarnchen@gmail.com>
2023-07-11 21:36:55 +0800

    chore: update demo

--- src/tree/_example/vscroll.jsx
+++ src/tree/_example/vscroll.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tree, Switch, Space, Form } from 'tdesign-react';
-
-const options = [];
-for (let i = 0; i < 3000; i++) {
-  options.push({
-    label: `第${i + 1}段`,
-    value: i,
-    children: [
-      {
-        label: `第${i + 1}段第1个子节点`,
-        value: `${i}.1`,
-      },
-      {
-        label: `第${i + 1}段第2个子节点`,
-        value: `${i}.2`,
-      },
-    ],
-  });
-}
 
 export default () => {
   const [checkable, setCheckable] = useState(true);
   const [showLine, toggleShowLine] = useState(true);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    const newOptions = [];
+    for (let i = 0; i < 3000; i++) {
+      newOptions.push({
+        label: `第${i + 1}段`,
+        value: i,
+        children: [
+          {
+            label: `第${i + 1}段第1个子节点`,
+            value: `${i}.1`,
+          },
+          {
+            label: `第${i + 1}段第2个子节点`,
+            value: `${i}.2`,
+          },
+        ],
+      });
+    }
+    setOptions(newOptions);
+  }, []);
+
   const defaultChecked = ['1.2', '2.2'];
   return (
     <Space direction="vertical">
