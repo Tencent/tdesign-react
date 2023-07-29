68e955879ad379a37adafd78df34f07ff87cd5f7
栗嘉男 <574980606@qq.com>
2023-07-30 00:15:49 +0800

    fix: fix

--- src/radio/RadioGroup.tsx
+++ src/radio/RadioGroup.tsx
@@ -22,12 +22,21 @@ export interface RadioGroupProps extends TdRadioGroupProps, StyledProps {
 /**
  * 单选选项组，里面可以嵌套 <Radio />
  */
-const RadioGroup: React.FC<RadioGroupProps> = (originalProps) => {
+const RadioGroup: React.FC<RadioGroupProps> = (props) => {
   const { classPrefix } = useConfig();
 
-  const props = useDefaultProps<RadioGroupProps>(originalProps, radioGroupDefaultProps);
+  // const props = useDefaultProps<RadioGroupProps>(originalProps, radioGroupDefaultProps);
 
-  const { disabled, children, onChange, size, variant, options = [], className, style } = props;
+  const {
+    disabled,
+    children,
+    onChange,
+    size,
+    variant,
+    options = [],
+    className,
+    style,
+  } = useDefaultProps<RadioGroupProps>(props, radioGroupDefaultProps);
 
   const [internalValue, setInternalValue] = useControlled(props, 'value', onChange);
   const [barStyle, setBarStyle] = useState({});
