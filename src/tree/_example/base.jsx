6d1fd1421030aacf0ce19aeccf1dab83632e5262
Uyarn <uyarnchen@gmail.com>
2023-08-10 13:39:58 +0800

    feat: new icon2.0 version

--- src/tree/_example/base.jsx
+++ src/tree/_example/base.jsx
@@ -4,48 +4,14 @@ import { Tree } from 'tdesign-react';
 const items = [
   {
     label: '第一段',
-    children: [
-      {
-        label: '第二段',
-      },
-      {
-        label: '第二段',
-      },
-    ],
-  },
-  {
-    label: '第一段',
-    children: [
-      {
-        label: '第二段',
-      },
-      {
-        label: '第二段',
-      },
-    ],
-  },
-  {
-    label: '第一段',
-    children: [
-      {
-        label: '第二段',
-      },
-      {
-        label: '第二段',
-      },
-    ],
-  },
-  {
-    label: '第一段',
-    children: [
-      {
-        label: '第二段',
-      },
-      {
-        label: '第二段',
-      },
-    ],
+    children: [],
   },
 ];
 
-export default () => <Tree data={items} activable hover transition />;
+const render0perations = () => <div>111</div>;
+
+export default () => (
+  <>
+    <Tree data={items} line icon activable expandAll expandParent operations={render0perations} />
+  </>
+);
