be6410bb849d34dfcaaf487f0ec4ba2d375df51d
zzk <974758671@qq.com>
2023-10-29 20:30:12 +0800

    docs(table): the linkage between editable table editing components does not take effect

--- src/table/_example/editable-row.jsx
+++ src/table/_example/editable-row.jsx
@@ -113,7 +113,7 @@ export default function EditableRowTable() {
     // 以下内容应用于全量数据校验（单独的行校验不需要）
     if (openCheckAll) {
       data[rowIndex] = editedRow;
-      setData(data);
+      setData([...data]);
     }
   };
 
@@ -200,8 +200,8 @@ export default function EditableRowTable() {
               { label: '宣传物料制作费用', value: '宣传物料制作费用' },
               { label: 'algolia 服务报销', value: 'algolia 服务报销' },
               // 如果状态选择了 已过期，则 Letters 隐藏 G 和 H
-              { label: '相关周边制作费', value: '相关周边制作费', show: () => editedRow.status !== 0 },
-              { label: '激励奖品快递费', value: '激励奖品快递费', show: () => editedRow.status !== 0 },
+              { label: '相关周边制作费', value: '相关周边制作费', show: () => editedRow.status !== 1 },
+              { label: '激励奖品快递费', value: '激励奖品快递费', show: () => editedRow.status !== 1 },
             ].filter((t) => (t.show === undefined ? true : t.show())),
           }),
           showEditIcon: false,
