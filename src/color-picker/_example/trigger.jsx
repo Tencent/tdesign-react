2ae19b85f20f42d57941114ab6e104ebf149b999
Uyarn <uyarnchen@gmail.com>
2023-11-30 16:32:47 +0800

    fix(color-picker): fix hex alpha doesn't work

--- src/color-picker/_example/trigger.jsx
+++ src/color-picker/_example/trigger.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { ColorPicker } from 'tdesign-react';
 
 export default function PanelExample() {
-  return <ColorPicker defaultValue={'#0052d9'} showPrimaryColorPreview={false} />;
+  return <ColorPicker defaultValue={'#0052d9'} showPrimaryColorPreview={false} format="HEX" />;
 }
