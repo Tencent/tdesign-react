a9fd443cb9f01fbe39c0e3bd3b6a3e273ecafe93
Haixing <65376724+HaixingOoO@users.noreply.github.com>
2024-03-08 00:04:39 +0800

    test(list): complete list test (#2792)
    * test(list): complete list test
    
    * test(list): update test

--- src/list/__tests__/list.test.tsx
+++ src/list/__tests__/list.test.tsx
@@ -2,7 +2,7 @@ import { render, fireEvent, vi } from '@test/utils';
 import React from 'react';
 import List from '../List';
 
-const { ListItem } = List;
+const { ListItem, ListItemMeta } = List;
 
 describe('List 组件测试', () => {
   const data = [
@@ -50,6 +50,80 @@ describe('List 组件测试', () => {
       );
       fireEvent.scroll(container.firstChild);
       expect(fn).toHaveBeenCalled();
+    });
+
+    test('header and footer props', () => {
+      const header = 'header content';
+      const footer = 'footer content';
+      const { queryByText } = render(<List header={header} footer={footer} />);
+
+      expect(queryByText(header)).not.toBeNull();
+      expect(queryByText(header)).toBeInTheDocument();
+      expect(queryByText(footer)).not.toBeNull();
+      expect(queryByText(footer)).toBeInTheDocument();
+    });
+
+    test('asyncLoading props', () => {
+      const { container } = render(<List asyncLoading="loading" />);
+
+      expect(container.querySelector('.t-loading')).not.toBeNull();
+      expect(container.querySelector('.t-loading')).toBeInTheDocument();
+    });
+  });
+
+  describe('ListItem Component Test', () => {
+    test('content and children render', () => {
+      const contextText = 'content render';
+      const { container } = render(<ListItem content={<div id="content_id">{contextText}</div>} />);
+
+      const { container: childContainer } = render(
+        <ListItem>
+          <div id="child_test">{contextText}</div>
+        </ListItem>,
+      );
+
+      expect(container.querySelector('#content_id')).not.toBeNull();
+      expect(container.querySelector('#content_id')).toBeInTheDocument();
+      expect(childContainer.querySelector('#child_test')).not.toBeNull();
+      expect(childContainer.querySelector('#child_test')).toBeInTheDocument();
+    });
+
+    test('action', () => {
+      const { queryByText } = render(<ListItem action={<span>操作1</span>} />);
+
+      expect(queryByText('操作1')).not.toBeNull();
+      expect(queryByText('操作1')).toBeInTheDocument();
+    });
+  });
+
+  describe('ListItemMeta Component Test', () => {
+    const imgSrc = 'https://tdesign.gtimg.com/list-icon.png';
+    const description = 'Test Description';
+    test('image string', () => {
+      const { container } = render(<ListItemMeta image={imgSrc} />);
+
+      expect(container.querySelector('img')).not.toBeNull();
+      expect(container.querySelector('img')).toBeInTheDocument();
+    });
+    test('image TNode', () => {
+      const Img = () => <img id="img_test" src={imgSrc} alt="test img" />;
+      const { container } = render(<ListItemMeta image={<Img />} />);
+
+      expect(container.querySelector('#img_test')).not.toBeNull();
+      expect(container.querySelector('#img_test')).toBeInTheDocument();
+    });
+    test('description string', () => {
+      const { queryByText } = render(<ListItemMeta description={description} />);
+
+      expect(queryByText(description)).not.toBeNull();
+      expect(queryByText(description)).toBeInTheDocument();
+    });
+    test('description TNode', () => {
+      const Description = () => <div id="description_test">{description}</div>;
+      const { container } = render(<ListItemMeta description={<Description />} />);
+
+      expect(container.querySelector('#description_test')).not.toBeNull();
+      expect(container.querySelector('#description_test')).toBeInTheDocument();
     });
   });
 });
