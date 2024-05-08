9cb5792d13d0d82a6d6f66f5c3f69306be48eb91
zwkang <kang95630@gmail.com>
2024-05-08 21:51:17 +0800

    docs(dialog): add confirmLoading and setConfirmLoading example

--- src/dialog/_example/async.jsx
+++ src/dialog/_example/async.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { Dialog, Button } from 'tdesign-react';
+import { Dialog, Button, DialogPlugin } from 'tdesign-react';
+
+async function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time * 1000));
+}
 
 export default function BasicUsage() {
   const [visible, setVisible] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
     setVisible(true);
@@ -18,15 +22,44 @@ export default function BasicUsage() {
       setLoading(false);
     }, 2000);
   };
+
+  function showFunctionCallDialog() {
+    // 函数调用组件
+    const myDialog = DialogPlugin({
+      header: 'Basic Modal',
+      body: 'This is a dialog',
+      onConfirm: async ({ e }) => {
+        console.log('confirm clicked', e);
+        myDialog.setConfirmLoading(true);
+        await delay(3);
+        myDialog.setConfirmLoading(false);
+        myDialog.hide();
+      },
+      onClose: ({ e, trigger }) => {
+        console.log('e: ', e);
+        console.log('trigger: ', trigger);
+        myDialog.hide();
+      },
+      onCloseBtnClick: ({ e }) => {
+        console.log('close btn: ', e);
+      },
+    });
+  }
+
   return (
     <>
-      <Button theme="primary" onClick={handleClick}>
+      <Button theme="primary" onClick={handleClick} style={{ marginRight: 16 }}>
         Open Modal
       </Button>
+
+      <Button theme="primary" onClick={showFunctionCallDialog}>
+        Open Plugin Modal
+      </Button>
+
       <Dialog
         header="Basic Modal"
         visible={visible}
-        confirmBtn={<Button loading={loading}>保存中...</Button>}
+        confirmLoading={loading}
         onClose={handleClose}
         onConfirm={handleCloseAsync}
       >
