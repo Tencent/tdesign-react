3b985b543daeeae314e5ddaed3b60dc3d6f0d620
YiJie <51358815+NWYLZW@users.noreply.github.com>
2024-05-16 16:15:03 +0800

    feat(avatar): more events (#2906)
    * feat(avatar): more events
    
    * fix(avatar): typo
    
    * style(avatar): alignment event name style
    
    * Apply suggestions from code review
    
    * chore(avatar): update event description
    
    * style(avatar): simplify

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -27,6 +27,9 @@ const Avatar = forwardRefWithStatics(
       shape,
       size: avatarSize,
       onError,
+      onClick,
+      onHover,
+      onContextmenu,
       children,
       content,
       style,
@@ -117,6 +120,9 @@ const Avatar = forwardRefWithStatics(
         className={avatarClass}
         style={{ ...numSizeStyle, ...style }}
         {...avatarProps}
+        onClick={(e) => onClick?.({ e })}
+        onMouseEnter={(e) => onHover?.({ e })}
+        onContextMenu={(e) => onContextmenu?.({ e })}
       >
         {renderChildren}
       </div>
