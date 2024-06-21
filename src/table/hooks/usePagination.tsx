d4a55663e526749a1b3d3c8430fc41497b254476
HaixingOoO <974758671@qq.com>
2024-06-21 22:29:57 +0800

    chore(table): update code

--- src/table/hooks/usePagination.tsx
+++ src/table/hooks/usePagination.tsx
@@ -34,7 +34,6 @@ export default function usePagination(props: TdBaseTableProps) {
   useEffect(() => {
     if (!pagination) {
       setIsPaginateData(false);
-      setDataSource([]);
     }
   }, [pagination]);
 
