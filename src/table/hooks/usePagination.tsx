442b9533e1171190c2b721d92f2e199a489acb4f
HaixingOoO <974758671@qq.com>
2024-06-21 22:15:36 +0800

    fix(table): fix usePagination the isPagination issue
    fix #2957

--- src/table/hooks/usePagination.tsx
+++ src/table/hooks/usePagination.tsx
@@ -31,6 +31,13 @@ export default function usePagination(props: TdBaseTableProps) {
     [data, disableDataPage],
   );
 
+  useEffect(() => {
+    if (!pagination) {
+      setIsPaginateData(false);
+      setDataSource([]);
+    }
+  }, [pagination]);
+
   // 受控情况，只有 pagination.current 或者 pagination.pageSize 变化，才对数据进行排序
   useEffect(() => {
     if (!pagination || !pagination.current) return;
