f8a075ab90bc36adb93ad490e107cb57bf92a3c1
Haixing <65376724+HaixingOoO@users.noreply.github.com>
2024-08-20 15:40:45 +0800

    Update src/hooks/useMutationObserver.ts

--- src/hooks/useMutationObserver.ts
+++ src/hooks/useMutationObserver.ts
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from 'react';
 import debounce from 'lodash/debounce';
-import { isEqual } from 'lodash';
+import isEqual from 'lodash/isEqual';
 import useLatest from './useLatest';
 
 const DEFAULT_OPTIONS = {
