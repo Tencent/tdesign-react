9a5ff5b443272a758d9432b3abdb45c99be8bac8
junyuim <1861636+junyuim@users.noreply.github.com>
2024-09-29 17:45:54 +0800

    fix: Switch value is not in customValue error msg

--- src/switch/Switch.tsx
+++ src/switch/Switch.tsx
@@ -44,7 +44,7 @@ const Switch = React.forwardRef<HTMLButtonElement, SwitchProps>((originalProps, 
   };
 
   useEffect(() => {
-    if (Array.isArray(customValue) && !customValue.includes(value)) {
+    if (isControlled && Array.isArray(customValue) && !customValue.includes(value)) {
       log.error('Switch', `value is not in customValue: ${JSON.stringify(customValue)}`);
     }
     isControlled && setInnerChecked(value === activeValue);
