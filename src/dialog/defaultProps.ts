474ddf16d9cb0b99f45c0f888b854cb23e6e41fb
Wesley <985189328@qq.com>
2024-11-14 18:29:19 +0800

    fix: type

--- src/dialog/defaultProps.ts
+++ src/dialog/defaultProps.ts
@@ -2,7 +2,14 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdDialogProps } from './type';
+import { TdDialogCardProps, TdDialogProps } from './type';
+
+export const dialogCardDefaultProps: TdDialogCardProps = {
+  closeBtn: true,
+  footer: true,
+  header: true,
+  theme: 'default',
+};
 
 export const dialogDefaultProps: TdDialogProps = {
   closeBtn: true,
