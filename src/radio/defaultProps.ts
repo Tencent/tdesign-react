22432f4273bbb677b45ca40a072ecb495367485d
liweijie0812 <674416404@qq.com>
2024-12-17 17:48:18 +0800

    feat(radio): fix type and add readonly (#3280)
    * feat(RadioGroup): support readonly
    
    * docs: sync api
    
    * docs: update live demo
    
    * fix: fix type

--- src/radio/defaultProps.ts
+++ src/radio/defaultProps.ts
@@ -8,12 +8,14 @@ export const radioDefaultProps: TdRadioProps = {
   allowUncheck: false,
   defaultChecked: false,
   disabled: undefined,
+  readonly: undefined,
   value: undefined,
 };
 
 export const radioGroupDefaultProps: TdRadioGroupProps = {
   allowUncheck: false,
   disabled: undefined,
+  readonly: undefined,
   size: 'medium',
   variant: 'outline',
 };
