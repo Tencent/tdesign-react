86fba825ee6dcfce8c741274629001a081c3cc01
Rylan <rylanbot@qq.com>
2025-03-07 12:20:27 +0800

    docs(ConfigProvider): add a new global Pagination API demo

--- packages/components/config-provider/_example/pagination.tsx
+++ packages/components/config-provider/_example/pagination.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { merge } from 'lodash-es';
 import { ConfigProvider, Pagination } from 'tdesign-react';
 import enConfig from 'tdesign-react/es/locale/en_US';
+import { PageInfo } from '../type';
 
 export default function configDemo() {
   // 全局特性配置，可以引入英文默认配置 enConfig，还可以在默认配置的基础上进行自定义配置
@@ -10,6 +11,12 @@ export default function configDemo() {
       itemsPerPage: '{size} / page',
       jumpTo: 'jump to',
       total: 'Total {total} items',
+      onChange(pageInfo: PageInfo) {
+        console.log('onChange', pageInfo);
+      },
+      onCurrentChange(current: number, pageInfo: PageInfo) {
+        console.log('onCurrentChange', current, pageInfo);
+      }
     },
   });
 
