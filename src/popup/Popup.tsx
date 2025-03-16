6c1991019ee4c01fc223324e136b6c03def91986
Uyarn <uyarnchen@gmail.com>
2025-03-16 17:21:52 +0800

    chore: replace react-popper

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -3,7 +3,6 @@ import { CSSTransition } from 'react-transition-group';
 import isFunction from 'lodash/isFunction';
 import debounce from 'lodash/debounce';
 import classNames from 'classnames';
-import { usePopper } from 'react-popper';
 import { Placement } from '@popperjs/core';
 import useControlled from '../hooks/useControlled';
 import useAnimation from '../hooks/useAnimation';
@@ -18,6 +17,8 @@ import useWindowSize from '../hooks/useWindowSize';
 import { popupDefaultProps } from './defaultProps';
 import useDefaultProps from '../hooks/useDefaultProps';
 import useAttach from '../hooks/useAttach';
+import usePopper from '../hooks/usePopper';
+
 import { getCssVarsValue } from '../_util/dom';
 
 export interface PopupProps extends TdPopupProps {
