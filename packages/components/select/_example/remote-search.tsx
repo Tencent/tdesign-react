5bd962f5490a704cc8895f05d4345690ffad8dac
RSS1102 <jimmyrss1102@gmail.com>
2025-04-16 11:15:40 +0800

    chore(Select): optimization remote-search example

--- packages/components/select/_example/remote-search.tsx
+++ packages/components/select/_example/remote-search.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { Select } from 'tdesign-react';
+import { Select, Space } from 'tdesign-react';
 
 const RemoteSearchSelect = () => {
   const [value, setValue] = useState('');
-
   const [loading, setLoading] = useState(false);
   const [options, setOptions] = useState([]);
+
+  const [multipleValue, setMultipleValue] = useState([]);
+  const [multipleLoading, setMultipleLoading] = useState(false);
+  const [multipleOptions, setMultipleOptions] = useState([]);
 
   const onChange = (value: string) => {
     setValue(value);
@@ -38,16 +41,78 @@ const RemoteSearchSelect = () => {
     }, 500);
   };
 
+  const handleMultipleRemoteSearch = (search: string) => {
+    setMultipleLoading(true);
+
+    setTimeout(() => {
+      setMultipleLoading(false);
+      if (search) {
+        const remoteOptions = [
+          {
+            value: `腾讯_test1`,
+            label: `腾讯_test1`,
+          },
+          {
+            value: `腾讯_test2`,
+            label: `腾讯_test2`,
+          },
+          {
+            value: `腾讯_test3`,
+            label: `腾讯_test3`,
+          },
+          {
+            value: `腾讯_test1_1`,
+            label: `腾讯_test1_1`,
+          },
+          {
+            value: `腾讯_test2_2`,
+            label: `腾讯_test2_2`,
+          },
+          {
+            value: `腾讯_test3_3`,
+            label: `腾讯_test3_3`,
+          },
+        ].filter((item) => item.label.includes(search));
+
+        const mergedOptions = [...remoteOptions, ...multipleValue];
+        const multipleOptions = Array.from(new Map(mergedOptions.map((item) => [item.value, item])).values());
+
+        setMultipleOptions(multipleOptions);
+      } else {
+        setMultipleOptions(multipleValue);
+      }
+    }, 500);
+  };
+
+  const onMultipleChange = (value: string[]) => {
+    setMultipleValue(value);
+  };
+
   return (
-    <Select
-      filterable
-      value={value}
-      onChange={onChange}
-      style={{ width: '40%' }}
-      loading={loading}
-      onSearch={handleRemoteSearch}
-      options={options}
-    />
+    <Space direction="vertical" style={{ width: 350 }}>
+      <Select
+        value={value}
+        filterable
+        placeholder="请选择"
+        loading={loading}
+        options={options}
+        onSearch={handleRemoteSearch}
+        onChange={onChange}
+      />
+
+      <Select
+        value={multipleValue}
+        valueType="object"
+        filterable
+        placeholder="请选择"
+        loading={multipleLoading}
+        options={multipleOptions}
+        onSearch={handleMultipleRemoteSearch}
+        onChange={onMultipleChange}
+        multiple
+        reserveKeyword
+      />
+    </Space>
   );
 };
 
