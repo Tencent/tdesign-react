66ef80f6f806ebc372c892cf1f227424327d0693
betavs <betavs@qq.com>
2025-04-28 10:35:30 +0800

    fix(select): improve select component behavior and trigger handling

--- packages/components/input/Input.tsx
+++ packages/components/input/Input.tsx
@@ -224,7 +224,7 @@ const Input = forwardRefWithStatics(
         ref={inputRef}
         placeholder={placeholder}
         type={renderType}
-        className={`${classPrefix}-input__inner`}
+        className={`${classPrefix}-input__inner${showInput ? '' : ` ${classPrefix}-input--soft-hidden`}`}
         value={formatDisplayValue}
         readOnly={isInnerInputReadonly}
         disabled={disabled}
@@ -268,7 +268,7 @@ const Input = forwardRefWithStatics(
       >
         {prefixIconContent}
         {labelContent ? <div className={`${classPrefix}-input__prefix`}>{labelContent}</div> : null}
-        {showInput && renderInput}
+        {renderInput}
         {autoWidth && (
           <span ref={inputPreRef} className={`${classPrefix}-input__input-pre`}>
             {innerValue || placeholder}
