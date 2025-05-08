9d6c93c5b747d475e1a5390afe25ab5762a12335
Heising <heising@travelconnect.cn>
2025-05-08 10:49:47 +0800

    fix(Plugin): fix call plugin error

--- packages/components/_util/react-render.ts
+++ packages/components/_util/react-render.ts
@@ -33,6 +33,8 @@ try {
 }
 
 function toggleWarning(skip: boolean) {
+  const mainVersion = Number((version || '').split('.')[0]);
+  if (mainVersion >= 19) return;
   const { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } = fullClone;
 
   if (
