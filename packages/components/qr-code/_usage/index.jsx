20d55b54ee216c34fc5fd77157044348350a7d13
张宏 <wonkzhang@tencent.com>
2025-06-05 16:43:56 +0800

    feat(QRCode):添加_usage功能

---
+++ packages/components/qr-code/_usage/index.jsx
@@ -0,0 +1,46 @@
+/**
+ *  该脚本为自动生成，如有需要请在 /script/generate-usage.js 中调整
+ */
+
+// @ts-nocheck
+import React, { useState, useEffect, useMemo } from 'react';
+import BaseUsage, { useConfigChange, usePanelChange } from '@tdesign/react-site/src/components/BaseUsage';
+import jsxToString from 'react-element-to-jsx-string';
+
+import { QRCode } from 'tdesign-react';
+import configProps from './props.json';
+
+export default function Usage() {
+  const [configList, setConfigList] = useState(configProps);
+
+  const { changedProps, onConfigChange } = useConfigChange(configList);
+
+  const panelList = [{ label: 'QRCode', value: 'QRCode' }];
+
+  const { panel, onPanelChange } = usePanelChange(panelList);
+
+  const [renderComp, setRenderComp] = useState();
+
+  const defaultProps = { value: 'https://tdesign.tencent.com/' };
+
+  useEffect(() => {
+    setRenderComp(<QRCode {...defaultProps} {...changedProps} />);
+  }, [changedProps]);
+
+  const jsxStr = useMemo(() => {
+    if (!renderComp) return '';
+    return jsxToString(renderComp);
+  }, [renderComp]);
+
+  return (
+    <BaseUsage
+      code={jsxStr}
+      panelList={panelList}
+      configList={configList}
+      onPanelChange={onPanelChange}
+      onConfigChange={onConfigChange}
+    >
+      {renderComp}
+    </BaseUsage>
+  );
+}
--- script/generate-usage/config.js
+++ script/generate-usage/config.js
@@ -1152,4 +1152,21 @@ module.exports = {
       }, [changedProps]);
     `,
   },
+  'qr-code': {
+    importStr: `
+      import configProps from './props.json';\n
+      import { QRCode } from 'tdesign-react';\n`,
+    configStr: `
+      const [configList, setConfigList] = useState(configProps);
+    `,
+    panelStr: `
+      const panelList = [{ label: 'QRCode', value: 'QRCode' }];
+    `,
+    usageStr: `
+      const defaultProps = { value: 'https://tdesign.tencent.com/' };\n
+      useEffect(() => {
+        setRenderComp(<QRCode {...defaultProps} {...changedProps} />);
+      }, [changedProps]);
+    `,
+  },
 };
