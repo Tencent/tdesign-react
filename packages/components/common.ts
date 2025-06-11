25d4b6c166ca7cb4c3038cad3588410c3b6ea12c
lifeiFront <lifei971102991!163.com>
2025-06-11 19:35:44 +0800

    feat(二维码组件开发): 二维码组件开发
    二维码组件开发

--- packages/components/common.ts
+++ packages/components/common.ts
@@ -5,7 +5,7 @@ import { ReactElement, ReactNode, CSSProperties, FormEvent, DragEvent, Synthetic
 // TElement 表示 API 只接受传入组件
 export type TElement<T = undefined> = T extends undefined ? ReactElement : (props: T) => ReactElement;
 // 1. TNode = ReactNode; 2. TNode<T> = (props: T) => ReactNode
-export type TNode<T = undefined> = T extends undefined ? ReactNode | (() => ReactNode) : ReactNode | ((props: T) => ReactNode);
+export type TNode<T = undefined> = T extends undefined ? ReactNode : ReactNode | ((props: T) => ReactNode);
 
 export type AttachNodeReturnValue = HTMLElement | Element | Document;
 export type AttachNode = CSSSelector | ((triggerNode?: HTMLElement) => AttachNodeReturnValue);
@@ -17,7 +17,7 @@ export type ScrollContainer = (() => ScrollContainerElement) | CSSSelector;
 export type FormResetEvent = FormEvent<HTMLFormElement>;
 export type FormSubmitEvent = FormEvent<HTMLFormElement>;
 
-// 组件 TS 类型，暂定 any，可能调整为 () => JSX.Element
+// 组件 TS 类型，暂定 any，可能调整为 () => React.ReactElement
 export type ComponentType = any;
 
 export type Styles = CSSProperties;
