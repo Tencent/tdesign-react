411fad317a92aeb725ff23a3909f090760a26aba
lifeiFront <lifei971102991!163.com>
2025-07-03 17:36:52 +0800

    feat: 主题切换
    主题切换

--- packages/components/qrcode/_usage/index.jsx
+++ packages/components/qrcode/_usage/index.jsx
@@ -3,35 +3,36 @@
  */
 
 // @ts-nocheck
-import React, { useState, useEffect, useMemo } from 'react';
-import BaseUsage, { useConfigChange, usePanelChange } from '@tdesign/react-site/src/components/BaseUsage';
-import jsxToString from 'react-element-to-jsx-string';
+import React, { useState, useEffect, useMemo } from "react";
+import BaseUsage, {
+  useConfigChange,
+  usePanelChange,
+} from "@site/src/components/BaseUsage";
+import jsxToString from "react-element-to-jsx-string";
 
-import { QRCode } from 'tdesign-react';
-import configProps from './props.json';
+import configProps from "./props.json";
+
+import { QRCode } from "tdesign-react";
 
 export default function Usage() {
   const [configList, setConfigList] = useState(configProps);
 
   const { changedProps, onConfigChange } = useConfigChange(configList);
 
-  const panelList = [{ label: 'QRCode', value: 'QRCode' }];
+  const panelList = [{ label: "QRCode", value: "QRCode" }];
 
   const { panel, onPanelChange } = usePanelChange(panelList);
 
   const [renderComp, setRenderComp] = useState();
 
-  const defaultProps = {
-    value: 'https://tdesign.tencent.com/',
-    bgColor: '#fff',
-  };
+  const defaultProps = { value: "https://tdesign.tencent.com/" };
 
   useEffect(() => {
     setRenderComp(<QRCode {...defaultProps} {...changedProps} />);
   }, [changedProps]);
 
   const jsxStr = useMemo(() => {
-    if (!renderComp) return '';
+    if (!renderComp) return "";
     return jsxToString(renderComp);
   }, [renderComp]);
 
