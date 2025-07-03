428dd79615112531921f6832ccced6e2d6e55321
Heising <heising@travelconnect.cn>
2025-07-03 11:20:43 +0800

    chore: optimize the use of react19 adapter

--- packages/components/config-provider/index.ts
+++ packages/components/config-provider/index.ts
@@ -1,10 +1,9 @@
 import ConfigProvider, { merge } from './ConfigProvider';
 import ConfigContext from './ConfigContext';
-import { renderAdapter } from '../_util/react-render';
 
 export type { Config, Locale } from './ConfigContext';
 export type { ConfigProviderProps } from './ConfigProvider';
 export * from './type';
 
-export { ConfigContext, ConfigProvider, merge, renderAdapter };
+export { ConfigContext, ConfigProvider, merge };
 export default ConfigProvider;
