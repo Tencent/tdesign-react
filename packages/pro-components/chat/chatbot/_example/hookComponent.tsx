2876257c75f973fd8816193b93f73d5a42349f92
lincao <carolin913@126.com>
2025-07-24 20:13:15 +0800

    feat(chatbot): demo refine

--- packages/pro-components/chat/chatbot/_example/hookComponent.tsx
+++ packages/pro-components/chat/chatbot/_example/hookComponent.tsx
@@ -73,20 +73,17 @@ export default function ComponentsBuild() {
         }
       },
       // 自定义请求参数
-      onRequest: (innerParams: ChatRequestParams) => {
-        const { prompt } = innerParams;
-        return {
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-          },
-          body: JSON.stringify({
-            uid: 'abcd',
-            prompt,
-            think: true,
-            search: true,
-          }),
-        };
-      },
+      onRequest: (innerParams: ChatRequestParams) => ({
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+        },
+        body: JSON.stringify({
+          uid: 'abcd',
+          think: true,
+          search: true,
+          ...innerParams,
+        }),
+      }),
     },
   });
 
