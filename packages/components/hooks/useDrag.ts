31d5e817ea8b4157b59f9ea110bce20ce1ebf3e6
Rylan <rylanbot@qq.com>
2025-07-31 14:22:25 +0800

    feat(useDrag): enhance event handling to support mobile touch interactions

--- packages/components/hooks/useDrag.ts
+++ packages/components/hooks/useDrag.ts
@@ -1,12 +1,15 @@
-import { useRef, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import type { MutableRefObject } from 'react';
 
 export interface Coordinate {
   x: number;
   y: number;
 }
 
+export type DraggableEvent = MouseEvent | TouchEvent;
+
 interface DraggableCallback {
-  (coordinate: Coordinate, event?: MouseEvent): void;
+  (coordinate: Coordinate, event?: DraggableEvent): void;
 }
 
 interface DraggableProps {
@@ -15,54 +18,60 @@ interface DraggableProps {
   end?: DraggableCallback;
 }
 
-const useDrag = (ref, options: DraggableProps) => {
+const useDrag = (ref: MutableRefObject<HTMLDivElement>, options: DraggableProps) => {
   const { start, end, drag } = options;
 
   const isDraggingRef = useRef(false);
 
-  const getCoordinate = (event: MouseEvent) => {
+  const getCoordinate = (event: DraggableEvent) => {
     try {
       const rect = ref.current?.getBoundingClientRect();
-      const left = event.clientX - rect.left;
-      const top = event.clientY - rect.top;
+
+      const isTouchEvent = 'touches' in event;
+      const evt = isTouchEvent ? event.touches[0] || event.changedTouches[0] : event;
+      const left = evt.clientX - rect.left;
+      const top = evt.clientY - rect.top;
       return {
         y: Math.min(Math.max(0, top), rect.height),
         x: Math.min(Math.max(0, left), rect.width),
       };
     } catch (error) {
-      return {
-        y: null,
-        x: null,
-      };
+      return { y: null, x: null };
     }
   };
 
-  const handlePointerMove = (e: MouseEvent) => {
+  const handlePointerMove = (e: DraggableEvent) => {
     if (isDraggingRef.current) drag(getCoordinate(e), e);
   };
 
-  const handlePointerUp = (e: MouseEvent) => {
+  const handlePointerUp = (e: DraggableEvent) => {
     isDraggingRef.current = false;
     end(getCoordinate(e), e);
     document.removeEventListener('mouseup', handlePointerUp);
     document.removeEventListener('mousemove', handlePointerMove);
+    document.removeEventListener('touchend', handlePointerUp);
+    document.removeEventListener('touchmove', handlePointerMove);
   };
 
-  const handlePointerDown = (e: MouseEvent) => {
+  const handlePointerDown = (e: DraggableEvent) => {
     isDraggingRef.current = true;
-    start(getCoordinate(e), e);
+    start(getCoordinate(e), e as any);
     document.addEventListener('mouseup', handlePointerUp);
     document.addEventListener('mousemove', handlePointerMove);
+    document.addEventListener('touchend', handlePointerUp);
+    document.addEventListener('touchmove', handlePointerMove);
   };
 
   useEffect(() => {
     const element = ref.current;
     if (element) {
       element.addEventListener('mousedown', handlePointerDown);
+      element.addEventListener('touchstart', handlePointerDown, { passive: false });
     }
     return () => {
       if (element) {
         element.removeEventListener('mousedown', handlePointerDown);
+        element.removeEventListener('touchstart', handlePointerDown);
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
