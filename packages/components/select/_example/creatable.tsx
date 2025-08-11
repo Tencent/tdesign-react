d26e5f979808aea056b2d947e55b61003a59504d
wū yāng <uyarnchen@gmail.com>
2025-08-11 15:45:56 +0800

    feat(Select): support creatable in multiple mode (#3717)

--- packages/components/select/_example/creatable.tsx
+++ packages/components/select/_example/creatable.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Select } from 'tdesign-react';
+import { Select, Space } from 'tdesign-react';
 
 export default function SelectCreatable() {
   const [value, setValue] = useState('');
+  const [multipleValue, setMultipleValue] = useState([]);
   const [options, changeOptions] = useState([
     { label: '选项一', value: '1' },
     { label: '选项二', value: '2' },
@@ -11,18 +12,33 @@ export default function SelectCreatable() {
   const onChange = (value: string) => {
     setValue(value);
   };
+  const onMultipleChange = (value: string[]) => {
+    setMultipleValue(value);
+  };
   const handleOnCreate = (value: string) => {
     changeOptions(options.concat([{ value, label: value }]));
   };
   return (
-    <Select
-      value={value}
-      onChange={onChange}
-      style={{ width: '40%' }}
-      options={options}
-      filterable
-      creatable
-      onCreate={handleOnCreate}
-    />
+    <Space breakLine style={{ width: '100%' }}>
+      <Select
+        value={value}
+        onChange={onChange}
+        style={{ width: '40%' }}
+        options={options}
+        filterable
+        creatable
+        onCreate={handleOnCreate}
+      />
+      <Select
+        value={multipleValue}
+        onChange={onMultipleChange}
+        style={{ width: '40%' }}
+        options={options}
+        filterable
+        creatable
+        multiple
+        onCreate={handleOnCreate}
+      />
+    </Space>
   );
 }
