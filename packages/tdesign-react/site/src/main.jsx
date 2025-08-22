b3256446068998836a9be62d5ebe69a223659edf
黎伟杰 <674416404@qq.com>
2025-08-22 16:49:13 +0800

    chore: @tdesign/site-components instead of tdesign-site-components

--- packages/tdesign-react/site/src/main.jsx
+++ packages/tdesign-react/site/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { registerLocaleChange } from 'tdesign-site-components';
+import { registerLocaleChange } from '@tdesign/site-components';
 import App from './App';
 
 // import tdesign style;
 import '@tdesign/components/style/index.js';
 import '@tdesign/common-style/web/docs.less';
 
-import 'tdesign-site-components/lib/styles/style.css';
-import 'tdesign-site-components/lib/styles/prism-theme.less';
-import 'tdesign-site-components/lib/styles/prism-theme-dark.less';
+import '@tdesign/site-components/lib/styles/style.css';
+import '@tdesign/site-components/lib/styles/prism-theme.less';
+import '@tdesign/site-components/lib/styles/prism-theme-dark.less';
 
 import 'tdesign-icons-view';
 
