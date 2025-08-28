5105537fb78a9a5edf7d2a8bcf8f6f4e2c572ffb
Rylan <rylanbot@qq.com>
2025-08-28 15:03:42 +0800

    fix(Popup): optimize trigger ref handling

--- packages/components/popup/hooks/useTrigger.tsx
+++ packages/components/popup/hooks/useTrigger.tsx
@@ -1,17 +1,19 @@
-import React, { useRef, useEffect, isValidElement, useCallback, useMemo } from 'react';
+import React, { isValidElement, useEffect, useMemo, useRef } from 'react';
 import { isFragment } from 'react-is';
 import classNames from 'classnames';
-import { supportRef, getRefDom, getNodeRef } from '../../_util/ref';
 import composeRefs from '../../_util/composeRefs';
 import { off, on } from '../../_util/listener';
+import { getNodeRef, getRefDom, supportRef } from '../../_util/ref';
+import useConfig from '../../hooks/useConfig';
 
 const ESC_KEY = 'Escape';
 
 export default function useTrigger({ content, disabled, trigger, visible, onVisibleChange, triggerRef, delay }) {
+  const { classPrefix } = useConfig();
+
   const hasPopupMouseDown = useRef(false);
   const mouseDownTimer = useRef(0);
   const visibleTimer = useRef(null);
-  const triggerDataKey = useRef(`t-popup--${Math.random().toFixed(10)}`);
   const leaveFlag = useRef(false); // 防止多次触发显隐
 
   // 禁用和无内容时不展示
@@ -181,33 +183,24 @@ export default function useTrigger({ content, disabled, trigger, visible, onVisi
       },
     };
 
-    if (supportRef(triggerNode)) {
-      triggerProps.ref = composeRefs(triggerRef, getNodeRef(triggerNode as any));
-    } else {
-      // 标记 trigger 元素
-      triggerProps['data-popup'] = triggerDataKey.current;
-    }
-
+    triggerProps.ref = composeRefs(triggerRef, getNodeRef(triggerNode as any));
     return triggerProps;
   }
 
   // 整理 trigger 元素
   function getTriggerNode(children: React.ReactNode) {
     const triggerNode =
-      isValidElement(children) && !isFragment(children) ? children : <span className="t-trigger">{children}</span>;
+      isValidElement(children) && !isFragment(children) && supportRef(children) ? (
+        children
+      ) : (
+        <span className={`${classPrefix}-trigger`}>{children}</span>
+      );
 
     return React.cloneElement(triggerNode, getTriggerProps(triggerNode));
   }
 
-  // ref 透传失败时使用 dom 查找
-  const getTriggerDom = useCallback(() => {
-    if (typeof document === 'undefined') return {};
-    return document.querySelector(`[data-popup="${triggerDataKey.current}"]`);
-  }, []);
-
   return {
     getTriggerNode,
     getPopupProps,
-    getTriggerDom,
   };
 }
