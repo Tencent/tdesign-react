4548219f8e4e859969df3e4190de98edaa827d29
HaixingOoO <974758671@qq.com>
2025-09-06 22:49:07 +0800

    refactor(affix): refactor affix component

--- packages/components/_util/dom.ts
+++ packages/components/_util/dom.ts
@@ -92,3 +92,11 @@ export function getCurrentPrimaryColor(token: string): string {
   }
   return '';
 }
+
+export type BindElement = HTMLElement | Window | null | undefined;
+
+export function getTargetRect(target: BindElement): DOMRect {
+  return target !== window
+    ? (target as HTMLElement).getBoundingClientRect()
+    : ({ top: 0, bottom: window.innerHeight } as DOMRect);
+}
