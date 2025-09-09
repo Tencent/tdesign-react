60109622c1032891cc897efca346562480e3bb85
HaixingOoO <974758671@qq.com>
2025-09-09 22:36:42 +0800

    test(tooltip): fix Tooltip test case

--- packages/components/tooltip/__tests__/tooltip.test.tsx
+++ packages/components/tooltip/__tests__/tooltip.test.tsx
@@ -23,7 +23,8 @@ describe('Tooltip 组件测试', () => {
     expect(queryByTestId(tooltipTestId)).toBeNull();
 
     // 模拟鼠标进入
-    fireEvent.mouseEnter(getByText(triggerElement));
+    await fireEvent.mouseEnter(getByText(triggerElement));
+    await mockTimeout(() => false, 50);
 
     // 鼠标进入后，有元素，而且内容为 tooltipText
     const popupElement = queryByTestId(tooltipTestId);
@@ -34,7 +35,8 @@ describe('Tooltip 组件测试', () => {
     });
 
     // 模拟鼠标离开
-    fireEvent.mouseLeave(getByText(triggerElement));
+    await fireEvent.mouseLeave(getByText(triggerElement));
+    await mockTimeout(() => false, 50);
 
     // 鼠标离开，style 的 display 应该为 none
     const popupElement2 = queryByTestId(tooltipTestId);
@@ -53,7 +55,8 @@ describe('Tooltip 组件测试', () => {
         </Tooltip>,
       );
 
-      fireEvent.mouseEnter(result.getByText(triggerElement));
+      await fireEvent.mouseEnter(result.getByText(triggerElement));
+      await mockTimeout(() => false, 50);
       await waitFor(() => result.queryByTestId(tooltipTestId));
 
       return {
