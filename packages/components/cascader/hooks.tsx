d8c61e854b34468a40f85eb27eaa3ed0eafe2843
阿菜 Cai <jimmyrss1102@gmail.com>
2025-09-10 19:36:41 +0800

    fix(Cascader): fix `selectedOptions` param in `valueDisplay` (#3809)
    * fix(Cascader): fix `selectedOptions` param in `valueDisplay`
    
    * fix(Cascader): update getCascaderItems to handle multiple prop in options retrieval

--- packages/components/cascader/hooks.tsx
+++ packages/components/cascader/hooks.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useMemo, useRef } from 'react';
 
-import { isEqual, isFunction } from 'lodash-es';
+import { isArray, isEqual, isFunction } from 'lodash-es';
 
 import TreeStore from '@tdesign/common-js/tree-v1/tree-store';
 import type { TypeTreeNodeData } from '@tdesign/common-js/tree-v1/types';
@@ -184,13 +184,38 @@ export const useCascaderContext = (props: TdCascaderProps) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputVal, scopeVal]);
 
-  const getCascaderItems = (arrValue: CascaderValue[]) => {
+  const getCascaderItems = (
+    arrValue: CascaderValue[],
+    valueType: TdCascaderProps['valueType'],
+    multiple: TdCascaderProps['multiple'],
+  ) => {
     const { treeStore } = cascaderContext;
     const optionsData: TreeOptionData[] = [];
-    arrValue.forEach((value) => {
-      const nodes = treeStore?.getNodes(value);
-      nodes && nodes[0] && optionsData.push(nodes[0].data);
-    });
+
+    if (!treeStore) return optionsData;
+
+    if (valueType === 'full') {
+      if (multiple) {
+        // 未来需支持全路径拼接搜索
+        arrValue.forEach((value) => {
+          if (isArray(value) && value.length) {
+            const nodeValue = value[value.length - 1];
+            const [node] = treeStore.getNodes(nodeValue) || [];
+            node?.data && optionsData.push(node.data);
+          }
+        });
+      } else if (isArray(arrValue) && arrValue.length) {
+        const nodeValue = arrValue[arrValue.length - 1];
+        const [node] = treeStore.getNodes(nodeValue) || [];
+        node?.data && optionsData.push(node.data);
+      }
+    } else if (valueType === 'single') {
+      arrValue.forEach((value) => {
+        const [node] = treeStore.getNodes(value) || [];
+        node?.data && optionsData.push(node.data);
+      });
+    }
+
     return optionsData;
   };
 
