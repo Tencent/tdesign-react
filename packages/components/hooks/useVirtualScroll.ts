8988efb8e0654e4adaba7e87b27f1ddaa979242d
Rylan <rylanbot@qq.com>
2025-09-13 01:15:59 +0800

    chore(useVirtualScroll): refactor state management

--- packages/components/hooks/useVirtualScroll.ts
+++ packages/components/hooks/useVirtualScroll.ts
@@ -27,7 +27,9 @@ const requestAnimationFrame =
 
 const useVirtualScroll = (container: MutableRefObject<HTMLElement>, params: UseVirtualScrollParams) => {
   const { data, scroll } = params;
+
   const dataRef = useRef(data);
+  const containerHeight = useRef(0);
 
   /** 注意测试：数据长度为空；数据长度小于表格高度等情况。即期望只有数据量达到一定程度才允许开启虚拟滚动 */
   const [visibleData, setVisibleData] = useState<any[]>([]);
@@ -35,13 +37,11 @@ const useVirtualScroll = (container: MutableRefObject<HTMLElement>, params: UseV
   const [translateY, setTranslateY] = useState(() => (data?.length || 0) * (scroll?.rowHeight || 50));
   // 滚动高度，用于显示滚动条
   const [scrollHeight, setScrollHeight] = useState(0);
+  // 每一行到顶部的距离总和
   const trScrollTopHeightList = useRef<number[]>([]);
-  // 记录已经渲染计算过的所有子节点高度数组
+  // 每一行的实际高度
   const [trHeightList, setTrHeightList] = useState<number[]>([]);
-  const containerHeight = useRef(0);
-  const [startAndEndIndex, setStartAndEndIndex] = useState<[number, number]>(() => [0, (scroll?.bufferSize || 10) * 3]);
 
-  // 设置初始值
   const tScroll = useMemo(() => {
     if (!scroll) return {};
     return {
@@ -53,11 +53,12 @@ const useVirtualScroll = (container: MutableRefObject<HTMLElement>, params: UseV
       fixedRows: scroll.fixedRows ?? [0, 0],
     };
   }, [scroll]);
-
   const visibleCount = Math.min(tScroll.bufferSize * 3, data.length);
 
-  // 当前场景是否满足开启虚拟滚动的条件
   const isVirtualScroll = useMemo(() => tScroll.type === 'virtual' && tScroll.threshold < data.length, [tScroll, data]);
+
+  const [startAndEndIndex, setStartAndEndIndex] = useState<[number, number]>(() => [0, visibleCount]);
+
   const getTrScrollTopHeightList = (trHeightList: number[]) => {
     const list: number[] = [];
     // 大数据场景不建议使用 forEach 一类函数迭代
@@ -203,7 +204,6 @@ const useVirtualScroll = (container: MutableRefObject<HTMLElement>, params: UseV
         /**
         /* 进入这个分支的场景可能有：
          * - 初始化
-         * - 从非虚拟滚动切换到虚拟滚动
          * - 外部数据动态更新（长度变化、内容结构变化等）
          */
         dataRef.current = data;
