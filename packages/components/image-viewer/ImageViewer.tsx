48fc540a153b0f3b31fad0b16c68b62cefc50bcd
张宏 <wonkzhang@163.com>
2025-09-15 20:12:07 +0800

    feat(imageviewer): imageViewer图片预览组 件增加默认的trigger
    ImageViewer图片预览组 件增加默认的trigger

--- packages/components/image-viewer/ImageViewer.tsx
+++ packages/components/image-viewer/ImageViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { isFunction } from 'lodash-es';
 import { ImageModal } from './ImageViewerModal';
@@ -13,6 +13,7 @@ import useControlled from '../hooks/useControlled';
 import useDefaultProps from '../hooks/useDefaultProps';
 import { canUseDocument } from '../_util/dom';
 import useAttach from '../hooks/useAttach';
+import { DefaultTrigger } from './defaultTrigger';
 
 export interface ImageViewerProps extends TdImageViewerProps, StyledProps {}
 
@@ -30,21 +31,34 @@ const ImageViewer: React.FC<ImageViewerProps> = (originalProps) => {
   const imageScale = useImageScale(imageScaleD);
   const viewerScale = useViewerScale(viewerScaleD);
 
-  const isMini = mode === 'modeless';
+  const isMini = useMemo(() => mode === 'modeless', [mode]);
 
-  const close: ImageModalProps['onClose'] = (context) => {
-    setVisible(false, context);
-    setTimeout(() => setVisibled(false), 196);
-  };
+  const close: ImageModalProps['onClose'] = useCallback(
+    (context) => {
+      setVisible(false, context);
+      setTimeout(() => setVisibled(false), 196);
+    },
+    [setVisible],
+  );
 
-  const open = () => {
+  const open = useCallback(() => {
     if (!images) return;
     setVisible(true, null);
     setVisibled(true);
-  };
-  // todo 兼容旧api，新： open close 旧： onOpen, onClose
-  // @ts-ignore TODO 待类型完善后移除
-  const uiImage: TNode = isFunction(trigger) ? trigger({ open, close, onOpen: open, onClose: close }) : trigger;
+  }, [images, setVisible]);
+
+  const uiImage: TNode = useMemo(() => {
+    // 如果 trigger 为空，则使用默认触发器
+    if (!trigger && list.length) {
+      const { mainImage, thumbnail } = list[0];
+      // 如果 mainImage 为空，则使用 thumbnail
+      const showImage = mainImage || thumbnail;
+      return <DefaultTrigger showImage={showImage} onClick={open} />;
+    }
+    // todo 兼容旧api，新： open close 旧： onOpen, onClose
+    // @ts-ignore TODO 待类型完善后移除
+    return isFunction(trigger) ? trigger({ open, close, onOpen: open, onClose: close }) : trigger;
+  }, [close, list, open, trigger]);
 
   const attachElement = useMemo(() => {
     if (!canUseDocument || !imageViewerAttach) return null;
---
+++ packages/components/image-viewer/defaultTrigger.tsx
@@ -0,0 +1,41 @@
+import React, { useMemo, type FC } from 'react';
+import { BrowseIcon } from 'tdesign-icons-react';
+import { Image } from 'tdesign-react';
+import useConfig from '../hooks/useConfig';
+
+interface DefaultTriggerProps {
+  // 需要展示的图片
+  showImage: string | File;
+  // 点击事件
+  onClick: () => void;
+}
+
+/**
+ * 默认触发器
+ *
+ * @param {DefaultTriggerProps} props 默认触发器属性
+ * @returns {JSX.Element} 默认触发器
+ */
+export const DefaultTrigger: FC<DefaultTriggerProps> = ({ showImage, onClick }) => {
+  const { classPrefix } = useConfig();
+
+  const maskEl = useMemo(
+    () => (
+      <div className={`${classPrefix}-image-viewer__trigger-hover`} onClick={onClick}>
+        <BrowseIcon name={'browse'} className={`${classPrefix}-image-viewer__trigger-icon`} />
+        <span>预览</span>
+      </div>
+    ),
+    [classPrefix, onClick],
+  );
+
+  return (
+    <Image
+      src={showImage}
+      overlayContent={maskEl}
+      overlayTrigger="hover"
+      fit="contain"
+      className={`${classPrefix}-image-viewer__trigger`}
+    />
+  );
+};
