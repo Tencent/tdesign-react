c7d497f38fd56bf48ca70d7692e186d0a20e4b98
carolin913 <carolin913@126.com>
2025-09-16 19:13:58 +0800

    feat(chatmarkdown): add event demo

--- packages/pro-components/chat/chat-markdown/_example/event.tsx
+++ packages/pro-components/chat/chat-markdown/_example/event.tsx
@@ -3,51 +3,7 @@ import { Button, Space } from 'tdesign-react';
 import { ChatMarkdown, findTargetElement } from '@tdesign-react/aigc';
 
 const doc = `
-# This is TDesign
-
-## This is TDesign
-
-### This is TDesign
-
-#### This is TDesign
-
-The point of reference-style links is not that they’re easier to write. The point is that with reference-style links, your document source is vastly more readable. Compare the above examples: using reference-style links, the paragraph itself is only 81 characters long; with inline-style links, it’s 176 characters; and as raw \`HTML\`, it’s 234 characters. In the raw \`HTML\`, there’s more markup than there is text.
-
-> This is a blockquote with two paragraphs. Lorem ipsum dolor sit amet.
-
-an example | *an example* | **an example**
-
-1. Bird
-1. McHale
-1. Parish
-    1. Bird
-    1. McHale
-        1. Parish
-
-- Red
-- Green
-- Blue
-    - Red
-    - Green
-        - Blue
-
-This is [an example](http://example.com/ "Title") inline link.
-
-<http://example.com/>
-
-\`\`\`bash
-$ npm i tdesign-vue-next
-\`\`\`
-
----
-
-\`\`\`javascript
-import { createApp } from 'vue';
-import App from './app.vue';
-
-const app = createApp(App);
-app.use(TDesignChat);
-\`\`\`
+这是一个markdown[链接地址](http://example.com), 点击后**不会**自动跳转.
 `;
 
 export default function ThinkContentDemo() {
@@ -120,10 +76,5 @@ export default function ThinkContentDemo() {
     setIsTyping(!isTyping);
   };
 
-  return (
-    <Space direction="vertical">
-      <Button onClick={toggleTyping}>{isTyping ? '暂停' : '流式输出'}</Button>
-      <ChatMarkdown content={displayText} />
-    </Space>
-  );
+  return <ChatMarkdown content={displayText} />;
 }
