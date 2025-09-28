ef302bb68605c79498dc35370d9ebdc903885462
Rylan <rylanbot@qq.com>
2025-09-28 17:12:48 +0800

    fix(Table): ensure virtual scrolling calculations account for the first and last row (#3792)
    * fix: handle undefined classNames in `TBody` and `TFoot`
    
    * fix(Table): ensure virtual scrolling calculations account for the first and last row
    
    * fix(TBody): ensure unique key
    
    * fix(TBody): remove unnecessary data indexing during non-virtual scroll
    
    * fix: improve virtual index retrieval
    
    * chore: update CHANGELOG.md
    
    * fix: optimize full row rendering logic for virtual scrolling
    
    * chore(TBody): rename functions for clarity and consistency in rendering full rows
    
    * fix(useVirtualScroll): remove console logs and ensure correct length check
    
    * chore: update snapshots
    
    * fix(useVirtualScroll): change ref type
    
    * chore: update CHANGELOG.md
    
    * fix(TBody): prevent pushing null values to trNodeList
    
    * chore: remove underscores from virtual row properties
    
    * revert(useVirtualScroll): optimize data comparison
    
    * fix(BaseTable): concat data correctly
    
    * fix(TBody): use uniqueId for row key
    
    * chore: remove unused function
    
    * revert(useVirtualScroll): replace requestAnimationFrame with setTimeout
    
    * revert(BaseTable): use `setTimeout`
    
    * refactor(TBody): streamline full row rendering
    
    * fix(useVirtualScroll): prevent reset of scroll bar
    
    * fix(BaseTable): adjust null check position
    
    * fix(TBody): correct classname
    
    * chore: update snapshots
    
    * chore: update common
    
    * chore: update snapshot
    
    * fix(TBody): simplify full row rendering logic
    
    * fix: avoid scrollbar flicker on initialization
    
    * feat: support `fixedRows` configuration in virtual scrolling
    
    * fix(useFixed): ensure scroll height is correct
    
    * fix(useFixed): ensure table width updates correctly after data rendering to prevent flicker
    
    * fix(useVirtualScroll): improve handling of fake data for fixed rows
    
    * chore(BaseTable): update virtual fake data identifiers for first and last full rows
    
    * fix(useFixed): move the forced reflow location
    
    * fix(Affix): add optional chaining to scroll container height calculation
    
    * chore(useVirtualScroll): refactor state management
    
    * feat(useDeepEffect): enhance dependency comparison with function check
    
    * revert(TBody): `handleRowMounted` in virtual scroll
    
    * fix(BaseTable): improve virtual scroll initialization
    
    * fix(BaseTable): refine virtual scroll initialization logic
    
    * chore: update CHANGELOG.md
    
    * fix(BaseTable): optimize virtual scroll initialization handling
    
    * fix(BaseTable): enhance affixed header width calculating
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/components/table/TBody.tsx
+++ packages/components/table/TBody.tsx
@@ -1,16 +1,17 @@
-import React, { CSSProperties, MutableRefObject, ReactNode, useMemo } from 'react';
-import { camelCase, get, pick } from 'lodash-es';
+/* eslint-disable no-underscore-dangle */
+import React, { type CSSProperties, type MutableRefObject, type ReactNode, useMemo } from 'react';
 import classNames from 'classnames';
-import TR, { ROW_LISTENERS, TABLE_PROPS } from './TR';
+import { camelCase, get, pick } from 'lodash-es';
 import { useLocaleReceiver } from '../locale/LocalReceiver';
 import { TableClassName } from './hooks/useClassName';
 import useRowspanAndColspan from './hooks/useRowspanAndColspan';
-import { BaseTableProps, RowAndColFixedPosition } from './interface';
-import { TdBaseTableProps } from './type';
-import { PaginationProps } from '../pagination';
-import { VirtualScrollConfig } from '../hooks/useVirtualScroll';
+import TR, { ROW_LISTENERS, TABLE_PROPS, type TrProps } from './TR';
 
-export const ROW_AND_TD_LISTENERS = ROW_LISTENERS.concat('cell-click');
+import type { RowMountedParams, VirtualScrollConfig } from '../hooks/useVirtualScroll';
+import type { PaginationProps } from '../pagination';
+import type { BaseTableProps, RowAndColFixedPosition } from './interface';
+import type { TableRowData, TdBaseTableProps } from './type';
+
 export interface TableBodyProps extends BaseTableProps {
   classPrefix: string;
   ellipsisOverlayClassName: string;
@@ -25,8 +26,10 @@ export interface TableBodyProps extends BaseTableProps {
   virtualConfig: VirtualScrollConfig;
   pagination?: PaginationProps;
   allTableClasses?: TableClassName;
-  handleRowMounted?: (rowData: any) => void;
+  handleRowMounted?: (params: RowMountedParams) => void;
 }
+
+export const ROW_AND_TD_LISTENERS = ROW_LISTENERS.concat('cell-click');
 
 // table 到 body 的相同属性
 export const extendTableProps = [
@@ -55,17 +58,37 @@ export const extendTableProps = [
   'onScrollY',
 ];
 
+const trProperties = [
+  'classPrefix',
+  'ellipsisOverlayClassName',
+  'rowAndColFixedPosition',
+  'scroll',
+  'tableRef',
+  'tableContentRef',
+  'trs',
+  'bufferSize',
+  'isVirtual',
+  'rowHeight',
+  'scrollType',
+];
+
 export default function TBody(props: TableBodyProps) {
   // 如果不是变量复用，没必要对每一个参数进行解构（解构过程需要单独的内存空间存储临时变量）
   const { data, columns, rowKey, firstFullRow, lastFullRow, virtualConfig, allTableClasses } = props;
-  const [global, t] = useLocaleReceiver('table');
-  const { tableFullRowClasses, tableBaseClass } = allTableClasses;
-  const { skipSpansMap } = useRowspanAndColspan(data, columns, rowKey, props.rowspanAndColspan);
-  const columnLength = columns.length;
-  const dataLength = data?.length;
 
+  const { isVirtualScroll } = virtualConfig;
+  const renderData = isVirtualScroll ? virtualConfig.visibleData : data;
+
+  const [global, t] = useLocaleReceiver('table');
+
+  const { skipSpansMap } = useRowspanAndColspan(data, columns, rowKey, props.rowspanAndColspan);
+  const isSkipSnapsMapNotFinish = Boolean(props.rowspanAndColspan && !skipSpansMap.size);
+
+  const { tableFullRowClasses, tableBaseClass } = allTableClasses;
   const tbodyClasses = useMemo(() => [tableBaseClass.body], [tableBaseClass.body]);
   const hasFullRowConfig = useMemo(() => firstFullRow || lastFullRow, [firstFullRow, lastFullRow]);
+
+  const isEmpty = !data?.length && !props.loading && !hasFullRowConfig;
 
   const renderEmpty = (columns: TableBodyProps['columns']) => (
     <tr className={classNames([tableBaseClass.emptyRow, { [tableFullRowClasses.base]: props.isWidthOverflow }])}>
@@ -80,22 +103,42 @@ export default function TBody(props: TableBodyProps) {
     </tr>
   );
 
-  const getFullRow = (columnLength: number, type: 'first-full-row' | 'last-full-row') => {
-    const tType = camelCase(type);
+  const renderFullRow = (type: 'first-full-row' | 'last-full-row') => {
     const fullRowNode = {
       'first-full-row': firstFullRow,
       'last-full-row': lastFullRow,
     }[type];
-
     if (!fullRowNode) return null;
+
+    const rowProps: React.ComponentProps<'tr'> = {};
+    if (isVirtualScroll) {
+      const fullRowIndex = {
+        'first-full-row': 0,
+        'last-full-row': data.length - 1,
+      }[type];
+      const rowVisible = renderData.some((item) => item.__VIRTUAL_SCROLL_INDEX === fullRowIndex);
+      if (!rowVisible) return null;
+      rowProps.ref = (ref) => {
+        if (ref) {
+          // 首尾行的高度可能不固定
+          props?.handleRowMounted({
+            ref,
+            data: data[fullRowIndex],
+          });
+        }
+      };
+    }
+
     const isFixedToLeft = props.isWidthOverflow && columns.find((col) => col.fixed === 'left');
+    const tType = camelCase(type);
     const classes = [tableFullRowClasses.base, tableFullRowClasses[tType]];
-    /** innerFullRow 和 innerFullElement 同时存在，是为了保证 固定列时，当前行不随内容进行横向滚动 */
+
+    /** innerFullRow 和 innerFullElement 同时存在，是为了保证固定列时，当前行不随内容进行横向滚动 */
     return (
-      <tr className={classNames(classes)}>
-        <td colSpan={columnLength}>
+      <tr key={type} className={classNames(classes)} {...rowProps}>
+        <td colSpan={columns.length}>
           <div
-            className={classNames({ [tableFullRowClasses.innerFullRow]: isFixedToLeft })}
+            className={classNames({ [tableFullRowClasses.innerFullRow]: isFixedToLeft }) || undefined}
             style={isFixedToLeft ? { width: `${props.tableWidth.current}px` } : {}}
           >
             <div className={tableFullRowClasses.innerFullElement}>{fullRowNode}</div>
@@ -105,55 +148,53 @@ export default function TBody(props: TableBodyProps) {
     );
   };
 
-  const firstFullRowNode = useMemo(
-    () => getFullRow(columnLength, 'first-full-row'),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [firstFullRow, columnLength, getFullRow],
-  );
+  const renderExpandedRows = (row: TableRowData, rowIndex: number) => {
+    const p = {
+      row,
+      index: rowIndex,
+      columns,
+      tableWidth: props.tableWidth.current,
+      isWidthOverflow: props.isWidthOverflow,
+    };
+    return props.renderExpandedRow?.(p);
+  };
 
-  const lastFullRowNode = useMemo(
-    () => getFullRow(columnLength, 'last-full-row'),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [lastFullRow, columnLength, getFullRow],
-  );
-
-  const isSkipSnapsMapNotFinish = Boolean(props.rowspanAndColspan && !skipSpansMap.size);
-
-  const getTRNodeList = () => {
+  const renderTRNodeList = () => {
     if (isSkipSnapsMapNotFinish) return null;
+
     const trNodeList: ReactNode[] = [];
-    const properties = [
-      'classPrefix',
-      'ellipsisOverlayClassName',
-      'rowAndColFixedPosition',
-      'scroll',
-      'tableRef',
-      'tableContentRef',
-      'trs',
-      'bufferSize',
-      'isVirtual',
-      'rowHeight',
-      'scrollType',
-    ];
 
-    const renderData = virtualConfig.isVirtualScroll ? virtualConfig.visibleData : data;
+    // 首行数据
+    const firstFullRowNode = renderFullRow('first-full-row');
+    firstFullRowNode && trNodeList.push(firstFullRowNode);
 
+    // body 数据
     renderData?.forEach((row, rowIndex) => {
+      if (row.__VIRTUAL_FAKE_DATA) return;
+
+      const getRowIndex = () => {
+        const virtualIndex = row.__VIRTUAL_SCROLL_INDEX;
+        if (isVirtualScroll && firstFullRow && virtualIndex) {
+          // 确保 serial-number 索引不受到首行 FAKE 数据影响
+          return virtualIndex - 1;
+        }
+        return virtualIndex ?? rowIndex;
+      };
+
       const trProps = {
         ...pick(props, TABLE_PROPS),
         rowKey: props.rowKey || 'id',
         row,
         columns,
-        // eslint-disable-next-line
-        rowIndex: row.__VIRTUAL_SCROLL_INDEX || rowIndex,
-        dataLength,
+        rowIndex: getRowIndex(),
+        dataLength: data.length,
         skipSpansMap,
         virtualConfig,
         classPrefix: props.classPrefix,
         ellipsisOverlayClassName: props.ellipsisOverlayClassName,
-        ...pick(props, properties),
+        ...pick(props, trProperties),
         pagination: props.pagination,
-      };
+      } as TrProps;
       if (props.onCellClick) {
         trProps.onCellClick = props.onCellClick;
       }
@@ -163,27 +204,20 @@ export default function TBody(props: TableBodyProps) {
       );
       trNodeList.push(trNode);
 
-      // 执行展开行渲染
-      if (props.renderExpandedRow) {
-        const p = {
-          row,
-          index: rowIndex,
-          columns,
-          tableWidth: props.tableWidth.current,
-          isWidthOverflow: props.isWidthOverflow,
-        };
-        const expandedContent = props.renderExpandedRow(p);
-        expandedContent && trNodeList.push(expandedContent);
-      }
+      // 展开行数据
+      const expandedRowNode = renderExpandedRows(row, rowIndex);
+      expandedRowNode && trNodeList.push(expandedRowNode);
     });
+
+    // 尾行数据
+    const lastFullRowNode = renderFullRow('last-full-row');
+    lastFullRowNode && trNodeList.push(lastFullRowNode);
     return trNodeList;
   };
 
-  const isEmpty = !data?.length && !props.loading && !hasFullRowConfig;
-
   // 垫上隐藏的 tr 元素高度
   const translate = `translateY(${virtualConfig.translateY}px)`;
-  const posStyle: CSSProperties = virtualConfig.isVirtualScroll
+  const posStyle: CSSProperties = isVirtualScroll
     ? ({
         transform: translate,
         msTransform: translate,
@@ -192,17 +226,9 @@ export default function TBody(props: TableBodyProps) {
       } as CSSProperties)
     : undefined;
 
-  const list = (
-    <>
-      {firstFullRowNode}
-      {getTRNodeList()}
-      {lastFullRowNode}
-    </>
-  );
-
   return (
-    <tbody className={classNames(tbodyClasses)} style={{ ...posStyle }}>
-      {isEmpty ? renderEmpty(columns) : list}
+    <tbody className={classNames(tbodyClasses) || undefined} style={{ ...posStyle }}>
+      {isEmpty ? renderEmpty(columns) : renderTRNodeList()}
     </tbody>
   );
 }
