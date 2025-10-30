1e575a88ac52e6cb9e711dc52931682f728b2a1d
RSS1102 <jimmyrss1102@gmail.com>
2025-10-30 15:17:11 +0800

    fix(Popup): set default attach property to 'body'

--- packages/components/popup/defaultProps.ts
+++ packages/components/popup/defaultProps.ts
@@ -5,6 +5,7 @@
 import { TdPopupProps } from './type';
 
 export const popupDefaultProps: TdPopupProps = {
+  attach: 'body',
   destroyOnClose: false,
   hideEmptyPopup: false,
   placement: 'top',
