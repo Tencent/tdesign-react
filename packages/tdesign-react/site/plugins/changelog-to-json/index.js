9077c336b44c28111c6b1c9e7d622b0a94860f78
Rylan <rylanbot@qq.com>
2025-12-27 00:45:35 +0800

    feat(changelog-to-json): support english

--- packages/tdesign-react/site/plugins/changelog-to-json/index.js
+++ packages/tdesign-react/site/plugins/changelog-to-json/index.js
@@ -1,31 +1,52 @@
-import generateChangelogJson from '@tdesign/common-docs/plugins/changelog-to-json';
-import { promises } from 'fs';
+import { promises as fs } from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+import generateChangelogJson from '@tdesign/common-docs/plugins/changelog-to-json';
+
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const distDir = path.resolve(__dirname, '../../dist');
+const rootDir = path.resolve(__dirname, '../../../');
 
-const outputPath = path.resolve(__dirname, '../../dist/changelog.json');
-const changelogPath = path.resolve(__dirname, '../../../CHANGELOG.md');
+const changelogConfigs = [
+  {
+    input: 'CHANGELOG.md',
+    output: 'changelog.json',
+  },
+  {
+    input: 'CHANGELOG.en-US.md',
+    output: 'changelog.en-US.json',
+  },
+];
+
+async function generate(input) {
+  return generateChangelogJson(path.resolve(rootDir, input), 'web');
+}
 
 export default function changelog2Json() {
   return {
     name: 'changelog-to-json',
     configureServer(server) {
       // 开发模式时拦截请求
-      server.middlewares.use('/changelog.json', async (_, res) => {
-        const json = await generateChangelogJson(changelogPath, 'web');
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(json));
+      changelogConfigs.forEach(({ input, output }) => {
+        server.middlewares.use(`/${output}`, async (_, res) => {
+          const json = await generate(input);
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(json));
+        });
       });
     },
+
     async closeBundle() {
       // 生产构建时写入物理文件
-      if (process.env.NODE_ENV === 'production') {
-        const json = await generateChangelogJson(changelogPath, 'web');
-        await promises.writeFile(outputPath, JSON.stringify(json));
-      }
+      if (process.env.NODE_ENV !== 'production') return;
+      await Promise.all(
+        changelogConfigs.map(async ({ input, output }) => {
+          const json = await generate(input);
+          await fs.writeFile(path.resolve(distDir, output), JSON.stringify(json));
+        }),
+      );
     },
   };
 }
