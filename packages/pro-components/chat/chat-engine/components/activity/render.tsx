1a298e4c4a6918d025f89226193e083c8c7a47e9
carolin913 <carolin913@126.com>
2026-02-05 15:11:27 +0800

    chore: fix lint

--- packages/pro-components/chat/chat-engine/components/activity/render.tsx
+++ packages/pro-components/chat/chat-engine/components/activity/render.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useMemo, Component, ErrorInfo } from 'react';
 import isEqual from 'react-fast-compare';
 import { type ActivityData } from 'tdesign-web-components';
--- packages/pro-components/chat/chat-engine/hooks/useAgentActivity.ts
+++ packages/pro-components/chat/chat-engine/hooks/useAgentActivity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useRef, useEffect } from 'react';
 import type { ActivityConfig } from '../components/activity/types';
 import { activityRegistry } from '../components/activity/registry';
