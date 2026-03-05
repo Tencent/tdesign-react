04fc8297b971f2167e5eaf3b9bbbdbbc4b98ef89
Rylan <rylanbot@qq.com>
2026-03-05 22:36:38 +0800

    fix(SelectInput): prevent popup from opening when clicking the clear icon

--- packages/components/hooks/useInnerPopupVisible.tsx
+++ packages/components/hooks/useInnerPopupVisible.tsx
@@ -4,19 +4,29 @@ import type { PopupVisibleChangeContext } from '../popup';
 function useInnerPopupVisible(handler: (visible: boolean, ctx: PopupVisibleChangeContext) => void) {
   const { classPrefix } = useConfig();
 
-  const isClearIconClick = (target: EventTarget | null): boolean => {
-    if (!target || !(target instanceof Element)) return false;
-    return !!(
-      target?.closest?.(`.${classPrefix}-input__suffix-clear`) ||
-      target?.closest?.(`.${classPrefix}-tag-input__suffix-clear`) ||
-      target?.closest?.(`.${classPrefix}-range-input__suffix-clear`)
-    );
+  // fix: https://github.com/Tencent/tdesign-react/issues/2320
+  // 点击 clear icon 是否触发 Popup 显隐切换的逻辑交给具体逻辑自己处理，避免重复触发
+  const clearSelectors = [
+    `.${classPrefix}-input__suffix-clear`,
+    `.${classPrefix}-tag-input__suffix-clear`,
+    `.${classPrefix}-range-input__suffix-clear`,
+  ];
+
+  // fix: https://github.com/Tencent/tdesign-react/issues/4157
+  const tagCloseSelector = `.${classPrefix}-tag__icon-close`;
+
+  const isMatchSelector = (target: EventTarget | null, selectors: string | string[]): boolean => {
+    if (!(target instanceof Element)) return false;
+    const list = Array.isArray(selectors) ? selectors : [selectors];
+    return list.some((selector) => target.closest(selector));
   };
 
   return (visible: boolean, ctx: PopupVisibleChangeContext) => {
-    // Fix: https://github.com/Tencent/tdesign-react/issues/2320
-    // 点击 clear icon 是否触发 Popup 显隐切换的逻辑交给具体逻辑自己处理，避免重复触发
-    if (isClearIconClick(ctx?.e?.target) && visible) return;
+    const target = ctx?.e?.target;
+
+    if (visible && isMatchSelector(target, clearSelectors)) return;
+    if (visible && isMatchSelector(target, tagCloseSelector)) return;
+
     // 执行原函数
     handler(visible, ctx);
   };
