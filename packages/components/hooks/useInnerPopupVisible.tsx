016a36a56715afa25a401d8108981c1f9df696f8
Rylan <rylanbot@qq.com>
2026-03-09 17:23:01 +0800

    chore: optimize

--- packages/components/hooks/useInnerPopupVisible.tsx
+++ packages/components/hooks/useInnerPopupVisible.tsx
@@ -1,35 +1,36 @@
+import { useCallback } from 'react';
 import useConfig from './useConfig';
+
 import type { PopupVisibleChangeContext } from '../popup';
 
 function useInnerPopupVisible(handler: (visible: boolean, ctx: PopupVisibleChangeContext) => void) {
   const { classPrefix } = useConfig();
 
-  // fix: https://github.com/Tencent/tdesign-react/issues/2320
-  // 点击 clear icon 是否触发 Popup 显隐切换的逻辑交给具体逻辑自己处理，避免重复触发
-  const clearSelectors = [
+  const ignoreSelectors = [
+    // fix: https://github.com/Tencent/tdesign-react/issues/2320
+    // 点击 clear icon 是否触发 Popup 显隐切换的逻辑交给具体逻辑自己处理，避免重复触发
     `.${classPrefix}-input__suffix-clear`,
     `.${classPrefix}-tag-input__suffix-clear`,
     `.${classPrefix}-range-input__suffix-clear`,
+    // fix: https://github.com/Tencent/tdesign-react/issues/4157
+    `.${classPrefix}-tag__icon-close`,
   ];
 
-  // fix: https://github.com/Tencent/tdesign-react/issues/4157
-  const tagCloseSelector = `.${classPrefix}-tag__icon-close`;
-
-  const isMatchSelector = (target: EventTarget | null, selectors: string | string[]): boolean => {
+  const shouldIgnore = (target: EventTarget | null) => {
     if (!(target instanceof Element)) return false;
-    const list = Array.isArray(selectors) ? selectors : [selectors];
-    return list.some((selector) => target.closest(selector));
+    return ignoreSelectors.some((selector) => target.closest(selector));
   };
 
-  return (visible: boolean, ctx: PopupVisibleChangeContext) => {
-    const target = ctx?.e?.target;
-
-    if (visible && isMatchSelector(target, clearSelectors)) return;
-    if (visible && isMatchSelector(target, tagCloseSelector)) return;
-
-    // 执行原函数
-    handler(visible, ctx);
-  };
+  return useCallback(
+    (visible: boolean, ctx: PopupVisibleChangeContext) => {
+      const target = ctx?.e?.target;
+      if (visible && shouldIgnore(target)) return;
+      // 执行原函数
+      handler(visible, ctx);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [handler],
+  );
 }
 
 export default useInnerPopupVisible;
