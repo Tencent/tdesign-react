305448983f412dc05f440b585179c26dfa4f13ac
Rylan <rylanbot@qq.com>
2026-03-16 14:59:46 +0800

    fix(Calender): avoid "undefined" class

--- packages/components/calendar/hooks/usePrefixClass.ts
+++ packages/components/calendar/hooks/usePrefixClass.ts
@@ -5,15 +5,19 @@ export default function usePrefixClass() {
   const { classPrefix } = useConfig();
 
   return useCallback(
-    (...args: (string | [string, string?, string?])[]) => {
+    (...args: (string | undefined | (string | undefined)[])[]) => {
       let className = '';
       args.forEach((item, index) => {
         if (item && index > 0) className = className.concat(' ');
         if (item instanceof Array) {
-          const [block, element, modifier] = item;
+          const [block, ...rest] = item;
+          if (!block) return;
           className = className.concat(classPrefix, '-', block);
+          const [element, ...modifiers] = rest;
           if (element) className = className.concat('__', element);
-          if (modifier) className = className.concat('--', modifier);
+          modifiers.forEach((modifier) => {
+            if (modifier) className = className.concat('--', modifier);
+          });
         } else if (typeof item === 'string') {
           className = className.concat(classPrefix, '-', item);
         }
