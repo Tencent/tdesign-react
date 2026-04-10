8b77ee12ffbe23731b8a6f028fe63d563c8d4dca
阿菜 Cai <jimmyrss1102@gmail.com>
2026-04-11 04:08:53 +0800

    fix(tag-input): 修复清空图标及输入框显示逻辑

--- packages/components/tag-input/TagInput.tsx
+++ packages/components/tag-input/TagInput.tsx
@@ -100,7 +100,7 @@ const TagInput = forwardRef<InputRef, TagInputProps>((originalProps, ref) => {
 
   const tagInputPlaceholder = !tagValue?.length ? placeholder : '';
 
-  const showClearIcon = Boolean(!readOnly && !disabled && clearable && isHover && tagValue?.length);
+  const showClearIcon = Boolean(!readOnly && !disabled && clearable && isHover && (tagValue?.length || tInputValue));
 
   useImperativeHandle(ref as InputRef, () => ({ ...(tagInputRef.current || {}) }));
 
@@ -246,7 +246,7 @@ const TagInput = forwardRef<InputRef, TagInputProps>((originalProps, ref) => {
       suffix={suffix}
       prefixIcon={prefixIcon}
       suffixIcon={suffixIconNode}
-      showInput={!inputProps?.readOnly || !inputProps?.readonly || !tagValue || !tagValue?.length}
+      showInput={!readOnly || !tagValue || !tagValue?.length}
       keepWrapperWidth={!autoWidth}
       onPaste={onPaste}
       onClick={onInnerClick}
