a9ebcc733741ebb23a7fedb5cd9c5193c71ce331
Rylan <rylanbot@qq.com>
2026-04-23 11:54:15 +0800

    chore: update demo

--- packages/components/tag-input/_example/excess.tsx
+++ packages/components/tag-input/_example/excess.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { TagInput, Space } from 'tdesign-react';
+import { Space, TagInput } from 'tdesign-react';
 import type { TagInputValue } from 'tdesign-react';
 
 export default function TagInputExcessExample() {
-  const [tags, setTags] = useState<TagInputValue>(['Vue', 'React']);
+  const [tags, setTags] = useState<TagInputValue>(['Vue', 'React', 'Angular', 'Miniprogram', 'Uniapp', 'Flutter']);
   return (
     <Space direction="vertical" style={{ width: '80%' }}>
       {/* <!-- 标签数量超出时，滚动显示 --> */}
