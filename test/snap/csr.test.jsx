886ee87c0ca756167a3f9b900f5377163d5759fa
wū yāng <uyarnchen@gmail.com>
2026-08-06 22:53:20 +0800

    chore: update snapshot

--- test/snap/csr.test.jsx
+++ test/snap/csr.test.jsx
@@ -34,6 +34,33 @@ class ResizeObserver {
   }
 }
 
+function normalizeIconMaskIds(container) {
+  const instanceIndexes = new Map();
+  const maskIdReplacements = new Map();
+
+  container.querySelectorAll('mask[id*="-instance-"][id*="-overlap-"]').forEach((mask) => {
+    const matched = /^(t-icon-.+)-instance-([a-zA-Z0-9_]+)(-overlap-.+)$/.exec(mask.id);
+    if (!matched) return;
+
+    const instanceKey = `${matched[1]}-instance-${matched[2]}`;
+    if (!instanceIndexes.has(instanceKey)) {
+      instanceIndexes.set(instanceKey, instanceIndexes.size);
+    }
+
+    const normalizedId = `${matched[1]}-instance-snapshot${instanceIndexes.get(instanceKey)}${matched[3]}`;
+    maskIdReplacements.set(mask.id, normalizedId);
+    mask.id = normalizedId;
+  });
+
+  container.querySelectorAll('[mask]').forEach((element) => {
+    const maskReference = /^url\(#(.+)\)$/.exec(element.getAttribute('mask') || '');
+    const normalizedId = maskReference && maskIdReplacements.get(maskReference[1]);
+    if (normalizedId) {
+      element.setAttribute('mask', `url(#${normalizedId})`);
+    }
+  });
+}
+
 function runTest() {
   const files = glob.sync('packages/components/**/_example/*.tsx', {
     ignore: IGNORE_ASYNC_EXAMPLE_LIST,
@@ -48,6 +75,7 @@ function runTest() {
         const demo = await import(`../../${file}`);
         const RealDemoComp = demo.default ? demo.default : demo;
         const { container } = render(<RealDemoComp />);
+        normalizeIconMaskIds(container);
         expect(container).toMatchSnapshot();
       });
     });
