a7278511e698241eac729621704833bf79c5f999
wū yāng <uyarnchen@gmail.com>
2026-08-11 14:35:27 +0800

    chore: replace image url (#4353)

--- packages/components/list/__tests__/list.test.tsx
+++ packages/components/list/__tests__/list.test.tsx
@@ -98,7 +98,7 @@ describe('List 组件测试', () => {
   });
 
   describe('ListItemMeta Component Test', () => {
-    const imgSrc = 'https://tdesign.gtimg.com/list-icon.png';
+    const imgSrc = 'https://tdesign.gtimg.com/site/images/list-icon.png';
     const description = 'Test Description';
     test('image string', () => {
       const { container } = render(<ListItemMeta image={imgSrc} />);
